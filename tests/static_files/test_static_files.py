8f10fb948edc35d14f584732ee4a8ae651c90692
Ashwin Vinod <ashwinvinodsa@gmail.com>
2022-01-26 23:53:23 +0530

    add more tests for staticfile

--- tests/static_files/test_static_files.py
+++ tests/static_files/test_static_files.py
@@ -10,3 +10,25 @@ def test_staticfiles(tmpdir):
         response = client.get("/static/test.txt")
         assert response.status_code == 200
         assert response.text == "content"
+
+
+def test_staticfiles_for_slash_path(tmpdir):
+    path = tmpdir.join("text.txt")
+    path.write("content")
+
+    static_files_config = StaticFilesConfig(path="/", directories=[tmpdir])
+    with create_test_client([], static_files_config=static_files_config) as client:
+        response = client.get("/text.txt")
+        assert response.status_code == 200
+        assert response.text == "content"
+
+
+def test_staticfiles_for_empty_path(tmpdir):
+    path = tmpdir.join("text.txt")
+    path.write("content")
+
+    static_files_config = StaticFilesConfig(path="", directories=[tmpdir])
+    with create_test_client([], static_files_config=static_files_config) as client:
+        response = client.get("/text.txt")
+        assert response.status_code == 200
+        assert response.text == "content"
