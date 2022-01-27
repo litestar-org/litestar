9b0825cf83f9182d7c4ad787dbff56579be55c87
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-01-27 21:19:11 +0100

    updated pre-commit settings

--- tests/kwargs/test_validations.py
+++ tests/kwargs/test_validations.py
@@ -7,6 +7,7 @@ from starlite import (
     Starlite,
     get,
     post,
+    websocket,
 )
 from starlite.constants import RESERVED_KWARGS
 
@@ -67,19 +68,24 @@ def test_raises_exception_when_keys_are_ambiguous(handler):
         Starlite(route_handlers=[handler])
 
 
-@pytest.mark.parametrize("reserved_kwarg", [kwarg for kwarg in RESERVED_KWARGS if kwarg not in ["socket", "request"]])
+@pytest.mark.parametrize("reserved_kwarg", RESERVED_KWARGS)
 def test_raises_when_reserved_kwargs_are_misused(reserved_kwarg):
+    decorator = post if reserved_kwarg != "socket" else websocket
+
     exec(f"def test_fn({reserved_kwarg}: int) -> None: pass")
-    handler_with_path_param = post("/{" + reserved_kwarg + ":int}")(locals()["test_fn"])
+    handler_with_path_param = decorator("/{" + reserved_kwarg + ":int}")(locals()["test_fn"])
     with pytest.raises(ImproperlyConfiguredException):
         Starlite(route_handlers=[handler_with_path_param])
 
     exec(f"def test_fn({reserved_kwarg}: int) -> None: pass")
-    handler_with_dependency = post("/", dependencies={reserved_kwarg: Provide(my_dependency)})(locals()["test_fn"])
+    handler_with_dependency = decorator("/", dependencies={reserved_kwarg: Provide(my_dependency)})(locals()["test_fn"])
     with pytest.raises(ImproperlyConfiguredException):
         Starlite(route_handlers=[handler_with_dependency])
 
-    exec(f"def test_fn({reserved_kwarg}: int = Parameter(query='my_param')) -> None: pass")
-    handler_with_aliased_param = post("/")(locals()["test_fn"])
-    with pytest.raises(ImproperlyConfiguredException):
-        Starlite(route_handlers=[handler_with_aliased_param])
+    # these kwargs are set to Any when the signature model is generated,
+    # because pydantic can't handle generics for non pydantic classes. So these tests wont work for aliased parameters.
+    if reserved_kwarg not in ["socket", "request"]:
+        exec(f"def test_fn({reserved_kwarg}: int = Parameter(query='my_param')) -> None: pass")
+        handler_with_aliased_param = decorator("/")(locals()["test_fn"])
+        with pytest.raises(ImproperlyConfiguredException):
+            Starlite(route_handlers=[handler_with_aliased_param])
