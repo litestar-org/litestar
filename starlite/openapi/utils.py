bf929fe83ce7c8d1afc0b261e04beef8b766435a
Tory Clasen <ToryClasen@Gmail.com>
2022-02-15 15:22:16 -0500

    feat: extract tags from route_handler
    Signed-off-by: Tory Clasen <ToryClasen@Gmail.com>

--- starlite/openapi/utils.py
+++ starlite/openapi/utils.py
@@ -1,4 +1,7 @@
 import re
+from typing import List, Optional
+
+from starlite.handlers.http import HTTPRouteHandler
 
 CAPITAL_LETTERS_PATTERN = re.compile(r"(?=[A-Z])")
 
@@ -6,3 +9,9 @@ CAPITAL_LETTERS_PATTERN = re.compile(r"(?=[A-Z])")
 def pascal_case_to_text(s: str) -> str:
     """Given a 'PascalCased' string, return its split form- 'Pascal Cased'"""
     return " ".join(re.split(CAPITAL_LETTERS_PATTERN, s)).strip()
+
+
+def extract_tags_from_route_handler(route_handler: HTTPRouteHandler) -> Optional[List[str]]:
+    child_tags = set(route_handler.tags or [])
+    parent_tags = set(route_handler.owner.tags if route_handler.owner and hasattr(route_handler.owner, "tags") else [])
+    return list(child_tags | parent_tags) or None
