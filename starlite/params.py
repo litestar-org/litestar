804f92598282ce879d5f5e390d5ff4dd6aada00f
Peter Schutt <peter@topsport.com.au>
2022-06-11 19:25:34 +1000

    Initial implementation.
    - defines `Dependency` in `params.py`
    - `Dependency` inserts `is_dependency=True` into `field.extra`
    - excludes explicit dependencies from parameter documentation.

--- starlite/params.py
+++ starlite/params.py
@@ -113,3 +113,42 @@ def Body(
         regex=regex,  # type: ignore
         **extra,
     )
+
+
+@validate_arguments(config={"arbitrary_types_allowed": True})
+def Dependency(
+    *,
+    default: Any = Undefined,
+    const: Optional[bool] = None,
+    gt: Optional[float] = None,
+    ge: Optional[float] = None,
+    lt: Optional[float] = None,
+    le: Optional[float] = None,
+    multiple_of: Optional[float] = None,
+    min_items: Optional[int] = None,
+    max_items: Optional[int] = None,
+    min_length: Optional[int] = None,
+    max_length: Optional[int] = None,
+    regex: Optional[str] = None
+) -> Any:
+    """
+    Creates a pydantic FieldInfo instance with an extra kwargs,
+    used for both parameter parsing and OpenAPI schema generation.
+    """
+    extra: Dict[str, Any] = {}
+    extra.update(is_dependency=True)
+    return Field(
+        default,
+        const=const,  # type: ignore
+        gt=gt,  # type: ignore
+        ge=ge,  # type: ignore
+        lt=lt,  # type: ignore
+        le=le,  # type: ignore
+        multiple_of=multiple_of,  # type: ignore
+        min_items=min_items,  # type: ignore
+        max_items=max_items,  # type: ignore
+        min_length=min_length,  # type: ignore
+        max_length=max_length,  # type: ignore
+        regex=regex,  # type: ignore
+        **extra,
+    )
