ad1b429d0faa5ad3c0dc612ff7a3a37a3a0a79ee
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-06-11 20:52:40 +0200

    added a test for generic model injection

---
+++ tests/dependency_injection/test_injection_of_generic_models.py
@@ -0,0 +1,45 @@
+from typing import Generic, Optional, Type, TypeVar
+
+from pydantic import BaseModel
+from pydantic.generics import GenericModel
+from starlette.status import HTTP_200_OK
+
+from starlite import Provide, create_test_client, get
+
+T = TypeVar("T")
+
+
+class Store(GenericModel, Generic[T]):
+    """Abstract store"""
+
+    model: Type[T]
+
+    def get(self, id: str) -> Optional[T]:
+        raise NotImplementedError
+
+
+class Item(BaseModel):
+    name: str
+
+
+class DictStore(Store[Item]):
+    """In-memory store implementation"""
+
+    def get(self, id: str) -> Optional[T]:
+        return None
+
+
+@get("/")
+def root(store: DictStore) -> Optional[Item]:
+    assert isinstance(store, DictStore)
+    return store.get("0")
+
+
+def get_item_store() -> DictStore:
+    return DictStore(model=Item)  # type: ignore
+
+
+def test_generic_model_injection() -> None:
+    with create_test_client(root, dependencies={"store": Provide(get_item_store, use_cache=True)}) as client:
+        response = client.get("/")
+        assert response.status_code == HTTP_200_OK
