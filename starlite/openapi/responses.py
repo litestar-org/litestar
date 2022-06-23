fd86dc5a1468bafe775f28b24a78e87918717de8
Tim Wedde <timwedde@icloud.com>
2022-06-23 18:28:53 +0200

    Type `extra` as a nullable union of dict and list

--- starlite/openapi/responses.py
+++ starlite/openapi/responses.py
@@ -125,7 +125,9 @@ def create_error_responses(exceptions: List[Type[HTTPException]]) -> Iterator[Tu
                 properties=dict(
                     status_code=Schema(type=OpenAPIType.INTEGER),
                     detail=Schema(type=OpenAPIType.STRING),
-                    extra=Schema(type=OpenAPIType.OBJECT, additionalProperties=Schema()),
+                    extra=Schema(
+                        type=[OpenAPIType.NULL, OpenAPIType.OBJECT, OpenAPIType.ARRAY], additionalProperties=Schema()
+                    ),
                 ),
                 description=pascal_case_to_text(get_name(exc)),
                 examples=[{"status_code": status_code, "detail": HTTPStatus(status_code).phrase, "extra": {}}],
