70230881f722c46740afedf9666c1e3583b25dc0
Peter Schutt <peter@topsport.com.au>
2022-06-27 18:47:36 +1000

    Change to order of exception handler resolution.
    - check the MRO before checking for a 500 handler
    - only use the 500 handler for non-HTTPException exceptions
    - description of algo in docstring
    - add tests with multiple handlers in `exception_handlers` mapping

--- tests/utils/test_exceptions.py
+++ tests/utils/test_exceptions.py
@@ -3,12 +3,16 @@ from typing import Any, Dict, Type, Union
 import pytest
 from starlette.status import HTTP_400_BAD_REQUEST, HTTP_500_INTERNAL_SERVER_ERROR
 
-from starlite import InternalServerException, ValidationException
+from starlite import HTTPException, InternalServerException, ValidationException
 from starlite.types import ExceptionHandler
 from starlite.utils.exception import get_exception_handler
 
 
 def handler(_: Any, __: Any) -> Any:
+    return None
+
+
+def handler_2(_: Any, __: Any) -> Any:
     return None
 
 
@@ -23,6 +27,11 @@ def handler(_: Any, __: Any) -> Any:
         ({Exception: handler}, ValidationException(), handler),
         ({ValueError: handler}, ValidationException(), handler),
         ({ValidationException: handler}, Exception(), None),
+        ({HTTP_500_INTERNAL_SERVER_ERROR: handler}, ValidationException(), None),
+        ({HTTP_500_INTERNAL_SERVER_ERROR: handler, HTTPException: handler_2}, ValidationException(), handler_2),
+        ({HTTPException: handler, ValidationException: handler_2}, ValidationException(), handler_2),
+        ({HTTPException: handler, ValidationException: handler_2}, InternalServerException(), handler),
+        ({HTTP_500_INTERNAL_SERVER_ERROR: handler, HTTPException: handler_2}, InternalServerException(), handler),
     ],
 )
 def test_get_exception_handler(
