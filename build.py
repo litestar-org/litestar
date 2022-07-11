db80df0bac0f7d89667b6f6c1f160c0ba03ff731
Zachary Dremann <dremann@gmail.com>
2022-07-10 22:18:48 -0400

    Move cargo stuff entirely within the extensions/rust dir

--- build.py
+++ build.py
@@ -7,7 +7,9 @@ def build(setup_kwargs: Dict[str, Any]) -> None:
     """
     Add rust_extensions to the setup dict
     """
-    setup_kwargs["rust_extensions"] = [RustExtension("starlite.rust_backend", binding=Binding.PyO3)]
+    setup_kwargs["rust_extensions"] = [
+        RustExtension("starlite.rust_backend", path="starlite/extensions/rust/Cargo.toml", binding=Binding.PyO3)
+    ]
 
 
 if __name__ == "__main__":
