63b13ae0e3bf2a07659ac6b9cb8e578a0c5d4772
Zachary Dremann <dremann@gmail.com>
2022-07-10 22:21:06 -0400

    Move rust extension out of starlite folder

--- build.py
+++ build.py
@@ -8,7 +8,7 @@ def build(setup_kwargs: Dict[str, Any]) -> None:
     Add rust_extensions to the setup dict
     """
     setup_kwargs["rust_extensions"] = [
-        RustExtension("starlite.rust_backend", path="starlite/extensions/rust/Cargo.toml", binding=Binding.PyO3)
+        RustExtension("starlite.rust_backend", path="extensions/rust/Cargo.toml", binding=Binding.PyO3)
     ]
 
 
