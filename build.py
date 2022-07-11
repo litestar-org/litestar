604789cead48937b331a7e72da6f8afc0ba14f26
Zachary Dremann <dremann@gmail.com>
2022-07-10 21:07:55 -0400

    Build rust project with poetry

--- build.py
+++ build.py
@@ -1,4 +1,14 @@
+from typing import Any, Dict
+
+from setuptools_rust import Binding, RustExtension
 
 
-def build():
-    pass
+def build(setup_kwargs: Dict[str, Any]) -> None:
+    """
+    Add rust_extensions to the setup dict
+    """
+    setup_kwargs["rust_extensions"] = [RustExtension("starlite.rust_backend", binding=Binding.PyO3)]
+
+
+if __name__ == "__main__":
+    build({})
