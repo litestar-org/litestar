b236b8b0fd60a14bf07ec2c4e024c979ff243a68
Peter Schutt <peter@topsport.com.au>
2022-07-17 08:49:00 +1000

    Improves variable naming.

--- starlite/utils/lifecycle_hooks.py
+++ starlite/utils/lifecycle_hooks.py
@@ -6,9 +6,9 @@ from .helpers import is_async_callable
 
 
 class LifecycleHook:
-    def __init__(self, fn: Callable[..., Any]) -> None:
-        self.wrapped = [fn]  # wrap in list to prevent implicit binding
-        self.fn_is_async = is_async_callable(fn)
+    def __init__(self, handler: Callable[..., Any]) -> None:
+        self.wrapped = [handler]  # wrap in list to prevent implicit binding
+        self.is_async_handler = is_async_callable(handler)
 
     @property
     def hook(self) -> Callable[..., Any]:
@@ -16,6 +16,6 @@ class LifecycleHook:
         return self.wrapped[0]
 
     async def __call__(self, *args: Any) -> Any:
-        if self.fn_is_async:
+        if self.is_async_handler:
             return await self.hook(*args)
         return await run_sync(self.hook, *args)
