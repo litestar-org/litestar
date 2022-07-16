4e6f862186a3c4cbd724f4d4806b23408e002e2f
Peter Schutt <peter@topsport.com.au>
2022-07-17 09:36:16 +1000

    Adds docstring for LifecycleHook.

--- starlite/utils/lifecycle_hooks.py
+++ starlite/utils/lifecycle_hooks.py
@@ -7,6 +7,28 @@ from .helpers import is_async_callable
 
 
 class LifecycleHook:
+    """
+    Container for assignment of lifecycle hook handlers to instances.
+
+    A callable assigned to a class is implicitly converted to a bound method on an instance:
+
+        >>> def a_callable(): ...
+        ...
+        >>> class C:
+        ...   callable = a_callable
+        ...
+        >>> C().callable()
+        Traceback (most recent call last):
+          File "<stdin>", line 1, in <module>
+        TypeError: a_callable() takes 0 positional arguments but 1 was given
+
+    `LifecycleHook` supports the pattern of storing handlers as attributes of the application layers, and caching
+    resolved handlers on `HTTPRouteHandler` instances.
+
+    Additionally, it pre-computes whether the handler function is async, simplifying calling the handler during the
+    request/response cycle.
+    """
+
     def __init__(self, handler: Callable[..., Any]) -> None:
         if is_async_callable(handler):
             self.wrapped = [handler]  # wrap in list to prevent implicit binding
