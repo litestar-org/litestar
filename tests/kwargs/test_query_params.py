bb7d80ecbe26aebb7c3389e1478608264bb11969
josepdaniel <36941460+josepdaniel@users.noreply.github.com>
2022-07-19 13:25:42 +0200

    Issue #255: allow single element query param in sequence types (#262)
    * Array handling in query params
    
    * Refactor array handling
    
    Co-authored-by: Joseph Daniel <jdn@logpoint.com>

--- tests/kwargs/test_query_params.py
+++ tests/kwargs/test_query_params.py
@@ -1,5 +1,16 @@
 from datetime import datetime
-from typing import List, Optional
+from typing import (
+    Any,
+    Deque,
+    Dict,
+    FrozenSet,
+    List,
+    MutableSequence,
+    Optional,
+    Set,
+    Tuple,
+    Union,
+)
 from urllib.parse import urlencode
 
 import pytest
@@ -78,6 +89,16 @@ from starlite.testing import create_test_client
             },
             False,
         ),
+        (
+            {
+                "page": 1,
+                "pageSize": 1,
+                "brands": ["Nike"],
+                "from_date": datetime.now().timestamp(),
+                "to_date": datetime.now().timestamp(),
+            },
+            False,
+        ),
     ],
 )
 def test_query_params(params_dict: dict, should_raise: bool) -> None:
@@ -103,3 +124,65 @@ def test_query_params(params_dict: dict, should_raise: bool) -> None:
             assert response.status_code == HTTP_400_BAD_REQUEST
         else:
             assert response.status_code == HTTP_200_OK
+
+
+@pytest.mark.parametrize(
+    "expected_type,provided_value,default,expected_response_code",
+    [
+        (List[int], [1, 2, 3], ..., HTTP_200_OK),
+        (List[int], [1], ..., HTTP_200_OK),
+        (List[str], ["foo", "bar"], Parameter(min_items=1), HTTP_200_OK),
+        (List[str], ["foo", "bar"], Parameter(min_items=3), HTTP_400_BAD_REQUEST),
+        (List[int], ["foo", "bar"], ..., HTTP_400_BAD_REQUEST),
+        (Tuple[int, str, int], (1, "foo", 2), ..., HTTP_200_OK),
+        (Optional[List[str]], [], None, HTTP_200_OK),
+        (Any, [1, 2, 3], ..., HTTP_200_OK),
+        (Union[int, List[int]], [1, 2, 3], None, HTTP_200_OK),
+        (Union[int, List[int]], [1], None, HTTP_200_OK),
+        (Deque[int], [1, 2, 3], None, HTTP_200_OK),
+        (Set[int], [1, 2, 3], None, HTTP_200_OK),
+        (FrozenSet[int], [1, 2, 3], None, HTTP_200_OK),
+        (Tuple[int, ...], [1, 2, 3], None, HTTP_200_OK),
+        (MutableSequence[int], [1, 2, 3], None, HTTP_200_OK),
+    ],
+)
+def test_query_param_arrays(expected_type: Any, provided_value: Any, default: Any, expected_response_code: int) -> None:
+    test_path = "/test"
+
+    @get(test_path)
+    def test_method_with_default(param: Any = default) -> None:
+        return None
+
+    @get(test_path)
+    def test_method_without_default(param: Any) -> None:
+        return None
+
+    test_method = test_method_without_default if default is ... else test_method_with_default
+    # Set the type annotation of 'param' in a way mypy can deal with
+    test_method.fn.__annotations__["param"] = expected_type
+
+    with create_test_client(test_method) as client:
+        params = urlencode({"param": provided_value}, doseq=True)
+        response = client.get(f"{test_path}?{params}")
+        assert response.status_code == expected_response_code
+
+
+def test_query_kwarg() -> None:
+    test_path = "/test"
+
+    params = urlencode(
+        {
+            "a": ["foo", "bar"],
+            "b": "qux",
+        },
+        doseq=True,
+    )
+
+    @get(test_path)
+    def test_method(a: List[str], b: List[str], query: Dict[str, Union[str, List[str]]]) -> None:
+        assert query == {"a": ["foo", "bar"], "b": ["qux"]}
+        return None
+
+    with create_test_client(test_method) as client:
+        response = client.get(f"{test_path}?{params}")
+        assert response.status_code == HTTP_200_OK
