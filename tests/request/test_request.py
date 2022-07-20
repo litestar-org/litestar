a42d956fb0104245cc0e2c304b8fd41aeeba47da
Cody Fincher <cody.fincher@gmail.com>
2022-07-19 22:00:54 -0500

    fix: remove type ignore; update pre-commit

--- tests/request/test_request.py
+++ tests/request/test_request.py
@@ -9,7 +9,7 @@ from starlite.connection import Request
 
 
 @skipIf(sys.version_info < (3, 8), "skipping due to python 3.7 async failures")
-@pytest.mark.asyncio  # type: ignore
+@pytest.mark.asyncio
 async def test_request_empty_body_to_json() -> None:
     with patch.object(Request, "body", return_value=b""):
         request_empty_payload: Request = Request(scope={"type": "http"})
@@ -18,7 +18,7 @@ async def test_request_empty_body_to_json() -> None:
 
 
 @skipIf(sys.version_info < (3, 8), "skipping due to python 3.7 async failures")
-@pytest.mark.asyncio  # type: ignore
+@pytest.mark.asyncio
 async def test_request_invalid_body_to_json() -> None:
     with patch.object(Request, "body", return_value=b"invalid"), pytest.raises(JSONDecodeError):
         request_empty_payload: Request = Request(scope={"type": "http"})
@@ -26,7 +26,7 @@ async def test_request_invalid_body_to_json() -> None:
 
 
 @skipIf(sys.version_info < (3, 8), "skipping due to python 3.7 async failures")
-@pytest.mark.asyncio  # type: ignore
+@pytest.mark.asyncio
 async def test_request_valid_body_to_json() -> None:
     with patch.object(Request, "body", return_value=b'{"test": "valid"}'):
         request_empty_payload: Request = Request(scope={"type": "http"})
