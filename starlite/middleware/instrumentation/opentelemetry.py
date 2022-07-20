d8b51b153732af9b4d85bacd209c726a56654615
Cody Fincher <cody.fincher@gmail.com>
2022-07-19 22:00:30 -0500

    wip: added additional test cases.

--- starlite/middleware/instrumentation/opentelemetry.py
+++ starlite/middleware/instrumentation/opentelemetry.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Dict, Tuple
+from typing import TYPE_CHECKING, Any, Dict, Optional, Tuple, Union
 
 from opentelemetry.instrumentation.asgi import (
     OpenTelemetryMiddleware as BaseOpenTelemetryMiddleware,
@@ -7,33 +7,42 @@ from opentelemetry.semconv.trace import SpanAttributes
 from opentelemetry.util.http import get_excluded_urls, parse_excluded_urls
 from starlette.routing import Match
 
-from starlite.config import InstrumentationConfig
-
 if TYPE_CHECKING:
+    from opentelemetry.instrumentation.asgi import (
+        _ClientRequestHookT,
+        _ClientResponseHookT,
+        _ServerRequestHookT,
+    )
+    from opentelemetry.util.http import ExcludeList
     from starlette.types import ASGIApp, Scope
 
 _excluded_urls_from_env = get_excluded_urls("STARLITE")
 
 
-class OpenTelemetryMiddleware(BaseOpenTelemetryMiddleware):
+class OpenTelemetryMiddleware(BaseOpenTelemetryMiddleware):  # type: ignore
     """OpenTelemetry middleware for Starlite"""
 
-    def __init__(self, app: "ASGIApp", config: InstrumentationConfig):
-        tracer_provider = config.tracer_provider
-        server_request_hook = config.server_request_hook
-        client_request_hook = config.client_request_hook
-        client_response_hook = config.client_response_hook
-        excluded_urls = (
-            _excluded_urls_from_env if config.excluded_urls is None else parse_excluded_urls(config.excluded_urls)
-        )
+    def __init__(
+        self,
+        app: "ASGIApp",
+        excluded_urls: Optional[Union[str, "ExcludeList"]] = None,
+        server_request_hook: "_ServerRequestHookT" = None,
+        client_request_hook: "_ClientRequestHookT" = None,
+        client_response_hook: "_ClientResponseHookT" = None,
+        tracer_provider: Optional[str] = None,
+    ):
+        if excluded_urls is None:
+            excluded_urls = _excluded_urls_from_env
+        elif isinstance(excluded_urls, str):
+            excluded_urls = parse_excluded_urls(excluded_urls)
         super().__init__(
-            app=app,
-            tracer_provider=tracer_provider,
+            app,
             excluded_urls=excluded_urls,
-            default_span_details=_get_route_details,
             server_request_hook=server_request_hook,
             client_request_hook=client_request_hook,
             client_response_hook=client_response_hook,
+            tracer_provider=tracer_provider,
+            default_span_details=_get_route_details,
         )
 
 
--- tests/middleware/test_instrumentation_middleware.py
+++ tests/middleware/test_instrumentation_middleware.py
@@ -1,20 +1,19 @@
 import logging
-from typing import Any, cast
+from typing import cast
 
-import pytest
+from opentelemetry.util.http import ExcludeList
 from starlette.responses import PlainTextResponse
 from starlette.types import ASGIApp
 
 from starlite import get
 from starlite.config import InstrumentationBackend, InstrumentationConfig
-from starlite.middleware.instrumentation.base import InstrumentationMiddleware
 from starlite.middleware.instrumentation.opentelemetry import OpenTelemetryMiddleware
 from starlite.testing import create_test_client
 
 logger = logging.getLogger(__name__)
 
 
-@get(path="/")
+@get(path="/test_path")
 def handler() -> PlainTextResponse:
     return PlainTextResponse("_starlite_" * 4000, status_code=200)
 
@@ -23,5 +22,45 @@ def test_invalid_instrumentation_middleware_backend() -> None:
 
     try:
         create_test_client(route_handlers=[handler], instrumentation_config=InstrumentationConfig(backend="oracle-oem"))
-    except Exception as exc:
-        assert isinstance(exc, ValueError)
+    except Exception as e:
+        assert isinstance(e, ValueError)
+
+
+def test_opentelemetry_backend() -> None:
+
+    client = create_test_client(
+        route_handlers=[handler], instrumentation_config=InstrumentationConfig(backend="opentelemetry")
+    )
+    unpacked_middleware = []
+    cur = client.app.asgi_handler
+    while hasattr(cur, "app"):
+        unpacked_middleware.append(cur)
+        cur = cast(ASGIApp, cur.app)  # type: ignore
+    else:
+        unpacked_middleware.append(cur)
+    assert len(unpacked_middleware) == 2
+    middleware = unpacked_middleware[1].handler  # type: ignore
+    assert isinstance(middleware, OpenTelemetryMiddleware)
+
+
+def test_opentelemetry_backend_excluded_urls() -> None:
+
+    client = create_test_client(
+        route_handlers=[handler],
+        instrumentation_config=InstrumentationConfig(
+            backend=InstrumentationBackend.OPENTELEMETRY, excluded_urls="/test_path"
+        ),
+    )
+    unpacked_middleware = []
+    cur = client.app.asgi_handler
+    while hasattr(cur, "app"):
+        unpacked_middleware.append(cur)
+        cur = cast(ASGIApp, cur.app)  # type: ignore
+    else:
+        unpacked_middleware.append(cur)
+    assert len(unpacked_middleware) == 2
+    middleware = unpacked_middleware[1].handler  # type: ignore
+    assert isinstance(middleware, OpenTelemetryMiddleware)
+    assert isinstance(middleware.excluded_urls, ExcludeList)
+
+    # assert middleware.excluded_urls == parse_excluded_urls("/test_path")
