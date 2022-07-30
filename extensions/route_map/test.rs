379e7113391e1850e2e8f849bdcf5446ea81eddf
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-07-30 12:36:04 +0200

    Rust Bindings: CI (#317)
    * updated ci to handle rust testing
    
    * update python path
    
    * fix coverage
    
    * addressed review comments
    
    * remove flag preventing install when deps are cached

--- extensions/route_map/test.rs
+++ extensions/route_map/test.rs
@@ -3,6 +3,7 @@ use pyo3::prelude::*;
 
 #[test]
 fn init() -> PyResult<()> {
+    pyo3::prepare_freethreaded_python();
     Python::with_gil(|py| -> PyResult<()> {
         let mut route_map = RouteMap::new(py, true.into())?;
         route_map.add_routes(py, vec![])?;
