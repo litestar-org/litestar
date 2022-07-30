58817e1b64694b71f7775a3b36337977f1e98bb4
nramos0 <xoen000@163.com>
2022-07-31 00:39:34 +0800

    reformat python scripts for rust tests

--- extensions/route_map/test/init/init_one_route_with_path.py
+++ extensions/route_map/test/init/init_one_route_with_path.py
@@ -2,9 +2,11 @@ from starlite import Starlite, get
 
 path = "/articles/{id:str}"
 
+
 @get(path=path)
 def handler_fn(id: str) -> None:
     ...
+
 
 app = Starlite(route_handlers=[handler_fn])
 route = app.routes[0]
