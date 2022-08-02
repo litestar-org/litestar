5e47fed2f4ee0d5d5901bd5948d762e6c7573897
Zachary Dremann <dremann@gmail.com>
2022-08-01 20:40:58 -0400

    Normalize paths after joining segments
    This makes `normalize_path` act sanely for an empty path, and avoids the
    possiblity of doubled slashes in the result of `join_paths`

--- starlite/utils/url.py
+++ starlite/utils/url.py
@@ -6,8 +6,6 @@ def normalize_path(path: str) -> str:
     """
     Normalizes a given path by ensuring it starts with a slash and does not end with a slash
     """
-    if path == "":
-        return path
     path = path.rstrip("/")
     if not path.startswith("/"):
         path = "/" + path
@@ -19,8 +17,4 @@ def join_paths(paths: Sequence[str]) -> str:
     """
     Normalizes and joins path fragments
     """
-    normalized_fragments = []
-    for fragment in paths:
-        fragment = normalize_path(fragment)
-        normalized_fragments.append(fragment)
-    return "".join(normalized_fragments)
+    return normalize_path("/".join(paths))
--- tests/utils/test_url.py
+++ tests/utils/test_url.py
@@ -12,16 +12,28 @@ from starlite.utils.url import join_paths, normalize_path
         ("/path/", "sub/", "/path/sub"),
         ("path/", "sub/", "/path/sub"),
         ("path", "sub/", "/path/sub"),
+        ("/", "/root/sub", "/root/sub"),
     ],
 )
 def test_join_url_fragments(base: str, fragment: str, expected: str) -> None:
     assert join_paths([base, fragment]) == expected
 
 
+def test_join_empty_list() -> None:
+    assert join_paths([]) == "/"
+
+
+def test_join_single() -> None:
+    assert join_paths([""]) == "/"
+    assert join_paths(["/"]) == "/"
+    assert join_paths(["root"]) == "/root"
+    assert join_paths(["root//other"]) == "/root/other"
+
+
 @pytest.mark.parametrize(
     "base,expected",
     [
-        ("", ""),
+        ("", "/"),
         ("/path", "/path"),
         ("path/", "/path"),
         ("path", "/path"),
