b3ca34d6314bedbab5047a36c18e52c64b63927f
Zachary Dremann <dremann@gmail.com>
2022-08-02 01:35:21 -0400

    Remove all trailing slashes when normalizing paths (#325)
    `normalize_path` is supposed to make sure the path does not end in a
    slash, but it only removes the last character if it is a slash.
    
    Additionally, replace all strings of 2+ slashes with a single slash in
    one pass, rather than replacing groups of two iteratively
    
    Make `normalize_path` act sanely for an empty path, and avoid the
    possiblity of doubled slashes in the result of `join_paths`

--- tests/utils/test_url.py
+++ tests/utils/test_url.py
@@ -12,12 +12,35 @@ from starlite.utils.url import join_paths, normalize_path
         ("/path/", "sub/", "/path/sub"),
         ("path/", "sub/", "/path/sub"),
         ("path", "sub/", "/path/sub"),
+        ("/", "/root/sub", "/root/sub"),
     ],
 )
 def test_join_url_fragments(base: str, fragment: str, expected: str) -> None:
     assert join_paths([base, fragment]) == expected
 
 
-@pytest.mark.parametrize("base,expected", [("/path", "/path"), ("path/", "/path"), ("path", "/path")])
+def test_join_empty_list() -> None:
+    assert join_paths([]) == "/"
+
+
+def test_join_single() -> None:
+    assert join_paths([""]) == "/"
+    assert join_paths(["/"]) == "/"
+    assert join_paths(["root"]) == "/root"
+    assert join_paths(["root//other"]) == "/root/other"
+
+
+@pytest.mark.parametrize(
+    "base,expected",
+    [
+        ("", "/"),
+        ("/path", "/path"),
+        ("path/", "/path"),
+        ("path", "/path"),
+        ("path////path", "/path/path"),
+        ("path//", "/path"),
+        ("///", "/"),
+    ],
+)
 def test_normalize_path(base: str, expected: str) -> None:
     assert normalize_path(base) == expected
