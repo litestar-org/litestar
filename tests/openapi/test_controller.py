c20715f2dcf2ef898dd7775abca357172e926e99
MobiusXS <57055149+mobiusxs@users.noreply.github.com>
2022-08-12 23:49:21 -0500

    separate root and redoc tests

--- tests/openapi/test_controller.py
+++ tests/openapi/test_controller.py
@@ -1,4 +1,3 @@
-import pytest
 from starlette.status import HTTP_200_OK
 
 from starlite.app import DEFAULT_OPENAPI_CONFIG
@@ -7,10 +6,16 @@ from starlite.testing import create_test_client
 from tests.openapi.utils import PersonController, PetController
 
 
-@pytest.mark.parametrize("url", ["/schema", "/schema/redoc"])
-def test_openapi_redoc(url: str) -> None:
+def test_openapi_root() -> None:
     with create_test_client([PersonController, PetController], openapi_config=DEFAULT_OPENAPI_CONFIG) as client:
-        response = client.get(url)
+        response = client.get("/schema")
+        assert response.status_code == HTTP_200_OK
+        assert response.headers["content-type"].startswith(MediaType.HTML.value)
+
+
+def test_openapi_redoc() -> None:
+    with create_test_client([PersonController, PetController], openapi_config=DEFAULT_OPENAPI_CONFIG) as client:
+        response = client.get("/schema/redoc")
         assert response.status_code == HTTP_200_OK
         assert response.headers["content-type"].startswith(MediaType.HTML.value)
 
