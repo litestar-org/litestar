cd3e669bf8f6fbf7e0489490606ae2f77f4b8330
Peter Schutt <peter.github@proton.me>
2022-08-29 10:16:01 +1000

    GH actions test hello-world example against install with no extras.
    There have been a couple of issue types that this test would have caused in the past:
    
    1. changes to python typing across versions - as the test suite has to support 3.7+ our tests are all typed with a different syntax than would be used in modern applications.
    2. accidental imports of optional dependencies - these aren't caught by the standard test suite as we install `full` extras to run the tests.

--- examples/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import hello_world, startup_and_shutdown
-
-__all__ = [
-    "hello_world",
-    "startup_and_shutdown",
-]
--- examples/tests/test_hello_world.py
+++ examples/tests/test_hello_world.py
@@ -1,11 +1,11 @@
 from starlette.status import HTTP_200_OK
 
-import examples
+from examples import hello_world
 from starlite.testing import TestClient
 
 
-async def test_hello_world_example() -> None:
-    with TestClient(app=examples.hello_world.app) as client:
+def test_hello_world_example() -> None:
+    with TestClient(app=hello_world.app) as client:
         r = client.get("/")
     assert r.status_code == HTTP_200_OK
     assert r.json() == {"hello": "world"}
