6cd6ba379f9959e93f785971bad0a640beac764e
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-09-02 17:20:53 +0200

    Issue #412: add session middleware (#423)
    * initial
    
    * updated deps
    
    * add tests
    
    * cleanup
    
    * fixed tests
    
    * added AD, session validation & expiration
    
    - Timestamp of the ciphertext and max-age of the cookies are now authenticated and time validated
    
    - Older session cookies are now overwritten and expired
    
    Update session.py
    
    * changed session validation condition
    
    - Returns False if the session age is less than equal to the current time
    
    * added constant for AAD
    
    AAD is additional authenticated data
    
    * removed keyerror handling
    
    This exception will not occur as those keys are being obtained from connection.cookies itself.
    
    * added test cases and addressed feedbacks
    
    * fixed linting
    
    * added a test case for AAD
    
    Update params.py
    
    * Update test_session_middleware.py
    
    * Update starlite/middleware/session.py
    
    Co-authored-by: infohash <46137868+infohash@users.noreply.github.com>

---
+++ starlite/middleware/session.py
@@ -0,0 +1,231 @@
+import binascii
+import contextlib
+import time
+from base64 import b64decode, b64encode
+from os import urandom
+from typing import TYPE_CHECKING, Any, Awaitable, Callable, List, Optional
+
+from orjson import OPT_SERIALIZE_NUMPY, dumps
+from orjson.orjson import loads
+from pydantic import (
+    BaseConfig,
+    BaseModel,
+    SecretBytes,
+    conint,
+    conlist,
+    constr,
+    validator,
+)
+from starlette.datastructures import MutableHeaders
+from starlette.requests import HTTPConnection
+from typing_extensions import Literal
+
+from starlite.datastructures import Cookie
+from starlite.exceptions import MissingDependencyException
+from starlite.middleware.base import MiddlewareProtocol
+from starlite.response import Response
+
+try:
+    from cryptography.exceptions import InvalidTag
+    from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+except ImportError as e:
+    raise MissingDependencyException("cryptography is not installed") from e
+
+if TYPE_CHECKING:
+    from starlette.types import ASGIApp, Message, Receive, Scope, Send
+
+ONE_DAY_IN_SECONDS = 60 * 60 * 24
+NONCE_SIZE = 12
+CHUNK_SIZE = 4096 - 64
+AAD = b"additional_authenticated_data="
+
+
+class SessionCookieConfig(BaseModel):
+    """Configuration for Session middleware."""
+
+    class Config(BaseConfig):
+        arbitrary_types_allowed = True
+
+    secret: SecretBytes
+    """
+    A secret key to use for generating an encryption key.
+    Must have a length of 16 (128 bits), 24 (192 bits) or 32 (256 bits) characters.
+    """
+    key: constr(min_length=1, max_length=256) = "session"  # type: ignore[valid-type]
+    """
+    Key to use for the cookie inside the header,
+    e.g. `session=<data>` where 'session' is the cookie key and <data> is the session data.
+
+    Notes:
+        - If a session cookie exceeds 4KB in size it is split. In this case the key will be of the format
+            'session-{segment number}'.
+    """
+    max_age: conint(ge=1) = ONE_DAY_IN_SECONDS * 14  # type: ignore[valid-type]
+    """Maximal age of the cookie before its invalidated."""
+    scopes: conlist(Literal["http", "websocket"], min_items=1, max_items=2) = ["http", "websocket"]  # type: ignore[valid-type]
+    """Scopes for the middleware - options are 'http' and 'websocket' with the default being both"""
+    path: str = "/"
+    """Path fragment that must exist in the request url for the cookie to be valid. Defaults to '/'."""
+    domain: Optional[str] = None
+    """Domain for which the cookie is valid."""
+    secure: bool = False
+    """Https is required for the cookie."""
+    httponly: bool = True
+    """Forbids javascript to access the cookie via 'Document.cookie'."""
+    samesite: Literal["lax", "strict", "none"] = "lax"
+    """Controls whether or not a cookie is sent with cross-site requests. Defaults to 'lax'."""
+
+    @validator("secret", always=True)
+    def validate_secret(cls, value: SecretBytes) -> SecretBytes:  # pylint: disable=no-self-argument
+        """Ensures that the 'secret' value is 128, 192 or 256 bits.
+
+        Args:
+            value: A bytes string.
+
+        Raises:
+            ValueError: if the bytes string is of incorrect length.
+
+        Returns:
+            A bytes string.
+        """
+        if len(value.get_secret_value()) not in [16, 24, 32]:
+            raise ValueError("secret length must be 16 (128 bit), 24 (192 bit) or 32 (256 bit)")
+        return value
+
+
+class SessionMiddleware(MiddlewareProtocol):
+    def __init__(
+        self,
+        app: "ASGIApp",
+        config: SessionCookieConfig,
+    ):
+        """Starlite SessionMiddleware.
+
+        Args:
+            app: The 'next' ASGI app to call.
+            config: SessionCookieConfig instance.
+        """
+        super().__init__(app)
+        self.app = app
+        self.config = config
+        self.aesgcm = AESGCM(config.secret.get_secret_value())
+
+    def dump_data(self, data: Any) -> List[bytes]:
+        """Given orjson serializable data, including pydantic models and numpy
+        types, dump it into a bytes string, encrypt, encode and split it into
+        chunks of the desirable size.
+
+        Args:
+            data: Data to serialize, encrypt, encode and chunk.
+
+        Notes:
+            - The returned list is composed of a chunks of a single base64 encoded
+                string that is encrypted using AES-CGM.
+
+        Returns:
+            List of encoded bytes string of a maximum length equal to the 'CHUNK_SIZE' constant.
+        """
+        serialized = dumps(data, default=Response.serializer, option=OPT_SERIALIZE_NUMPY)
+        associated_data = dumps({"expires_at": round(time.time()) + self.config.max_age})
+        nonce = urandom(NONCE_SIZE)
+        encrypted = self.aesgcm.encrypt(nonce, serialized, associated_data=associated_data)
+        encoded = b64encode(nonce + encrypted + AAD + associated_data)
+        return [encoded[i : i + CHUNK_SIZE] for i in range(0, len(encoded), CHUNK_SIZE)]
+
+    def load_data(self, data: List[bytes]) -> Any:
+        """Given a list of strings, decodes them into the session object.
+
+        Args:
+            data: A list of strings derived from the request's session cookie(s).
+
+        Returns:
+            A deserialized session value.
+        """
+        decoded = b64decode(b"".join(data))
+        nonce = decoded[:NONCE_SIZE]
+
+        associated_data = None
+        aad_starts_from = decoded.find(AAD)
+        if aad_starts_from != -1:
+            associated_data = decoded[aad_starts_from:].replace(AAD, b"")
+
+        encrypted_session = decoded[NONCE_SIZE:aad_starts_from]
+        decrypted = self.aesgcm.decrypt(nonce, encrypted_session, associated_data=associated_data)
+
+        session_validation = loads(associated_data)["expires_at"] > round(time.time())
+        return loads(decrypted) if session_validation else {}
+
+    def create_send_wrapper(
+        self, scope: "Scope", send: "Send", cookie_keys: List[str]
+    ) -> Callable[["Message"], Awaitable[None]]:
+        """
+        Creates a wrapper for the ASGI send function, which handles setting the cookies on the outgoing response.
+        Args:
+            scope: The ASGI connection scope.
+            send: The ASGI send function.
+            cookie_keys: Session cookie keys that are sent in the current request. It is required to expire all session
+                cookies from the current request and are replaced with new cookies in the upcoming response.
+
+        Returns:
+            None.
+        """
+
+        async def wrapped_send(message: "Message") -> None:
+            """A wrapper around the send function, declared in local scope to
+            use closure values.
+
+            Args:
+                message: An ASGI message.
+
+            Returns:
+                None
+            """
+            if message["type"] == "http.response.start":
+                headers = MutableHeaders(scope=message)
+                data = self.dump_data(scope.get("session"))
+                cookie_params = self.config.dict(exclude_none=True, exclude={"secret", "key"})
+                for i, datum in enumerate(data, start=0):
+                    headers.append(
+                        "Set-Cookie",
+                        Cookie(value=datum.decode("utf-8"), key=f"{self.config.key}-{i}", **cookie_params).to_header(
+                            header=""
+                        ),
+                    )
+                # Cookies with the same key overwrite the earlier cookie with that key. To expire earlier session
+                # cookies, first check how many session cookies will not be overwritten in this upcoming response. If
+                # leftover cookies are greater than or equal to 1, that means older session cookies have to be expired
+                # and their names are in cookie_keys.
+                cookies_left = len(cookie_keys) - len(data)
+                if cookies_left >= 1:
+                    cookie_params = self.config.dict(exclude_none=True, exclude={"secret", "max_age", "key"})
+                    for cookie_key in cookie_keys[len(data) :]:
+                        headers.append(
+                            "Set-Cookie",
+                            Cookie(value="null", key=cookie_key, expires=0, **cookie_params).to_header(header=""),
+                        )
+            await send(message)
+
+        return wrapped_send
+
+    async def __call__(self, scope: "Scope", receive: "Receive", send: "Send") -> None:
+        """
+        Args:
+            scope: The ASGI connection scope.
+            receive: The ASGI receive function.
+            send: The ASGI send function.
+
+        Returns:
+            None
+        """
+        if scope["type"] in self.config.scopes:
+            scope.setdefault("session", {})
+            connection = HTTPConnection(scope)
+            cookie_keys = sorted(key for key in connection.cookies if self.config.key in key)
+            if cookie_keys:
+                data = [connection.cookies[key].encode("utf-8") for key in cookie_keys]
+                # If these exceptions occur, the session must remain empty so do nothing.
+                with contextlib.suppress(InvalidTag, binascii.Error):
+                    scope["session"] = self.load_data(data)
+            await self.app(scope, receive, self.create_send_wrapper(scope, send, cookie_keys))
+        else:
+            await self.app(scope, receive, send)
---
+++ tests/middleware/test_session_middleware.py
@@ -0,0 +1,166 @@
+import secrets
+import time
+from base64 import b64decode, b64encode
+from os import urandom
+from typing import TYPE_CHECKING, Dict
+from unittest import mock
+
+import pytest
+from cryptography.exceptions import InvalidTag
+from orjson import dumps
+from pydantic import SecretBytes, ValidationError
+
+from starlite import DefineMiddleware, Request, get
+from starlite.middleware.session import (
+    AAD,
+    CHUNK_SIZE,
+    SessionCookieConfig,
+    SessionMiddleware,
+)
+from starlite.testing import create_test_client
+
+if TYPE_CHECKING:
+    from starlette.types import Receive, Scope, Send
+
+TEST_SECRET = SecretBytes(urandom(16))
+
+
+async def mock_asgi_app(scope: "Scope", receive: "Receive", send: "Send") -> None:
+    pass
+
+
+@pytest.mark.parametrize(
+    "secret, should_raise",
+    [
+        [urandom(16), False],
+        [urandom(24), False],
+        [urandom(32), False],
+        [urandom(17), True],
+        [urandom(4), True],
+        [urandom(100), True],
+        [b"", True],
+    ],
+)
+def test_config_validation(secret: bytes, should_raise: bool) -> None:
+    if should_raise:
+        with pytest.raises(ValidationError):
+            SessionCookieConfig(secret=SecretBytes(secret))
+    else:
+        SessionCookieConfig(secret=SecretBytes(secret))
+
+
+@pytest.fixture()
+def session_middleware() -> SessionMiddleware:
+    return SessionMiddleware(app=mock_asgi_app, config=SessionCookieConfig(secret=TEST_SECRET))
+
+
+def create_session(size: int = 16) -> Dict[str, str]:
+    return {"key": secrets.token_hex(size)}
+
+
+@pytest.mark.parametrize("session", [create_session(), create_session(size=4096)])
+def test_dump_and_load_data(session: dict, session_middleware: SessionMiddleware) -> None:
+    ciphertext = session_middleware.dump_data(session)
+    assert isinstance(ciphertext, list)
+
+    for text in ciphertext:
+        assert len(text) <= CHUNK_SIZE
+
+    plain_text = session_middleware.load_data(ciphertext)
+    assert plain_text == session
+
+
+@mock.patch("time.time", return_value=round(time.time()))
+def test_load_data_should_return_empty_if_session_expired(
+    time_mock: mock.MagicMock, session_middleware: SessionMiddleware
+) -> None:
+    """Should return empty dict if session is expired."""
+    ciphertext = session_middleware.dump_data(create_session())
+    time_mock.return_value = round(time.time()) + session_middleware.config.max_age + 1
+    plaintext = session_middleware.load_data(data=ciphertext)
+    assert plaintext == {}
+
+
+def test_set_session_cookies() -> None:
+    """Should set session cookies from session in response."""
+    chunks_multiplier = 2
+
+    @get(path="/test")
+    def handler(request: Request) -> None:
+        # Create large session by keeping it multiple of CHUNK_SIZE. This will split the session into multiple cookies.
+        # Then you only need to check if number of cookies set are more than the multiplying number.
+        request.session.update(create_session(size=CHUNK_SIZE * chunks_multiplier))
+
+    client = create_test_client(
+        route_handlers=[handler],
+        middleware=[DefineMiddleware(SessionMiddleware, config=SessionCookieConfig(secret=TEST_SECRET))],
+    )
+
+    response = client.get("/test")
+    assert len(response.cookies) > chunks_multiplier
+    # If it works for the multiple chunks of session, it works for the single chunk too. So, just check if "session-0"
+    # exists.
+    assert "session-0" in response.cookies
+
+
+@pytest.mark.parametrize("mutate", [False, True])
+def test_load_session_cookies_and_expire_previous(mutate: bool, session_middleware: SessionMiddleware) -> None:
+    """Should load session cookies into session from request and overwrite the
+    previously set cookies with the upcoming response.
+
+    Session cookies from the previous session should not persist because
+    session is mutable. Once the session is loaded from the cookies,
+    those cookies are redundant. The response sets new session cookies
+    overwriting or expiring the previous ones.
+    """
+    # Test for large session data. If it works for multiple cookies, it works for single also.
+    _session = create_session(size=4096)
+
+    @get(path="/test")
+    def handler(request: Request) -> dict:
+        nonlocal _session
+        if mutate:
+            # Modify the session, this will overwrite the previously set session cookies.
+            request.session.update(create_session())
+            _session = request.session
+        return request.session
+
+    ciphertext = session_middleware.dump_data(_session)
+
+    client = create_test_client(
+        route_handlers=[handler],
+        middleware=[DefineMiddleware(SessionMiddleware, config=SessionCookieConfig(secret=TEST_SECRET))],
+    )
+
+    response = client.get(
+        "/test",
+        cookies={
+            f"{session_middleware.config.key}-{i}": text.decode("utf-8") for i, text in enumerate(ciphertext, start=0)
+        },
+    )
+
+    assert response.json() == _session
+    # The session cookie names that were in the request will also be present in its response to overwrite or to expire
+    # them. So, the number of cookies in the response will be at least equal to or greater than the number of cookies
+    # that were in the request.
+    assert response.headers["set-cookie"].count("session") >= response.request.headers["Cookie"].count("session")
+
+
+def test_load_data_should_raise_invalid_tag_if_tampered_aad(session_middleware: SessionMiddleware) -> None:
+    """If AAD has been tampered with, the integrity of the data cannot be
+    verified and InavlidTag exception is raised."""
+    encrypted_session = session_middleware.dump_data(create_session())
+    # The attacker will tamper with the AAD to increase the expiry time of the cookie.
+    attacker_chosen_time = 300  # In seconds
+    fraudulent_associated_data = dumps(
+        {"expires_at": round(time.time()) + session_middleware.config.max_age + attacker_chosen_time}
+    )
+    decoded = b64decode(b"".join(encrypted_session))
+    aad_starts_from = decoded.find(AAD)
+    # The attacker removes the original AAD and attaches its own.
+    ciphertext = b64encode(decoded[:aad_starts_from] + AAD + fraudulent_associated_data)
+    # The attacker puts the data back to its original form.
+    encoded = [ciphertext[i : i + CHUNK_SIZE] for i in range(0, len(ciphertext), CHUNK_SIZE)]
+
+    with pytest.raises(InvalidTag):
+        session_middleware.load_data(encoded)
