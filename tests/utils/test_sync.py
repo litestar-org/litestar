0c00c1398c596150cbe84457fbc1bb4a45d2c2bc
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-09-04 18:29:08 +0200

    simplified implementation

--- tests/utils/test_sync.py
+++ tests/utils/test_sync.py
@@ -1,7 +1,7 @@
-from starlite.utils import CallableWrapper
+from starlite.utils.sync import AsyncCallable
 
 
-def test_function_wrapper_wraps_method_correctly() -> None:
+async def test_function_wrapper_wraps_method_correctly() -> None:
     class MyClass:
         def __init__(self) -> None:
             self.value = 0
@@ -11,12 +11,12 @@ def test_function_wrapper_wraps_method_correctly() -> None:
 
     instance = MyClass()
 
-    wrapped_method = CallableWrapper(instance.my_method)
+    wrapped_method = AsyncCallable(instance.my_method)
 
-    wrapped_method(1)
+    await wrapped_method(1)
     assert instance.value == 1
 
-    wrapped_method(value=10)
+    await wrapped_method(value=10)
     assert instance.value == 10
 
 
@@ -30,27 +30,27 @@ async def test_function_wrapper_wraps_async_method_correctly() -> None:
 
     instance = MyClass()
 
-    wrapped_method = CallableWrapper(instance.my_method)
+    wrapped_method = AsyncCallable(instance.my_method)
 
-    await wrapped_method(1)
+    await wrapped_method(1)  # type: ignore
     assert instance.value == 1
 
-    await wrapped_method(value=10)
+    await wrapped_method(value=10)  # type: ignore
     assert instance.value == 10
 
 
-def test_function_wrapper_wraps_function_correctly() -> None:
+async def test_function_wrapper_wraps_function_correctly() -> None:
     obj = {"value": 0}
 
     def my_function(new_value: int) -> None:
         obj["value"] = new_value
 
-    wrapped_function = CallableWrapper(my_function)
+    wrapped_function = AsyncCallable(my_function)
 
-    wrapped_function(1)
+    await wrapped_function(1)
     assert obj["value"] == 1
 
-    wrapped_function(new_value=10)
+    await wrapped_function(new_value=10)
     assert obj["value"] == 10
 
 
@@ -60,16 +60,16 @@ async def test_function_wrapper_wraps_async_function_correctly() -> None:
     async def my_function(new_value: int) -> None:
         obj["value"] = new_value
 
-    wrapped_function = CallableWrapper(my_function)
+    wrapped_function = AsyncCallable(my_function)
 
-    await wrapped_function(1)
+    await wrapped_function(1)  # type: ignore
     assert obj["value"] == 1
 
-    await wrapped_function(new_value=10)
+    await wrapped_function(new_value=10)  # type: ignore
     assert obj["value"] == 10
 
 
-def test_function_wrapper_wraps_class_correctly() -> None:
+async def test_function_wrapper_wraps_class_correctly() -> None:
     class MyCallable:
         value = 0
 
@@ -78,12 +78,12 @@ def test_function_wrapper_wraps_class_correctly() -> None:
 
     instance = MyCallable()
 
-    wrapped_class = CallableWrapper(instance)
+    wrapped_class = AsyncCallable(instance)
 
-    wrapped_class(1)
+    await wrapped_class(1)
     assert instance.value == 1
 
-    wrapped_class(new_value=10)
+    await wrapped_class(new_value=10)
     assert instance.value == 10
 
 
@@ -96,10 +96,10 @@ async def test_function_wrapper_wraps_async_class_correctly() -> None:
 
     instance = MyCallable()
 
-    wrapped_class = CallableWrapper(instance)
+    wrapped_class = AsyncCallable(instance)
 
-    await wrapped_class(1)
+    await wrapped_class(1)  # type: ignore
     assert instance.value == 1
 
-    await wrapped_class(new_value=10)
+    await wrapped_class(new_value=10)  # type: ignore
     assert instance.value == 10
