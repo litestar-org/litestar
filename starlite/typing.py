e865878ce63c9f6b6737aeec8c7e0b89da1e36f3
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-09-07 21:57:39 +0200

    initial

--- starlite/typing.py
+++ starlite/typing.py
@@ -1,3 +1,7 @@
+import dataclasses
+from dataclasses import _MISSING_TYPE
+from dataclasses import Field as DataclassField
+from dataclasses import is_dataclass
 from typing import (
     Any,
     Dict,
@@ -6,11 +10,13 @@ from typing import (
     Tuple,
     Type,
     TypeVar,
+    Union,
     cast,
     get_type_hints,
 )
 
 from pydantic import BaseModel, create_model
+from pydantic_factories.protocols import DataclassProtocol
 
 from starlite.exceptions import ImproperlyConfiguredException
 from starlite.utils import is_class_and_subclass
@@ -21,7 +27,7 @@ try:
 except ImportError:  # pragma: no cover
     from typing import _GenericAlias as GenericAlias  # type: ignore
 
-T = TypeVar("T", bound=BaseModel)
+T = TypeVar("T", bound=Union[BaseModel, DataclassProtocol])
 
 
 class Partial(Generic[T]):
@@ -35,21 +41,42 @@ class Partial(Generic[T]):
 
     def __class_getitem__(cls, item: Type[T]) -> Type[T]:
         """Modifies a given T subclass of BaseModel to be all optional."""
-        if not is_class_and_subclass(item, BaseModel):
-            raise ImproperlyConfiguredException(f"Partial[{item}] must be a subclass of BaseModel")
+        if not is_class_and_subclass(item, BaseModel) and not is_dataclass(item):
+            raise ImproperlyConfiguredException("Unsupported value type passed as the generic argument T to Partial[T]")
         if not cls._models.get(item):
-            field_definitions: Dict[str, Tuple[Any, None]] = {}
-            # traverse the object's mro and get all annotations
-            # until we find a BaseModel.
-            for obj in item.mro():
-                if issubclass(obj, BaseModel):
-                    for field_name, field_type in get_type_hints(obj).items():
-                        # we modify the field annotations to make it optional
-                        if not isinstance(field_type, GenericAlias) or type(None) not in field_type.__args__:
-                            field_definitions[field_name] = (Optional[field_type], None)
-                        else:
-                            field_definitions[field_name] = (field_type, None)
-                else:
-                    break
-            cls._models[item] = create_model(f"Partial{item.__name__}", **field_definitions)  # type: ignore
+            if issubclass(item, BaseModel):
+                cls._create_partial_pydantic_model(item=item)
+            else:
+                cls._create_partial_dataclass(item=item)
         return cast("Type[T]", cls._models.get(item))
+
+    @classmethod
+    def _create_partial_pydantic_model(cls, item: Type[BaseModel]) -> None:
+        field_definitions: Dict[str, Tuple[Any, None]] = {}
+        for field_name, field_type in get_type_hints(item).items():
+            # we modify the field annotations to make it optional
+            if not isinstance(field_type, GenericAlias) or type(None) not in field_type.__args__:
+                field_definitions[field_name] = (Optional[field_type], None)
+            else:
+                field_definitions[field_name] = (field_type, None)
+        cls._models[item] = create_model(f"Partial{item.__name__}", **field_definitions)  # type: ignore
+
+    @classmethod
+    def _create_partial_dataclass(cls, item: Type[DataclassProtocol]) -> None:
+        fields: Dict[str, DataclassField] = {}
+        for field_name, dataclass_field in item.__dataclass_fields__.items():
+            if not isinstance(dataclass_field.type, GenericAlias) or type(None) not in dataclass_field.type.__args__:
+                dataclass_field.type = Optional[dataclass_field.type]
+            if type(dataclass_field.default_factory) is _MISSING_TYPE:
+                dataclass_field.default = (
+                    None if type(dataclass_field.default) is _MISSING_TYPE else dataclass_field.default
+                )
+            dataclass_field.kw_only = True
+            fields[field_name] = dataclass_field
+        partial_type = dataclasses.dataclass(type(f"Partial{item.__name__}", (item,), {"__dataclass_fields__": fields}))
+        for field_name, annotation in item.__annotations__.items():
+            if not isinstance(annotation, GenericAlias) or type(None) not in annotation.__args__:
+                partial_type.__annotations__[field_name] = Optional[annotation]
+            else:
+                partial_type.__annotations__[field_name] = annotation
+        cls._models[item] = partial_type
--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,11 +1,11 @@
-from typing import Optional
+from typing import Any, Optional
 
 import pytest
 from pydantic import BaseModel
 
 from starlite.exceptions import ImproperlyConfiguredException
 from starlite.typing import Partial
-from tests import Person
+from tests import Person, PydanticDataClassPerson, VanillaDataClassPerson
 
 try:
     from typing import _UnionGenericAlias as GenericAlias  # type: ignore
@@ -13,18 +13,21 @@ except ImportError:
     from typing import _GenericAlias as GenericAlias  # type: ignore
 
 
-def test_partial() -> None:
+def test_partial_pydantic_model() -> None:
     partial = Partial[Person]
-    assert partial
+
+    assert partial.__fields__  # type: ignore
+
     for field in partial.__fields__.values():  # type: ignore
         assert field.allow_none
         assert not field.required
+
     for field in partial.__annotations__.values():
         assert isinstance(field, GenericAlias)
         assert type(None) in field.__args__
 
 
-def test_partial_superclass() -> None:
+def test_partial_pydantic_model_with_superclass() -> None:
     """Test that Partial returns the correct annotations for nested models."""
 
     class Parent(BaseModel):
@@ -38,21 +41,26 @@ def test_partial_superclass() -> None:
     for field in partial_child.__fields__.values():  # type: ignore
         assert field.allow_none
         assert not field.required
+
     assert partial_child.__annotations__ == {
         "foo": Optional[int],
         "bar": Optional[int],
     }
 
 
-def test_partial_basemodel() -> None:
+class Foo:
+    bar: int
+
+
+@pytest.mark.parametrize(
+    "cls, should_raise",
+    [(Foo, True), (Person, False), (VanillaDataClassPerson, False), (PydanticDataClassPerson, False)],
+)
+def test_validation(cls: Any, should_raise: bool) -> None:
     """Test that Partial returns no annotations for classes that don't inherit
     from BaseModel."""
-
-    class Foo:
-        bar: int
-
-    # The type checker will raise a warning that Foo is not a BaseModel,
-    # but we want to test for runtime behaviour in case someone passes in
-    # a class that doesn't inherit from BaseModel anyway.
-    with pytest.raises(ImproperlyConfiguredException):
-        Partial[Foo]  # type: ignore
+    if should_raise:
+        with pytest.raises(ImproperlyConfiguredException):
+            Partial[cls]()  # type: ignore
+    else:
+        Partial[cls]()  # type: ignore
