463a0fdeafdda204bd2eb48474817eb120721f36
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-09-22 22:36:31 +0200

    Cleanup logging config and fix default handlers (#509)
    housekeeping

--- tests/logging_config/test_logging.py
+++ tests/logging_config/test_logging.py
@@ -3,6 +3,7 @@ from typing import TYPE_CHECKING
 from unittest.mock import Mock, patch
 
 from starlite import Starlite
+from starlite.config.logging import default_handlers
 from starlite.logging import LoggingConfig
 from starlite.testing import TestClient, create_test_client
 
@@ -12,7 +13,7 @@ if TYPE_CHECKING:
 
 @patch("logging.config.dictConfig")
 def test_logging_debug(dict_config_mock: Mock) -> None:
-    config = LoggingConfig()
+    config = LoggingConfig(handlers=default_handlers)
     config.configure()
     assert dict_config_mock.mock_calls[0][1][0]["loggers"]["starlite"]["level"] == "INFO"
     dict_config_mock.reset_mock()
@@ -20,7 +21,7 @@ def test_logging_debug(dict_config_mock: Mock) -> None:
 
 @patch("logging.config.dictConfig")
 def test_logging_startup(dict_config_mock: Mock) -> None:
-    logger = LoggingConfig(loggers={"app": {"level": "INFO", "handlers": ["console"]}})
+    logger = LoggingConfig(handlers=default_handlers, loggers={"app": {"level": "INFO", "handlers": ["console"]}})
     with create_test_client([], on_startup=[logger.configure]):
         assert dict_config_mock.called
 
@@ -38,9 +39,9 @@ def test_queue_logger(caplog: "LogCaptureFixture") -> None:
 
 
 def test_logger_startup(caplog: "LogCaptureFixture") -> None:
-    with TestClient(app=Starlite(route_handlers=[], on_startup=[LoggingConfig().configure])) as client, caplog.at_level(
-        logging.INFO
-    ):
+    with TestClient(
+        app=Starlite(route_handlers=[], on_startup=[LoggingConfig(handlers=default_handlers).configure])
+    ) as client, caplog.at_level(logging.INFO):
         client.options("/")
         logger = logging.getLogger()
         handlers = logger.handlers
