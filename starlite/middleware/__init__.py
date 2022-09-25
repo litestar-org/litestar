30b6610820fbd8f7bc5b8c9813be882659568607
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-09-25 21:42:37 +0200

    update docstrings

--- starlite/middleware/__init__.py
+++ starlite/middleware/__init__.py
@@ -6,6 +6,7 @@ from starlite.middleware.base import DefineMiddleware, MiddlewareProtocol
 from starlite.middleware.compression import CompressionMiddleware
 from starlite.middleware.csrf import CSRFMiddleware
 from starlite.middleware.exceptions import ExceptionHandlerMiddleware
+from starlite.middleware.logging import LoggingMiddleware, LoggingMiddlewareConfig
 from starlite.middleware.rate_limit import RateLimitConfig, RateLimitMiddleware
 
 __all__ = [
@@ -15,6 +16,8 @@ __all__ = [
     "CompressionMiddleware",
     "DefineMiddleware",
     "ExceptionHandlerMiddleware",
+    "LoggingMiddleware",
+    "LoggingMiddlewareConfig",
     "MiddlewareProtocol",
     "RateLimitConfig",
     "RateLimitMiddleware",
--- starlite/middleware/logging.py
+++ starlite/middleware/logging.py
@@ -1,13 +1,14 @@
 import re
 from collections import OrderedDict
 from inspect import isawaitable
-from typing import TYPE_CHECKING, Any, Iterable, List, Optional, Set, Union
+from typing import TYPE_CHECKING, Any, Iterable, List, Optional, Set, Type, Union
 
 from orjson import OPT_OMIT_MICROSECONDS, OPT_SERIALIZE_NUMPY, dumps
 from pydantic import BaseModel
 
-from starlite import HttpMethod, Request, ScopeType
-from starlite.middleware.base import MiddlewareProtocol
+from starlite.connection import Request
+from starlite.enums import HttpMethod, ScopeType
+from starlite.middleware.base import DefineMiddleware, MiddlewareProtocol
 from starlite.utils import default_serializer, get_serializer_from_scope
 from starlite.utils.extractors import (
     ConnectionDataExtractor,
@@ -26,74 +27,6 @@ try:
 except ImportError:
     BoundLoggerLazyProxy = object  # type: ignore
     structlog_installed = False  # pylint: disable=invalid-name
-
-
-class LoggingMiddlewareConfig(BaseModel):
-    exclude: Optional[Union[str, List[str]]] = None
-    """
-    List of paths to exclude from logging.
-    """
-    logger_name: str = "starlite"
-    """
-    Name of the logger to retrieve using `app.get_logger("<name>")`.
-    """
-    request_cookie_keys_to_obfuscate: Set[str] = {"session"}
-    """
-    Request cookie keys to obfuscate. Obfuscated values are replaced with '*****' (max 10 char length).
-    """
-    request_headers_to_obfuscate: Set[str] = {"Authorization", "X-API-KEY"}
-    """
-    Request header keys to obfuscate. Obfuscated values are replaced with '*****' (max 10 char length).
-    """
-    response_cookie_keys_to_obfuscate: Set[str] = {"session"}
-    """
-    Response cookie keys to obfuscate. Obfuscated values are replaced with '*****' (max 10 char length).
-    """
-    response_headers_to_obfuscate: Set[str] = {"Authorization", "X-API-KEY"}
-    """
-    Response header keys to obfuscate. Obfuscated values are replaced with '*****' (max 10 char length).
-    """
-    request_log_message: str = "HTTP Request"
-    """
-    Log message to prepend when logging a request.
-    """
-    response_log_message: str = "HTTP Response"
-    """
-    Log message to prepend when logging a response.
-    """
-    request_log_fields: Iterable[RequestExtractorField] = (
-        "path",
-        "method",
-        "content_type",
-        "headers",
-        "cookies",
-        "query",
-        "path_params",
-        "body",
-    )
-    """
-    Fields to extract and log from the request.
-
-    Notes:
-        -  The order of fields in the iterable determines the order of the log message logged out.
-            Thus, re-arranging the log-message is as simple as changing the iterable.
-        -  To turn off logging of requests, use and empty iterable.
-
-    """
-    response_log_fields: Iterable[ResponseExtractorField] = (
-        "status_code",
-        "cookies",
-        "headers",
-        "body",
-    )
-    """
-    Fields to extract and log from the response. The order of fields in the iterable determines the order of the log message logged out.
-
-    Notes:
-        -  The order of fields in the iterable determines the order of the log message logged out.
-            Thus, re-arranging the log-message is as simple as changing the iterable.
-        -  To turn off logging of responses, use and empty iterable.
-    """
 
 
 class LoggingMiddleware(MiddlewareProtocol):
@@ -268,3 +201,108 @@ class LoggingMiddleware(MiddlewareProtocol):
             await send(message)
 
         return send_wrapper
+
+
+class LoggingMiddlewareConfig(BaseModel):
+    exclude: Optional[Union[str, List[str]]] = None
+    """
+    List of paths to exclude from logging.
+    """
+    logger_name: str = "starlite"
+    """
+    Name of the logger to retrieve using `app.get_logger("<name>")`.
+    """
+    request_cookie_keys_to_obfuscate: Set[str] = {"session"}
+    """
+    Request cookie keys to obfuscate. Obfuscated values are replaced with '*****' (max 10 char length).
+    """
+    request_headers_to_obfuscate: Set[str] = {"Authorization", "X-API-KEY"}
+    """
+    Request header keys to obfuscate. Obfuscated values are replaced with '*****' (max 10 char length).
+    """
+    response_cookie_keys_to_obfuscate: Set[str] = {"session"}
+    """
+    Response cookie keys to obfuscate. Obfuscated values are replaced with '*****' (max 10 char length).
+    """
+    response_headers_to_obfuscate: Set[str] = {"Authorization", "X-API-KEY"}
+    """
+    Response header keys to obfuscate. Obfuscated values are replaced with '*****' (max 10 char length).
+    """
+    request_log_message: str = "HTTP Request"
+    """
+    Log message to prepend when logging a request.
+    """
+    response_log_message: str = "HTTP Response"
+    """
+    Log message to prepend when logging a response.
+    """
+    request_log_fields: Iterable[RequestExtractorField] = (
+        "path",
+        "method",
+        "content_type",
+        "headers",
+        "cookies",
+        "query",
+        "path_params",
+        "body",
+    )
+    """
+    Fields to extract and log from the request.
+
+    Notes:
+        -  The order of fields in the iterable determines the order of the log message logged out.
+            Thus, re-arranging the log-message is as simple as changing the iterable.
+        -  To turn off logging of requests, use and empty iterable.
+
+    """
+    response_log_fields: Iterable[ResponseExtractorField] = (
+        "status_code",
+        "cookies",
+        "headers",
+        "body",
+    )
+    """
+    Fields to extract and log from the response. The order of fields in the iterable determines the order of the log message logged out.
+
+    Notes:
+        -  The order of fields in the iterable determines the order of the log message logged out.
+            Thus, re-arranging the log-message is as simple as changing the iterable.
+        -  To turn off logging of responses, use and empty iterable.
+    """
+    middleware_class: Type[LoggingMiddleware] = LoggingMiddleware
+    """
+    Middleware class to use. Should be a subclass of [starlite.middleware.LoggingMiddleware].
+    """
+
+    @property
+    def middleware(self) -> DefineMiddleware:
+        """Use this property to insert the config into a middleware list on one
+        of the application layers.
+
+        Examples:
+
+            ```python
+            from starlite import Starlite, Request, LoggingConfig, get
+            from starlite.middleware.logging import LoggingMiddlewareConfig
+
+            logging_config = LoggingConfig()
+
+            logging_middleware_config = LoggingMiddlewareConfig()
+
+
+            @get("/")
+            def my_handler(request: Request) -> None:
+                ...
+
+
+            app = Starlite(
+                route_handlers=[my_handler],
+                logging_config=logging_config,
+                middleware=[logging_middleware_config.middleware],
+            )
+            ```
+
+        Returns:
+            An instance of DefineMiddleware including 'self' as the config kwarg value.
+        """
+        return DefineMiddleware(self.middleware_class, config=self)
