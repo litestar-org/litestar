1c0269fc2ac1d840af0d1d1357b5a3fefaeadcb5
Jon Daly <jonathan.daly@sensynehealth.com>
2022-09-28 18:49:26 +0100

    Move constant from class to module

--- starlite/connection/websocket.py
+++ starlite/connection/websocket.py
@@ -43,6 +43,8 @@ if TYPE_CHECKING:
         WebSocketSendEvent,
     )
 
+DISCONNECT_MESSAGE = "connection is disconnected"
+
 
 class WebSocket(
     Generic[User, Auth],
@@ -51,8 +53,6 @@ class WebSocket(
     __slots__ = ("connection_state",)
 
     scope: "WebSocketScope"
-
-    DISCONNECT_MESSAGE = "connection is disconnected"
 
     def __init__(self, scope: "Scope", receive: "Receive" = empty_receive, send: "Send" = empty_send) -> None:
         """The Starlite WebSocket class.
@@ -77,7 +77,7 @@ class WebSocket(
 
         async def wrapped_receive() -> "ReceiveMessage":
             if self.connection_state == "disconnect":
-                raise WebSocketException(detail=self.DISCONNECT_MESSAGE)
+                raise WebSocketException(detail=DISCONNECT_MESSAGE)
             message = await receive()
             if message["type"] == "websocket.connect":
                 self.connection_state = "connect"
@@ -101,7 +101,7 @@ class WebSocket(
 
         async def wrapped_send(message: "Message") -> None:
             if self.connection_state == "disconnect":
-                raise WebSocketException(detail=self.DISCONNECT_MESSAGE)
+                raise WebSocketException(detail=DISCONNECT_MESSAGE)  # pragma: no cover
             await send(message)
 
         return wrapped_send
@@ -189,7 +189,7 @@ class WebSocket(
         if event["type"] == "websocket.disconnect":
             raise WebSocketException(detail="disconnect event", code=event["code"])
         if self.connection_state == "disconnect":
-            raise WebSocketException(detail=self.DISCONNECT_MESSAGE)
+            raise WebSocketException(detail=DISCONNECT_MESSAGE)
         return event.get("text") or "" if mode == "text" else event.get("bytes") or b""
 
     async def receive_text(self) -> str:
