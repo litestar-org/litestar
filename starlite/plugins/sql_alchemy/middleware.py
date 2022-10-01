1b391a46158902a20cece660b8a22605503f2834
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-10-01 21:32:21 +0200

    updated

--- starlite/plugins/sql_alchemy/middleware.py
+++ starlite/plugins/sql_alchemy/middleware.py
@@ -1,12 +1,14 @@
-from typing import TYPE_CHECKING, Any, Type, Union
+from typing import TYPE_CHECKING, Optional, Union, cast
+
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from starlite.middleware.base import MiddlewareProtocol
 
 if TYPE_CHECKING:
-    from sqlalchemy.ext.asyncio import AsyncSession
+
     from sqlalchemy.orm import Session
 
-    from starlite.types import ASGIApp, Receive, Scope, Send
+    from starlite.types import ASGIApp, Message, Receive, Scope, Send
 
 
 class SQLAlchemySessionMiddleware(MiddlewareProtocol):
@@ -15,27 +17,31 @@ class SQLAlchemySessionMiddleware(MiddlewareProtocol):
     def __init__(
         self,
         app: "ASGIApp",
-        engine_app_state_key: str,
         session_scope_key: str,
-        session_class: Union[Type["Session"], Type["AsyncSession"]],
-        **kwargs: Any
     ):
         """
 
         Args:
             app: The 'next' ASGI app to call.
-            engine_app_state_key: Key from which to retrieve the DB engine.
-            session_scope_key: Key under which to store the DB session created for the request.
-            session_class: SQLAlchemy Session class to use.
-            **kwargs: Any configuration to pass to the session class.
+            session_scope_key: Key under which a session is potentially stored.
         """
         self.app = app
         self.session_scope_key = session_scope_key
-        self.engine_app_state_key = engine_app_state_key
-        self.session_class = session_class
-        self.kwargs = kwargs
 
     async def __call__(self, scope: "Scope", receive: "Receive", send: "Send") -> None:
-        engine = scope["app"].state[self.engine_app_state_key]
-        scope[self.session_scope_key] = self.session_class(engine, **self.kwargs)  # type: ignore
-        await self.app(scope, receive, send)
+        async def send_wrapper(message: "Message") -> None:
+            session = cast("Optional[Union[Session, AsyncSession]]", scope.get(self.session_scope_key))
+            if session and message["type"] in {
+                "http.response.start",
+                "http.disconnect",
+                "websocket.disconnect",
+                "websocket.close",
+            }:
+                if isinstance(session, AsyncSession):
+                    await session.close()
+                else:
+                    session.close()
+                del scope[self.session_scope_key]  # type: ignore
+            await send(message)
+
+        await self.app(scope, receive, send_wrapper)
