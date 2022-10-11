3e7771d9411fe3373ffae519762a915e07eafb76
seladb <pcapplusplus@gmail.com>
2022-10-11 13:12:54 +0000

    Add Redis cache backend (#564)
    * Initial implementation
    
    * - Replace aioredis with redis-py
    - Add tests
    
    * Update lock file
    
    * Fix lint
    
    * Small lint fix
    
    * Add redis with hiredis extra
    
    * Add documentation
    
    * Add more documentation
    
    * Small fixes
    
    * Rename extra `redis_cache_backend` to `redis`

--- starlite/cache/__init__.py
+++ starlite/cache/__init__.py
@@ -1,4 +1,5 @@
 from .base import Cache, CacheBackendProtocol
+from .redis_cache_backend import RedisCacheBackend, RedisCacheBackendConfig
 from .simple_cache_backend import SimpleCacheBackend
 
-__all__ = ("CacheBackendProtocol", "SimpleCacheBackend", "Cache")
+__all__ = ("CacheBackendProtocol", "SimpleCacheBackend", "RedisCacheBackend", "RedisCacheBackendConfig", "Cache")
