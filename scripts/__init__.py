0655d88881eba4c537fc095ecc261c2a104e3b35
seladb <pcapplusplus@gmail.com>
2022-10-12 09:39:45 +0000

    Add documentation

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -4,14 +4,31 @@ from typing import List, Optional, Union
 
 
 def _run_command(command: List[str], exit_after_run: Optional[bool] = True) -> int:
-    completed_process = subprocess.run(command)
+    """Run a CLI command.
+
+    Args:
+        command: the CLI command to run
+        exit_after_run: exit after the command finished running
+
+    Returns:
+        The command's return code
+    """
+    completed_process = subprocess.run(command, check=False)
     if exit_after_run:
-        exit(completed_process.returncode)
+        sys.exit(completed_process.returncode)
 
     return completed_process.returncode
 
 
 def _pre_commit(hook_ids: Optional[Union[str, List[str]]] = None) -> None:
+    """Run one or more pre-commit hooks.
+
+    Args:
+        hook_ids: one or more hooks to run. It's an optional parameter, `None` means run all hooks
+
+    Returns:
+        None
+    """
     files = ["--files", *sys.argv[1:]] if len(sys.argv) > 1 else ["--all-files"]
 
     if not hook_ids:
@@ -26,28 +43,76 @@ def _pre_commit(hook_ids: Optional[Union[str, List[str]]] = None) -> None:
         cur_return_code = _run_command(["pre-commit", "run", "--color=always", hook_id, *files], exit_after_run=False)
         overall_return_code = overall_return_code or cur_return_code
 
-    exit(overall_return_code)
+    sys.exit(overall_return_code)
 
 
 def test() -> None:
+    """Run tests.
+
+    Args:
+        argv: a directory or file to run on
+
+    Returns:
+        None
+    """
+
     _run_command(["pytest", *sys.argv[1:]])
 
 
 def all_checks() -> None:
+    """Run all pre-commit checks.
+
+    Args:
+        argv: a directory or files to run on
+
+    Returns:
+        None
+    """
+
     _pre_commit()
 
 
 def lint() -> None:
+    """Run pylint from pre-commit.
+
+    Args:
+        argv: a directory or files to run on
+
+    Returns:
+        None
+    """
+
     _pre_commit("pylint")
 
 
 def fmt() -> None:
-    _pre_commit(["black", "isort", "prettier", "blacken-docs"])
+    """Run black, isort, prettier, blacken-docs and docformatter from pre-
+    commit.
+
+    Args:
+        argv: a directory or files to run on
+
+    Returns:
+        None
+    """
+
+    _pre_commit(["black", "isort", "prettier", "blacken-docs", "docformatter"])
 
 
 def type_check() -> None:
+    """Run mypy from pre-commit.
+
+    Args:
+        argv: a directory or files to run on
+
+    Returns:
+        None
+    """
+
     _pre_commit("mypy")
 
 
 def docs() -> None:
+    """Build docs."""
+
     _run_command(["mkdocs", "build"])
