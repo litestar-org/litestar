d86a17aaf7ab2eed23f1edeb853faf5c6607c67f
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-10-15 22:37:08 +0200

    removed beautifulsoup warning

--- tests/middleware/test_csrf.py
+++ tests/middleware/test_csrf.py
@@ -195,7 +195,7 @@ def test_csrf_form_parsing(engine: Any, template: str, template_dir: Path) -> No
             f'<html><body><div><form action="{url}" method="post">{template}</form></div></body></html>'
         )
         response = client.get("/")
-        html_soup = BeautifulSoup(html.unescape(response.text))
+        html_soup = BeautifulSoup(html.unescape(response.text), features="html.parser")
         data = {"_csrf_token": html_soup.body.div.form.input.attrs.get("value")}
         response = client.post("/", data=data)
         assert response.status_code == HTTP_201_CREATED
