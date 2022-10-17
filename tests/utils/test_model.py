a9ed05c50e0033363a54f756a9bcefa4c7b03cc0
Peter Schutt <peter.github@proton.me>
2022-10-17 11:55:47 +1000

    Tests for caching of converted model types.

---
+++ tests/utils/test_model.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+from unittest.mock import MagicMock
+
+from typing_extensions import TypedDict
+
+from starlite.utils import model
+
+if TYPE_CHECKING:
+    from pytest import MonkeyPatch  # noqa: PT013
+
+
+def test_convert_dataclass_to_model_cache(monkeypatch: "MonkeyPatch") -> None:
+    @dataclass
+    class DC:
+        a: str
+        b: int
+
+    response_mock = MagicMock()
+    create_model_from_dataclass_mock = MagicMock(return_value=response_mock)
+    monkeypatch.setattr(model, "create_model_from_dataclass", create_model_from_dataclass_mock)
+    # test calling the function twice returns the expected response each time
+    for _ in range(2):
+        response = model.convert_dataclass_to_model(DC)
+        assert response is response_mock
+    # ensures that the work of the function has only been done once
+    create_model_from_dataclass_mock.assert_called_once_with(DC)
+
+
+def test_convert_typeddict_to_model_cache(monkeypatch: "MonkeyPatch") -> None:
+    class TD(TypedDict):
+        a: str
+        b: int
+
+    response_mock = MagicMock()
+    create_model_from_typeddict_mock = MagicMock(return_value=response_mock)
+    monkeypatch.setattr(model, "create_model_from_typeddict", create_model_from_typeddict_mock)
+    # test calling the function twice returns the expected response each time
+    for _ in range(2):
+        response = model.convert_typeddict_to_model(TD)
+        assert response is response_mock
+    # ensures that the work of the function has only been done once
+    create_model_from_typeddict_mock.assert_called_once_with(TD)
