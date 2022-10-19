a8c7104cf096f0c7f8c9984359eb84b268e930ae
Peter Schutt <peter@topsport.com.au>
2022-10-20 01:46:28 +1000

    Improves and extends docs for Dependency function. (#622)
    * Improves and extends docs for Dependency function.
    
    Closes #618
    
    * Work around for test modifying global state.
    
    See issue #623

---
+++ examples/tests/dependency_injection/test_dependency_validation_error.py
@@ -0,0 +1,10 @@
+from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
+
+from examples.dependency_injection import dependency_validation_error
+from starlite.testing import TestClient
+
+
+def test_route_returns_internal_server_error() -> None:
+    with TestClient(app=dependency_validation_error.app) as client:
+        r = client.get("/")
+    assert r.status_code == HTTP_500_INTERNAL_SERVER_ERROR
