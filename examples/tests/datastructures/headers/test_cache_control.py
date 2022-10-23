fa6f9ea1bece0ee4e190c4ed2f840cbb0e27a07e
seladb <pcapplusplus@gmail.com>
2022-10-23 08:55:20 +0000

    Fix lint

--- examples/tests/datastructures/headers/test_cache_control.py
+++ examples/tests/datastructures/headers/test_cache_control.py
@@ -5,10 +5,10 @@ from starlite.testing import TestClient
 def test_cache_control_header() -> None:
     with TestClient(app=cache_control.app) as client:
         response = client.get("/population")
-        assert response.headers["cache-control"] in ["max-age=2628288, public", "public, max-age=2628288"]
+        assert response.headers["cache-control"] in ("max-age=2628288, public", "public, max-age=2628288")
 
         response = client.get("/chance_of_rain")
-        assert response.headers["cache-control"] in ["max-age=86400, public", "public, max-age=86400"]
+        assert response.headers["cache-control"] in ("max-age=86400, public", "public, max-age=86400")
 
         response = client.get("/timestamp")
         assert response.headers["cache-control"] == "no-store"
