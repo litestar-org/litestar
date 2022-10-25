cc3ca323991eb6c4ce07d2452ccdafac1816806d
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2022-10-25 14:25:07 +0200

    Add expiry mechanism to FakeAsyncMemcached, improve typing

--- tests/fake_memcached.py
+++ tests/fake_memcached.py
@@ -1,22 +1,35 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Union
+import time
 
 from aiomcache.client import Client
 
 
 class FakeAsyncMemcached(Client):
     def __init__(self) -> None:
-        self._cache: Dict[bytes, bytes] = {}
-        self._expirations: Dict[bytes, int] = {}
+        self._cache: Dict[str, bytes] = {}
+        self._expirations: Dict[str, int] = {}
+        self._added_times: Dict[str, float] = {}
 
-    async def get(self, key: bytes) -> Optional[bytes]:  # type: ignore[override]
-        return self._cache.get(key)
+    async def get(self, key: Union[bytes, str]) -> Optional[bytes]:  # type: ignore[override]
+        normalised_key = key.decode() if isinstance(key, bytes) else key
+        if normalised_key in self._cache:
+            expiry = self.ttl(key)
+            added = self._added_times.get(normalised_key)
+            now = time.monotonic()
+            if not expiry or (added + expiry > now):
+                return self._cache[normalised_key]
 
-    async def set(self, key: bytes, value: bytes, exptime: int) -> None:  # type: ignore[override]
-        self._cache[key] = value
-        self._expirations[key] = exptime
+    async def set(self, key: Union[bytes, str], value: bytes, exptime: Optional[int] = 0) -> None:  # type: ignore[override]
+        normalised_key = key.decode() if isinstance(key, bytes) else key
+        self._cache[normalised_key] = value
+        if exptime:
+            self._expirations[normalised_key] = exptime
+        self._added_times[normalised_key] = time.monotonic()
 
-    async def delete(self, key: bytes) -> None:  # type: ignore[override]
-        self._cache.pop(key)
+    async def delete(self, key: Union[bytes, str]) -> None:  # type: ignore[override]
+        normalised_key = key.decode() if isinstance(key, bytes) else key
+        if normalised_key in self._cache:
+            self._cache.pop(normalised_key)
 
-    def ttl(self, key: bytes) -> Optional[int]:
-        return self._expirations.get(key)
+    def ttl(self, key: Union[bytes, str]) -> Optional[int]:
+        return self._expirations.get(key.decode() if isinstance(key, bytes) else key)
