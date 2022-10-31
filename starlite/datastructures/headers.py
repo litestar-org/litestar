b85c7a65384cb3b53d4cf53f2299a21e232d3aa4
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2022-10-31 17:54:23 +0100

    Replace MutableHeaders

--- starlite/datastructures/headers.py
+++ starlite/datastructures/headers.py
@@ -1,10 +1,9 @@
 import re
 from abc import ABC, abstractmethod
 from typing import Any, ClassVar, Dict, Iterator, List, Mapping, Optional, Tuple
-from multidict import MultiDict, CIMultiDictProxy, CIMultiDict
 
+from multidict import CIMultiDict, CIMultiDictProxy
 from pydantic import BaseModel, Extra, Field, ValidationError, validator
-from starlette.datastructures import MutableHeaders
 from typing_extensions import Annotated
 
 from starlite.exceptions import ImproperlyConfiguredException
@@ -53,6 +52,155 @@ class Headers(CIMultiDictProxy[str, str]):
 
     def items(self) -> List[str]:
         return list(super().items())
+
+
+class MutableHeaders(Mapping[str, str]):
+    """
+    An immutable, case-insensitive multidict.
+    """
+
+    def __init__(
+        self,
+        headers: Optional[Mapping[str, str]] = None,
+        raw: Optional[List[Tuple[bytes, bytes]]] = None,
+        scope: Optional[Mapping[str, Any]] = None,
+    ) -> None:
+        self._list: List[Tuple[bytes, bytes]] = []
+        if headers is not None:
+            assert raw is None, 'Cannot set both "headers" and "raw".'
+            assert scope is None, 'Cannot set both "headers" and "scope".'
+            self._list = [(key.lower().encode("latin-1"), value.encode("latin-1")) for key, value in headers.items()]
+        elif raw is not None:
+            assert scope is None, 'Cannot set both "raw" and "scope".'
+            self._list = raw
+        elif scope is not None:
+            self._list = scope["headers"]
+
+    @property
+    def raw(self) -> List[Tuple[bytes, bytes]]:
+        return self._list
+
+    def keys(self) -> List[str]:  # type: ignore
+        return [key.decode("latin-1") for key, value in self._list]
+
+    def values(self) -> List[str]:  # type: ignore
+        return [value.decode("latin-1") for key, value in self._list]
+
+    def items(self) -> List[Tuple[str, str]]:  # type: ignore
+        return [(key.decode("latin-1"), value.decode("latin-1")) for key, value in self._list]
+
+    def getlist(self, key: str) -> List[str]:
+        get_header_key = key.lower().encode("latin-1")
+        return [item_value.decode("latin-1") for item_key, item_value in self._list if item_key == get_header_key]
+
+    def mutablecopy(self) -> "MutableHeaders":
+        return MutableHeaders(raw=self._list[:])
+
+    def setdefault(self, key: str, value: str) -> str:
+        """
+        If the header `key` does not exist, then set it to `value`.
+        Returns the header value.
+        """
+        set_key = key.lower().encode("latin-1")
+        set_value = value.encode("latin-1")
+
+        for idx, (item_key, item_value) in enumerate(self._list):
+            if item_key == set_key:
+                return item_value.decode("latin-1")
+        self._list.append((set_key, set_value))
+        return value
+
+    def update(self, other: Mapping) -> None:
+        for key, val in other.items():
+            self[key] = val
+
+    def append(self, key: str, value: str) -> None:
+        """
+        Append a header, preserving any duplicate entries.
+        """
+        append_key = key.lower().encode("latin-1")
+        append_value = value.encode("latin-1")
+        self._list.append((append_key, append_value))
+
+    def add_vary_header(self, vary: str) -> None:
+        existing = self.get("vary")
+        if existing is not None:
+            vary = ", ".join([existing, vary])
+        self["vary"] = vary
+
+    def __getitem__(self, key: str) -> str:
+        get_header_key = key.lower().encode("latin-1")
+        for header_key, header_value in self._list:
+            if header_key == get_header_key:
+                return header_value.decode("latin-1")
+        raise KeyError(key)
+
+    def __contains__(self, key: Any) -> bool:
+        get_header_key = key.lower().encode("latin-1")
+        for header_key, header_value in self._list:
+            if header_key == get_header_key:
+                return True
+        return False
+
+    def __iter__(self) -> Iterator[Any]:
+        return iter(self.keys())
+
+    def __len__(self) -> int:
+        return len(self._list)
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, Headers):
+            return False
+        return sorted(self.raw) == sorted(other.raw)
+
+    def __setitem__(self, key: str, value: str) -> None:
+        """
+        Set the header `key` to `value`, removing any duplicate entries.
+        Retains insertion order.
+        """
+        set_key = key.lower().encode("latin-1")
+        set_value = value.encode("latin-1")
+
+        found_indexes = []
+        for idx, (item_key, item_value) in enumerate(self._list):
+            if item_key == set_key:
+                found_indexes.append(idx)
+
+        for idx in reversed(found_indexes[1:]):
+            del self._list[idx]
+
+        if found_indexes:
+            idx = found_indexes[0]
+            self._list[idx] = (set_key, set_value)
+        else:
+            self._list.append((set_key, set_value))
+
+    def __delitem__(self, key: str) -> None:
+        """
+        Remove the header `key`.
+        """
+        del_key = key.lower().encode("latin-1")
+
+        pop_indexes = []
+        for idx, (item_key, item_value) in enumerate(self._list):
+            if item_key == del_key:
+                pop_indexes.append(idx)
+
+        for idx in reversed(pop_indexes):
+            del self._list[idx]
+
+    def __ior__(self, other: Mapping) -> "MutableHeaders":
+        if not isinstance(other, Mapping):
+            raise TypeError(f"Expected a mapping but got {other.__class__.__name__}")
+        self.update(other)
+        return self
+
+    def __or__(self, other: Mapping) -> "MutableHeaders":
+        if not isinstance(other, Mapping):
+            raise TypeError(f"Expected a mapping but got {other.__class__.__name__}")
+        new = self.mutablecopy()
+        new.update(other)
+        return new
 
 
 class Header(BaseModel, ABC):
