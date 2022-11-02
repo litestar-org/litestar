53fc9091e6c7edfe09dac957b4d15296d607970e
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2022-11-02 13:53:24 +0100

    Replace FormMultiDict (#734)
    * Move FormMultiDict to multidict

--- starlite/datastructures/form_multi_dict.py
+++ starlite/datastructures/form_multi_dict.py
@@ -1,11 +1,37 @@
-from typing import Any
+from typing import Any, Iterable, List, Mapping, Optional, Tuple, Union
 
-from starlette.datastructures import ImmutableMultiDict
+from multidict import MultiDict, MultiDictProxy
 
 from starlite.datastructures.upload_file import UploadFile
+from starlite.utils import deprecated
 
 
-class FormMultiDict(ImmutableMultiDict[str, Any]):
+class FormMultiDict(MultiDictProxy[Any]):
+    def __init__(
+        self, args: Optional[Union["FormMultiDict", Mapping[str, Any], Iterable[Tuple[str, Any]]]] = None
+    ) -> None:
+        super().__init__(MultiDict(args or {}))
+
+    def multi_items(self) -> List[Tuple[str, Any]]:
+        """Get all keys and values, including duplicates.
+
+        Returns:
+            A list of tuples containing key-value pairs
+        """
+        return [(key, value) for key in set(self) for value in self.getall(key)]
+
+    @deprecated("1.36.0", alternative="FormMultiDict.getall")
+    def getlist(self, key: str) -> List[str]:
+        """Get all values.
+
+        Args:
+            key: The key
+
+        Returns:
+            A list of values
+        """
+        return super().getall(key, [])
+
     async def close(self) -> None:
         """Closes all files in the multi-dict.
 
