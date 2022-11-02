cdedd947f9a49ba6dc609850783125da28bd7b12
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2022-11-02 18:32:15 +0100

    Docs and general cleanup

---
+++ docs/reference/datastructures/8-url.md
@@ -0,0 +1,13 @@
+# URL
+
+::: starlite.datastructures.URL
+    options:
+        members:
+            - from_components
+            - from_scope
+            - with_components
+
+
+::: starlite.datastructures.Address
+
+::: starlite.datastructures.url.make_absolute_url
--- starlite/datastructures/url.py
+++ starlite/datastructures/url.py
@@ -1,24 +1,39 @@
 from typing import TYPE_CHECKING, Any, NamedTuple, Optional, Union
 from urllib.parse import SplitResult, urlsplit, urlunsplit
 
+from starlite.datastructures import Headers
+
 if TYPE_CHECKING:
     from starlite.types import Scope
 
 
 class Address(NamedTuple):
+    """Just a network address."""
+
     host: str
     port: int
 
 
-def make_absolute_url(url_path: str, base_url: Union[str, "URL"]) -> str:
-    if isinstance(base_url, str):
-        base_url = URL(base_url)
-    netloc = base_url.netloc
-    path = base_url.path.rstrip("/") + url_path
-    return str(URL.from_components(scheme=base_url.scheme, netloc=netloc, path=path))
+def make_absolute_url(path: Union[str, "URL"], base: Union[str, "URL"]) -> str:
+    """Create an absolute URL.
+
+    Args:
+        path: URL path to make absolute
+        base: URL to use as a base
+
+    Returns:
+        A string representing the new, absolute URL
+    """
+    if isinstance(base, str):
+        base = URL(base)
+    netloc = base.netloc
+    path = base.path.rstrip("/") + str(path)
+    return str(URL.from_components(scheme=base.scheme, netloc=netloc, path=path))
 
 
 class URL(SplitResult):
+    """Namedtuple representing a URL."""
+
     def __new__(cls, url: Union[str, SplitResult]) -> "URL":
         if isinstance(url, SplitResult):
             result = url
@@ -27,6 +42,7 @@ class URL(SplitResult):
         return super().__new__(cls, *result)
 
     def __init__(self, url: Union[str, SplitResult]) -> None:
+        # This exists solely for type-checking and documentation purposes
         pass
 
     @classmethod
@@ -38,60 +54,83 @@ class URL(SplitResult):
         fragment: Optional[str] = None,
         query: Optional[str] = None,
     ) -> "URL":
-        result = urlsplit("")
+        """Create a new URL from components.
+
+        Args:
+            scheme: URL scheme
+            netloc: Network location
+            path: Hierarchical path
+            query: Query component
+            fragment: Fragment identifier
+
+        Returns:
+            A new URL with the given components
+        """
         return cls(
             SplitResult(
-                scheme=scheme or result.scheme,
-                netloc=netloc or result.netloc,
-                path=path or result.path,
-                fragment=fragment or result.fragment,
-                query=query or result.query,
+                scheme=scheme or "",
+                netloc=netloc or "",
+                path=path or "",
+                fragment=fragment or "",
+                query=query or "",
             )
         )
 
     @classmethod
     def from_scope(cls, scope: "Scope") -> "URL":
+        """Construct a URL from a [Scope][starlite.types.Scope]
+
+        Args:
+            scope: A scope
+
+        Returns:
+            A URL
+        """
         scheme = scope.get("scheme", "http")
         server = scope.get("server", None)
         path = scope.get("root_path", "") + scope["path"]
         query_string = scope.get("query_string", b"")
 
-        host_header = None
-        for key, value in scope["headers"]:
-            if key == b"host":
-                host_header = value.decode("latin-1")
-                break
-
-        if host_header is not None:
-            url = f"{scheme}://{host_header}{path}"
-        elif server is None:
-            url = path
-        else:
+        host = Headers.from_scope(scope).get("host")
+        if server and not host:
             host, port = server
             default_port = {"http": 80, "https": 443, "ws": 80, "wss": 443}[scheme]
-            if port == default_port:
-                url = f"{scheme}://{host}{path}"
-            else:
-                url = f"{scheme}://{host}:{port}{path}"
+            if port != default_port:
+                host = f"{host}:{port}"
 
-        if query_string:
-            url += "?" + query_string.decode()
-        return cls(url)
+        return cls.from_components(
+            scheme=scheme if server else None,
+            query=query_string.decode(),
+            netloc=host,
+            path=path,
+        )
 
     def with_components(
         self,
         scheme: Optional[str] = None,
         netloc: Optional[str] = None,
         path: Optional[str] = None,
-        fragment: Optional[str] = None,
         query: Optional[str] = None,
+        fragment: Optional[str] = None,
     ) -> "URL":
+        """Create a new URL, replacing the given components.
+
+        Args:
+            scheme: URL scheme
+            netloc: Network location
+            path: Hierarchical path
+            query: Query component
+            fragment: Fragment identifier
+
+        Returns:
+            A new URL with the given components replaced
+        """
         return URL.from_components(
             scheme=self.scheme or scheme,
             netloc=self.netloc or netloc,
             path=self.path or path,
-            fragment=self.fragment or fragment,
             query=self.query or query,
+            fragment=self.fragment or fragment,
         )
 
     def __str__(self) -> str:
