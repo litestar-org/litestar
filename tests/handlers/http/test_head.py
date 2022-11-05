b725eaa0912ba5298c2adceac2409fd80e27b0ba
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-11-05 22:28:55 +0100

    updated tests

--- tests/handlers/http/test_head.py
+++ tests/handlers/http/test_head.py
@@ -1,7 +1,18 @@
+from typing import TYPE_CHECKING
+
 import pytest
 
-from starlite import ImproperlyConfiguredException, create_test_client, head
+from starlite import (
+    File,
+    HttpMethod,
+    ImproperlyConfiguredException,
+    create_test_client,
+    head,
+)
 from starlite.status_codes import HTTP_200_OK
+
+if TYPE_CHECKING:
+    from starlite.response import FileResponse
 
 
 def test_head_decorator() -> None:
@@ -20,3 +31,23 @@ def test_head_decorator_raises_validation_error_if_body_is_declared() -> None:
         @head("/")
         def handler() -> dict:
             return {}
+
+
+def test_head_decorator_raises_validation_error_if_method_is_passed() -> None:
+    with pytest.raises(ImproperlyConfiguredException):
+
+        @head("/", http_method=HttpMethod.HEAD)
+        def handler() -> None:
+            return
+
+
+def test_head_decorator_does_not_raise_for_file() -> None:
+    @head("/")
+    def handler() -> File:
+        ...
+
+
+def test_head_decorator_does_not_raise_for_file_response() -> None:
+    @head("/")
+    def handler() -> "FileResponse":
+        ...
