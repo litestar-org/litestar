2cfd3d91fb9e7afb2d9fe1a6753c365e026411e2
Cody Fincher <codyfincher@google.com>
2022-11-07 10:06:16 -0600

    Updated protocol to allow for args and kwargs

--- starlite/types/protocols.py
+++ starlite/types/protocols.py
@@ -1,9 +1,10 @@
-from typing import Any
+from typing import Any, overload
 
 from typing_extensions import Protocol
 
 
 class Logger(Protocol):  # pragma: no cover
+    @overload
     def debug(self, event: str, **kwargs: Any) -> Any:
         """Outputs a log message at 'DEBUG' level.
 
@@ -11,7 +12,27 @@ class Logger(Protocol):  # pragma: no cover
              event: Log message.
              **kwargs: Any kwargs.
         """
+        ...
 
+    @overload
+    def debug(self, event: str, *args: Any) -> Any:
+        """Outputs a log message at 'DEBUG' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    def debug(self, event: str, *args: Any, **kwargs: Any) -> Any:
+        """Outputs a log message at 'DEBUG' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+
+    @overload
     def info(self, event: str, **kwargs: Any) -> Any:
         """Outputs a log message at 'INFO' level.
 
@@ -19,15 +40,55 @@ class Logger(Protocol):  # pragma: no cover
              event: Log message.
              **kwargs: Any kwargs.
         """
+        ...
 
-    def warning(self, event: str, **kwargs: Any) -> Any:
-        """Outputs a log message at 'WARN' level.
+    @overload
+    def info(self, event: str, *args: Any) -> Any:
+        """Outputs a log message at 'INFO' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    def info(self, event: str, *args: Any, **kwargs: Any) -> Any:
+        """Outputs a log message at 'INFO' level.
 
         Args:
              event: Log message.
              **kwargs: Any kwargs.
         """
 
+    @overload
+    def warning(self, event: str, **kwargs: Any) -> Any:
+        """Outputs a log message at 'WARNING' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    @overload
+    def warning(self, event: str, *args: Any) -> Any:
+        """Outputs a log message at 'WARNING' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    def warning(self, event: str, *args: Any, **kwargs: Any) -> Any:
+        """Outputs a log message at 'WARNING' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+
+    @overload
     def warn(self, event: str, **kwargs: Any) -> Any:
         """Outputs a log message at 'WARN' level.
 
@@ -35,7 +96,27 @@ class Logger(Protocol):  # pragma: no cover
              event: Log message.
              **kwargs: Any kwargs.
         """
+        ...
 
+    @overload
+    def warn(self, event: str, *args: Any) -> Any:
+        """Outputs a log message at 'WARN' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    def warn(self, event: str, *args: Any, **kwargs: Any) -> Any:
+        """Outputs a log message at 'WARN' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+
+    @overload
     def error(self, event: str, **kwargs: Any) -> Any:
         """Outputs a log message at 'ERROR' level.
 
@@ -43,15 +124,55 @@ class Logger(Protocol):  # pragma: no cover
              event: Log message.
              **kwargs: Any kwargs.
         """
+        ...
 
-    def fatal(self, event: str, **kwargs: Any) -> Any:
-        """Outputs a log message at 'CRITICAL' level.
+    @overload
+    def error(self, event: str, *args: Any) -> Any:
+        """Outputs a log message at 'ERROR' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    def error(self, event: str, *args: Any, **kwargs: Any) -> Any:
+        """Outputs a log message at 'ERROR' level.
 
         Args:
              event: Log message.
              **kwargs: Any kwargs.
         """
 
+    @overload
+    def fatal(self, event: str, **kwargs: Any) -> Any:
+        """Outputs a log message at 'FATAL' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    @overload
+    def fatal(self, event: str, *args: Any) -> Any:
+        """Outputs a log message at 'FATAL' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    def fatal(self, event: str, *args: Any, **kwargs: Any) -> Any:
+        """Outputs a log message at 'FATAL' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+
+    @overload
     def exception(self, event: str, **kwargs: Any) -> Any:
         """Logs a message with level 'ERROR' on this logger. The arguments are
         interpreted as for debug(). Exception info is added to the logging
@@ -61,9 +182,52 @@ class Logger(Protocol):  # pragma: no cover
              event: Log message.
              **kwargs: Any kwargs.
         """
+        ...
 
+    @overload
+    def exception(self, event: str, *args: Any) -> Any:
+        """Logs a message with level 'ERROR' on this logger. The arguments are
+        interpreted as for debug(). Exception info is added to the logging
+        message.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    def exception(self, event: str, *args: Any, **kwargs: Any) -> Any:
+        """Logs a message with level 'ERROR' on this logger. The arguments are
+        interpreted as for debug(). Exception info is added to the logging
+        message.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+
+    @overload
     def critical(self, event: str, **kwargs: Any) -> Any:
         """Outputs a log message at 'CRITICAL' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    @overload
+    def critical(self, event: str, *args: Any) -> Any:
+        """Outputs a log message at 'CRITICAL' level.
+
+        Args:
+             event: Log message.
+             **kwargs: Any kwargs.
+        """
+        ...
+
+    def critical(self, event: str, *args: Any, **kwargs: Any) -> Any:
+        """Outputs a log message at 'INFO' level.
 
         Args:
              event: Log message.
