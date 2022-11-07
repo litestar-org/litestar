439711f59dd47febe66146dc378fbd7c0513d931
Konstantin Mikhailov <jtraub@users.noreply.github.com>
2022-11-08 00:54:50 +1000

    Get rid of frames link to please docformatter

--- starlite/middleware/exceptions/debug_response.py
+++ starlite/middleware/exceptions/debug_response.py
@@ -99,8 +99,7 @@ def create_frame_html(frame: "FrameInfo", collapsed: bool) -> str:
 
 
 def create_exception_html(exc: BaseException, line_limit: int) -> str:
-    """Produce HTML representation of the exception
-    [frames](https://docs.python.org/3/reference/datamodel.html#frame-objects).
+    """Produce HTML representation of the exception frames.
 
     Args:
         exc: An Exception instance to generate.
