50b2ef750fe3c6c849b9d81f24442b2eebfbe039
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2022-11-11 12:05:32 +0100

    Introduce `send_as_attachment` flag for `StaticFiles`. (#785)
    * Introduce `send_as_attachment` flag for `StaticFiles`.
    
    This flag can be used to set the `content-disposition` to be either `inline` (`False`) or `attachment` (`True`). It defaults to `True`, mimicking the default behaviour of known web servers and frameworks.

--- starlite/static_files/base.py
+++ starlite/static_files/base.py
@@ -1,5 +1,5 @@
 from os.path import commonpath, join
-from typing import TYPE_CHECKING, List, Tuple, Union
+from typing import TYPE_CHECKING, Sequence, Tuple, Union
 
 from starlite.enums import ScopeType
 from starlite.exceptions import MethodNotAllowedException, NotFoundException
@@ -16,28 +16,35 @@ if TYPE_CHECKING:
 
 
 class StaticFiles:
-    """ASGI callable serving static files from a `FileSystemProtocol`"""
+    """This class is an ASGI App that handles file sending."""
 
-    __slots__ = ("is_html_mode", "directories", "adapter")
+    __slots__ = ("is_html_mode", "directories", "adapter", "send_as_attachment")
 
-    def __init__(self, is_html_mode: bool, directories: List["PathType"], file_system: "FileSystemProtocol") -> None:
-        """Initialize the application.
+    def __init__(
+        self,
+        is_html_mode: bool,
+        directories: Sequence["PathType"],
+        file_system: "FileSystemProtocol",
+        send_as_attachment: bool = False,
+    ) -> None:
+        """Initialize the Application.
 
         Args:
             is_html_mode: Flag dictating whether serving html. If true, the default file will be 'index.html'.
             directories: A list of directories to serve files from.
             file_system: The file_system spec to use for serving files.
+            send_as_attachment: Whether to send the file with a `content-disposition` header of
+             `attachment` or `inline`
         """
         self.adapter = FileSystemAdapter(file_system)
         self.directories = directories
         self.is_html_mode = is_html_mode
+        self.send_as_attachment = send_as_attachment
 
     async def get_fs_info(
-        self, directories: List["PathType"], file_path: str
+        self, directories: Sequence["PathType"], file_path: str
     ) -> Union[Tuple[str, "FileInfo"], Tuple[None, None]]:
-        """Resolves the file path and returns the resolved path and a.
-
-        [stat_result][os.stat_result].
+        """Return the resolved path and a [stat_result][os.stat_result].
 
         Args:
             directories: A list of directory paths.
@@ -74,15 +81,15 @@ class StaticFiles:
         filename = split_path[-1]
         joined_path = join(*split_path)  # noqa: PL118
         resolved_path, fs_info = await self.get_fs_info(directories=self.directories, file_path=joined_path)
-        content_disposition_type: "Literal['inline', 'attachment']" = "attachment"
+        content_disposition_type: "Literal['inline', 'attachment']" = (
+            "attachment" if self.send_as_attachment else "inline"
+        )
 
-        if self.is_html_mode:
-            content_disposition_type = "inline"
-            if fs_info and fs_info["type"] == "directory":
-                filename = "index.html"
-                resolved_path, fs_info = await self.get_fs_info(
-                    directories=self.directories, file_path=join(resolved_path or joined_path, filename)
-                )
+        if self.is_html_mode and fs_info and fs_info["type"] == "directory":
+            filename = "index.html"
+            resolved_path, fs_info = await self.get_fs_info(
+                directories=self.directories, file_path=join(resolved_path or joined_path, filename)
+            )
 
         if fs_info and fs_info["type"] == "file":
             await FileResponse(
--- tests/static_files/test_file_serving_resolution.py
+++ tests/static_files/test_file_serving_resolution.py
@@ -151,14 +151,14 @@ def test_static_files_response_mimetype(tmpdir: "Path", extension: str) -> None:
         assert response.headers["content-type"].startswith(expected_mime_type)
 
 
-@pytest.mark.parametrize("html_mode,disposition", [(True, "inline"), (False, "attachment")])
-def test_static_files_response_content_disposition(tmpdir: "Path", html_mode: bool, disposition: str) -> None:
-    fn = "test.txt"
-    path = tmpdir / fn
+@pytest.mark.parametrize("send_as_attachment,disposition", [(True, "attachment"), (False, "inline")])
+def test_static_files_content_disposition(tmpdir: "Path", send_as_attachment: bool, disposition: str) -> None:
+    path = tmpdir.mkdir("static_part").mkdir("static") / "test.txt"  # type: ignore
     path.write_text("content", "utf-8")
-    static_files_config = StaticFilesConfig(path="/static", directories=[tmpdir], html_mode=html_mode)
+
+    static_files_config = StaticFilesConfig(path="/static", directories=[tmpdir], send_as_attachment=send_as_attachment)
 
     with create_test_client([], static_files_config=static_files_config) as client:
-        response = client.get(f"/static/{fn}")
+        response = client.get("/static/static_part/static/test.txt")
         assert response.status_code == HTTP_200_OK
         assert response.headers["content-disposition"].startswith(disposition)
