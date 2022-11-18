086ad1175e1deacd3a3482f3df0cccc2d13fe394
Christopher Krause <ottermata@pm.me>
2022-11-18 21:47:00 +0100

    Hide automatically created OPTIONS routes in schema

--- examples/tests/openapi/test_customize_pydantic_model_name.py
+++ examples/tests/openapi/test_customize_pydantic_model_name.py
@@ -25,17 +25,6 @@ def test_schema_generation() -> None:
                         },
                         "deprecated": False,
                     },
-                    "options": {
-                        "operationId": "Options Handler",
-                        "responses": {
-                            "200": {
-                                "description": "Request fulfilled, document follows",
-                                "headers": {},
-                                "content": {"application/json": {"media_type_schema": {}}},
-                            }
-                        },
-                        "deprecated": False,
-                    },
                 }
             },
             "components": {
