ee3ec0bc97ea80363768245349da6e3c74814c0e
Peter Schutt <peter.github@proton.me>
2022-11-19 01:19:33 +1000

    Adds tests for {get,set}_starlite_scope_state utils.

--- tests/utils/test_scope.py
+++ tests/utils/test_scope.py
@@ -1,7 +1,22 @@
-from typing import Any
+from typing import TYPE_CHECKING, Any
+
+import pytest
 
 from starlite import BaseRouteHandler, HttpMethod, HTTPRouteHandler, Response, Starlite
-from starlite.utils import get_serializer_from_scope
+from starlite.constants import STARLITE
+from starlite.utils import (
+    get_serializer_from_scope,
+    get_starlite_scope_state,
+    set_starlite_scope_state,
+)
+
+if TYPE_CHECKING:
+    from starlite.types.asgi_types import HTTPScope
+
+
+@pytest.fixture()
+def scope() -> "HTTPScope":
+    return {"state": {}}  # type:ignore[typeddict-item]
 
 
 def test_get_serializer_from_scope() -> None:
@@ -26,3 +41,19 @@ def test_get_serializer_from_scope() -> None:
         )
         is MyResponse.serializer
     )
+
+
+def test_get_starlite_scope_state_sets_starlite_key_in_scope_state(scope: "HTTPScope") -> None:
+    get_starlite_scope_state(scope, "key")
+    assert STARLITE in scope["state"]
+    assert scope["state"][STARLITE]["key"] is None
+
+
+def test_get_starlite_scope_state_set_default(scope: "HTTPScope") -> None:
+    value = get_starlite_scope_state(scope, "key", "value")
+    assert scope["state"][STARLITE]["key"] == "value" == value
+
+
+def test_set_starlite_scope_state(scope: "HTTPScope") -> None:
+    set_starlite_scope_state(scope, "key", "value")
+    assert scope["state"][STARLITE]["key"] == "value"
