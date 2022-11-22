e7e003516db541637a591980210cea533f24411a
Peter Schutt <peter.github@proton.me>
2022-11-22 13:57:26 +1000

    Allows identical path params for any node.

--- starlite/asgi/routing_trie/types.py
+++ starlite/asgi/routing_trie/types.py
@@ -7,15 +7,15 @@ from typing import (
     Literal,
     NamedTuple,
     Optional,
-    Set,
     Type,
     Union,
 )
 
+from starlite.types.internal_types import PathParameterDefinition
+
 if TYPE_CHECKING:
 
     from starlite.types import ASGIApp, Method, RouteHandlerType
-    from starlite.types.internal_types import PathParameterDefinition
 
 
 class ASGIHandlerTuple(NamedTuple):
@@ -35,11 +35,12 @@ class RouteTrieNode:
         "asgi_handlers",
         "child_keys",
         "child_path_parameters",
-        "child_path_parameter_types",
+        "child_path_parameter_type_map",
         "children",
         "is_asgi",
         "is_mount",
         "is_path_type",
+        "path",
         "path_type_path_param_definition",
         "path_parameters",
     )
@@ -56,7 +57,7 @@ class RouteTrieNode:
     """
     Path parameter definition of immediate child nodes.
     """
-    child_path_parameter_types: Set[Type]
+    child_path_parameter_type_map: Dict[Type, "PathParameterDefinition"]
     """
     Types of path parameters of existing child path parameters.
     """
@@ -76,28 +77,50 @@ class RouteTrieNode:
     """
     Designate the node as being a mount route.
     """
+    path: str
+    """
+    String path to this node.
+    """
     path_parameters: List["PathParameterDefinition"]
     """
     A list of tuples containing path parameter definitions. This is used for parsing extracted path parameter values.
     """
 
 
-def create_node() -> RouteTrieNode:
+def create_node(
+    parent_node: Optional[RouteTrieNode] = None, component: Optional[Union[str, PathParameterDefinition]] = None
+) -> RouteTrieNode:
     """Create a RouteMapNode instance.
+
+    Uses `parent_node` and `component` to construct the `URL` path of the node. If neither provided, path of node is
+    `'/'`.
+
+    If `parent_node` and `component` provided, adds the new node to the parent's `children` mapping.
+
+    Args:
+        parent_node: The parent of the new node, if not root.
+        component: The path component that the node is to represent, or `None` if root.
 
     Returns:
         A route map node instance.
     """
+    parent_path = ("" if parent_node is None else parent_node.path).rstrip("/")
+    node_path = f"{{{component.full}}}" if isinstance(component, PathParameterDefinition) else component or ""
+    path = "/".join((parent_path, node_path))
 
     children: Dict[Union[str, "PathParameterDefinition"], "RouteTrieNode"] = {}
-    return RouteTrieNode(
+    node = RouteTrieNode(
         asgi_handlers={},
         child_keys=children.keys(),
         child_path_parameters=[],
-        child_path_parameter_types=set(),
+        child_path_parameter_type_map={},
         children=children,
         is_asgi=False,
         is_mount=False,
+        path=path,
         path_type_path_param_definition=None,
         path_parameters=[],
     )
+    if parent_node is not None and component is not None:
+        parent_node.children[component] = node
+    return node
