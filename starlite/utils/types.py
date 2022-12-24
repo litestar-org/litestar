bfc2438aeeb39da60f823c4e391c34dc75aaf6f4
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2022-12-24 21:15:41 +0200

    fix py.310 issues

--- starlite/utils/types.py
+++ starlite/utils/types.py
@@ -1,9 +1,17 @@
+import re
 from collections import deque
 from typing import Any, Deque, Iterable, Iterator, List, Sequence, Tuple, TypeVar
 
 from typing_extensions import TypeGuard, get_args
 
 T = TypeVar("T")
+
+tuple_types_regex = re.compile(
+    "^"
+    + "|".join(
+        [*[repr(x) for x in (List, Sequence, Iterable, Iterator, Tuple, Deque)], "tuple", "list", "collections.deque"]
+    )
+)
 
 
 def annotation_is_iterable_of_type(
@@ -21,11 +29,7 @@ def annotation_is_iterable_of_type(
     """
     if (args := get_args(annotation)) and (
         isinstance(annotation, (List, Sequence, Iterable, Iterator, Tuple, Deque, tuple, list, deque))  # type: ignore
-        # for python 3.8 and 3.9 we need to use string comparison rather than isinstance checks.
-        or any(
-            repr(annotation).startswith(repr(type_variable))
-            for type_variable in (List, Sequence, Iterable, Iterator, Tuple, Deque)
-        )
+        or tuple_types_regex.match(repr(annotation))
     ):
         return args[0] is type_value
     return False
