3637db22d3ddcbb90f4fcefcd9b2b7017a028d01
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2022-12-26 10:29:46 +0100

    Make `SessionCookieConfig` import deprecated and lazy

--- starlite/middleware/session/__init__.py
+++ starlite/middleware/session/__init__.py
@@ -1,9 +1,27 @@
-from .base import SessionMiddleware
-from .cookie_backend import (
-    CookieBackendConfig as SessionCookieConfig,  # backwards compatible export
-)
+from typing import Any
 
-__all__ = [
-    "SessionMiddleware",
-    "SessionCookieConfig",
-]
+from starlite.utils import warn_deprecation
+
+from .base import SessionMiddleware
+
+
+def __getattr__(name: str) -> Any:
+    """Provide lazy importing as per https://peps.python.org/pep-0562/"""
+
+    if name != "SessionCookieConfig":
+        raise AttributeError(f"Module {__package__} has no attribute {name}")
+
+    from .cookie_backend import CookieBackendConfig
+
+    warn_deprecation(
+        deprecated_name=f"{name} from {__package__}",
+        kind="import",
+        alternative="'from startlite.middleware.sessions.cookie_backend import CookieBackendConfig'",
+        version="1.47.0",
+    )
+
+    globals()[name] = CookieBackendConfig
+    return CookieBackendConfig
+
+
+__all__ = ["SessionMiddleware"]
