b1431528efe286a148431544fc60d65e77c2ae97
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2023-01-07 13:05:11 +0100

    add pragma

--- starlite/__main__.py
+++ starlite/__main__.py
@@ -1,3 +1,3 @@
-from starlite.cli.main import starlite_group
+from starlite.cli.main import starlite_group  # pragma: no cover
 
-starlite_group()  # pylint: disable=E1120
+starlite_group()  # pylint: disable=E1120  # pragma: no cover
