f6ccf34836d9e506848d6a94524b1e68a0b20eaf
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2023-01-31 15:43:18 +0100

    set cookies on client instance

--- docs/examples/tests/parameters/test_header_and_cookies_parameters.py
+++ docs/examples/tests/parameters/test_header_and_cookies_parameters.py
@@ -10,12 +10,11 @@ from starlite.status_codes import (
 def test_header_and_cookie_parameters() -> None:
     with TestClient(app=app) as client:
         assert client.get("/users/1").status_code == HTTP_400_BAD_REQUEST
-        assert (
-            client.get("/users/1", headers={"X-API-KEY": "foo"}, cookies={"my-cookie-param": "bar"}).status_code
-            == HTTP_401_UNAUTHORIZED
-        )
-        res = client.get(
-            "/users/1", headers={"X-API-KEY": "super-secret-secret"}, cookies={"my-cookie-param": "cookie-secret"}
-        )
+        client.cookies["my-cookie-param"] = "bar"
+
+        assert client.get("/users/1", headers={"X-API-KEY": "foo"}).status_code == HTTP_401_UNAUTHORIZED
+        client.cookies["my-cookie-param"] = "cookie-secret"
+
+        res = client.get("/users/1", headers={"X-API-KEY": "super-secret-secret"})
         assert res.status_code == HTTP_200_OK
         assert res.json() == {"id": 1, "name": "John Doe"}
