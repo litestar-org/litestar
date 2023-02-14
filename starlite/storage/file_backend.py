bc657164d8599c88093de347c121b0fd4afe6686
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2023-02-14 14:50:30 +0100

    add atomic writes

--- starlite/storage/file_backend.py
+++ starlite/storage/file_backend.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import os
 import shutil
 from datetime import datetime, timedelta
+from tempfile import mkstemp
 from typing import TYPE_CHECKING
 
-from anyio import Lock, Path
+from anyio import Path
 from anyio.to_thread import run_sync
 
 from .base import StorageBackend, StorageObject
@@ -19,29 +21,53 @@ class FileStorageBackend(StorageBackend):
     __slots__ = ("path", "_lock")
 
     def __init__(self, path: PathLike) -> None:
-        self._lock = Lock()
         self.path = Path(path)
 
     @staticmethod
-    async def _load_from_path(path: Path) -> StorageObject:
-        data = await path.read_bytes()
-        return StorageObject.from_bytes(data)
+    async def _load_from_path(path: Path) -> StorageObject | None:
+        try:
+            data = await path.read_bytes()
+            return StorageObject.from_bytes(data)
+        except FileNotFoundError:
+            return None
+
+    def _write_sync(self, target_file: Path, storage_obj: StorageObject) -> None:
+        try:
+            tmp_file_fd, tmp_file_name = mkstemp(dir=self.path, prefix=target_file.name + ".tmp")
+            renamed = False
+            try:
+                try:
+                    os.write(tmp_file_fd, storage_obj.to_bytes())
+                finally:
+                    os.close(tmp_file_fd)
+
+                shutil.move(tmp_file_name, target_file)
+                renamed = True
+            finally:
+                if not renamed:
+                    os.unlink(tmp_file_name)  # noqa: PL108
+        except OSError:
+            pass
+
+    async def _write(self, target_file: Path, storage_obj: StorageObject) -> None:
+        await run_sync(self._write_sync, target_file, storage_obj)
 
     async def get(self, key: str, renew: int | None = None) -> bytes | None:
         path = self.path / key
-        async with self._lock:
-            if not await path.exists():
-                return None
+        storage_obj = await self._load_from_path(path)
 
-            storage_obj = await self._load_from_path(path)
-            if not storage_obj.expired:
-                if renew and storage_obj.expires:
-                    storage_obj.expires = datetime.now() + timedelta(seconds=renew)
-                    await path.write_bytes(storage_obj.to_bytes())
-                return storage_obj.data
-            await path.unlink()
+        if not storage_obj:
+            return None
 
-        return None
+        if storage_obj.expired:
+            await path.unlink(missing_ok=True)
+            return None
+
+        if renew and storage_obj.expires:
+            storage_obj.expires = datetime.now() + timedelta(seconds=renew)
+            await self._write(path, storage_obj)
+
+        return storage_obj.data
 
     async def set(self, key: str, value: bytes, expires: int | None = None) -> None:
         await self.path.mkdir(exist_ok=True)
@@ -50,21 +76,18 @@ class FileStorageBackend(StorageBackend):
             expires=(datetime.now() + timedelta(seconds=expires)) if expires else None,
             data=value,
         )
-        async with self._lock:
-            await path.write_bytes(storage_obj.to_bytes())
+        await self._write(path, storage_obj)
 
     async def delete(self, key: str) -> None:
         path = self.path / key
-        async with self._lock:
-            await path.unlink(missing_ok=True)
+        await path.unlink(missing_ok=True)
 
     async def delete_all(self) -> None:
-        async with self._lock:
-            await run_sync(shutil.rmtree, self.path)
-            await self.path.mkdir()
+        await run_sync(shutil.rmtree, self.path)
+        await self.path.mkdir(exist_ok=True)
 
     async def delete_expired(self) -> None:
         async for file in self.path.iterdir():
             wrapper = await self._load_from_path(file)
-            if wrapper.expired:
-                await file.unlink()
+            if wrapper and wrapper.expired:
+                await file.unlink(missing_ok=True)
