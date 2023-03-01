21ad0e1cdf3bb6b24aac80ab025ac0d993edeeea
Peter Schutt <peter.github@proton.me>
2023-03-01 20:13:50 +1000

    Address review.
    - Moves sqlalchemy version check logic inside the try/except ImportError block.

--- tests/contrib/sqlalchemy_1/conftest.py
+++ tests/contrib/sqlalchemy_1/conftest.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 try:
     import sqlalchemy
-except ImportError:
-    collect_ignore_glob = ["*"]
-else:
+
     if sqlalchemy.__version__.startswith("2."):
         collect_ignore_glob = ["*"]
+except ImportError:
+    collect_ignore_glob = ["*"]
