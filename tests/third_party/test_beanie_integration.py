b3a490853eb76906326772dfd8c07c6d1fa96811
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2023-03-02 12:23:47 +0100

    don't use mongomock_motor on windows

--- tests/third_party/test_beanie_integration.py
+++ tests/third_party/test_beanie_integration.py
@@ -1,6 +1,7 @@
 import pytest
 from beanie import Document, Indexed, init_beanie
 from mongomock_motor import AsyncMongoMockClient
+import sys
 
 from starlite import post
 from starlite.status_codes import HTTP_201_CREATED
@@ -33,6 +34,7 @@ async def create_widget_handler(data: Widget) -> Widget:
 
 
 @pytest.mark.xfail(reason="beanie does not support serialization via '.dict'")
+@pytest.mark.skipif(sys.platform == "win32", reason="mongomock_motor does not work on windows")
 def test_beanie_serialization() -> None:
     with create_test_client(create_widget_handler, on_startup=[initialize_beanie]) as client:
         response = client.post("widget", json={"name": "moishe zuchmir"})
