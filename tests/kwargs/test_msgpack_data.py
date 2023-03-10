b072f719f148b62809df8491813f8483222bd2a5
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2023-03-10 12:34:24 +0100

    formatting pass

--- tests/kwargs/test_msgpack_data.py
+++ tests/kwargs/test_msgpack_data.py
@@ -20,6 +20,5 @@ def test_request_body_msgpack() -> None:
         assert data == test_data
 
     with create_test_client([test_header, test_annotated]) as client:
-
         response = client.post("/annotated", content=encode_msgpack(test_data))
         assert response.status_code == HTTP_201_CREATED
