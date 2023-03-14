c7f1665b12c8908bc5bc2adcb5eb13caf9b736be
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-03-14 12:41:06 +0100

    Various CLI updates (#1322)
    * update CLI autodiscovery logic
    
    * update docs
    
    * fix info double printing introduced in #1218
    
    * Only resolve StarliteEnv for commands that request it
    
    * Add version CLI command
    
    * Update CLI command section titles
    
    * Update starlite/cli/_utils.py
    
    Co-authored-by: Peter Schutt <peter@topsport.com.au>
    
    ---------
    
    Co-authored-by: Peter Schutt <peter@topsport.com.au>

--- tests/cli/test_env_resolution.py
+++ tests/cli/test_env_resolution.py
@@ -3,15 +3,10 @@ from typing import Optional
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
+from click import ClickException
 
 from starlite import Starlite
-from starlite.cli._utils import AUTODISCOVER_PATHS, StarliteEnv, _path_to_dotted_path
-from tests.cli import (
-    APP_FILE_CONTENT,
-    CREATE_APP_FILE_CONTENT,
-    GENERIC_APP_FACTORY_FILE_CONTENT,
-    GENERIC_APP_FACTORY_FILE_CONTENT_STRING_ANNOTATION,
-)
+from starlite.cli._utils import StarliteEnv, _path_to_dotted_path
 from tests.cli.conftest import CreateAppFileFixture
 
 
@@ -56,24 +51,64 @@ def test_starlite_env_from_env_host(monkeypatch: MonkeyPatch, app_file: Path) ->
 
 
 @pytest.mark.parametrize(
-    "file_content",
+    "path",
     [
-        APP_FILE_CONTENT,
-        CREATE_APP_FILE_CONTENT,
-        GENERIC_APP_FACTORY_FILE_CONTENT,
-        GENERIC_APP_FACTORY_FILE_CONTENT_STRING_ANNOTATION,
+        pytest.param("app.py", id="app_file"),
+        pytest.param("application.py", id="application_file"),
+        pytest.param("app/main.py", id="app_module"),
+        pytest.param("app/any_name.py", id="app_module_random"),
+        pytest.param("application/another_random_name.py", id="application_module_random"),
     ],
 )
-@pytest.mark.parametrize("path", AUTODISCOVER_PATHS)
 def test_env_from_env_autodiscover_from_files(
-    path: str, file_content: str, create_app_file: CreateAppFileFixture
+    path: str, app_file_content: str, app_file_app_name: str, create_app_file: CreateAppFileFixture
 ) -> None:
     directory = None
     if "/" in path:
         directory, path = path.split("/", 1)
 
-    tmp_file_path = create_app_file(path, directory, content=file_content)
+    tmp_file_path = create_app_file(file=path, directory=directory, content=app_file_content)
+    env = StarliteEnv.from_env(None)
+
+    dotted_path = _path_to_dotted_path(tmp_file_path.relative_to(Path.cwd()))
+
+    assert isinstance(env.app, Starlite)
+    assert env.app_path == f"{dotted_path}:{app_file_app_name}"
+
+
+@pytest.mark.parametrize(
+    "module_name,app_file",
+    [
+        ("app", "main.py"),
+        ("application", "main.py"),
+        ("app", "anything.py"),
+        ("application", "anything.py"),
+    ],
+)
+def test_env_from_env_autodiscover_from_module(
+    module_name: str,
+    app_file: str,
+    app_file_content: str,
+    app_file_app_name: str,
+    create_app_file: CreateAppFileFixture,
+) -> None:
+    create_app_file(
+        file=app_file,
+        directory=module_name,
+        content=app_file_content,
+        init_content=f"from .{app_file.split('.')[0]} import {app_file_app_name}",
+    )
     env = StarliteEnv.from_env(None)
 
     assert isinstance(env.app, Starlite)
-    assert env.app_path == f"{_path_to_dotted_path(tmp_file_path.relative_to(Path.cwd()))}:app"
+    assert env.app_path == f"{module_name}:{app_file_app_name}"
+
+
+@pytest.mark.parametrize("path", [".app.py", "_app.py", ".application.py", "_application.py"])
+def test_env_from_env_autodiscover_from_files_ignore_paths(
+    path: str, app_file_content: str, create_app_file: CreateAppFileFixture
+) -> None:
+    create_app_file(file=path, directory=None, content=app_file_content)
+
+    with pytest.raises(ClickException):
+        StarliteEnv.from_env(None)
