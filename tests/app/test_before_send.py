ea70d6dd6ae56012e9bed38f45c05676ee086515
Peter Schutt <peter@topsport.com.au>
2023-03-21 17:48:55 +1000

    Before send handlers receive 3 args. (#1354)
    These were initially 2-arg callables, until we realised that Scope was required for the sqlalchemy plugin.
    
    Moving to 2.0 is a good time to simplify this.

--- tests/app/test_before_send.py
+++ tests/app/test_before_send.py
@@ -1,4 +1,7 @@
-from typing import TYPE_CHECKING, Dict
+# ruff: noqa: UP006
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from starlite import get
 from starlite.datastructures import MutableScopeHeaders
@@ -6,8 +9,10 @@ from starlite.status_codes import HTTP_200_OK
 from starlite.testing import create_test_client
 
 if TYPE_CHECKING:
+    from typing import Dict
+
     from starlite.datastructures import State
-    from starlite.types import Message
+    from starlite.types import Message, Scope
 
 
 def test_before_send() -> None:
@@ -15,7 +20,7 @@ def test_before_send() -> None:
     def handler() -> Dict[str, str]:
         return {"key": "value"}
 
-    async def before_send_hook_handler(message: "Message", state: "State") -> None:
+    async def before_send_hook_handler(message: Message, state: State, scope: Scope) -> None:
         if message["type"] == "http.response.start":
             headers = MutableScopeHeaders(message)
             headers.add("My Header", state.message)
