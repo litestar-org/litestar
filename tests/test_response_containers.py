37f06c5a75450fc0c687e4fa3c7976d7ca7ce3e6
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-03-29 17:38:37 +0200

    suppert setting status codes in Redirect container

--- tests/test_response_containers.py
+++ tests/test_response_containers.py
@@ -11,7 +11,7 @@ from starlite import get
 from starlite.datastructures import ETag
 from starlite.exceptions import ImproperlyConfiguredException
 from starlite.file_system import BaseLocalFileSystem
-from starlite.response_containers import File
+from starlite.response_containers import File, Redirect, ResponseContainer
 from starlite.status_codes import HTTP_200_OK
 from starlite.testing import RequestFactory, create_test_client
 
@@ -155,3 +155,34 @@ def test_file_system_validation(tmpdir: "Path") -> None:
         path=path,
         file_system=ImplementedFS(),
     )
+
+
+@pytest.mark.parametrize(
+    "status_code,expected_status_code",
+    [
+        (301, 301),
+        (302, 302),
+        (303, 303),
+        (307, 307),
+        (308, 308),
+    ],
+)
+def test_redirect_dynamic_status_code(status_code: int | None, expected_status_code: int) -> None:
+    @get("/")
+    def handler() -> ResponseContainer:
+        return Redirect(path="/something-else", status_code=status_code)  # type: ignore[arg-type]
+
+    with create_test_client([handler]) as client:
+        res = client.get("/", follow_redirects=False)
+        assert res.status_code == expected_status_code
+
+
+@pytest.mark.parametrize("handler_status_code", [301, 307, None])
+def test_redirect(handler_status_code: int | None) -> None:
+    @get("/", status_code=handler_status_code)
+    def handler() -> ResponseContainer:
+        return Redirect(path="/something-else", status_code=301)
+
+    with create_test_client([handler]) as client:
+        res = client.get("/", follow_redirects=False)
+        assert res.status_code == 301
