4c9b60485c9154f37160d378348bcc5b05d002e7
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-03-31 12:47:34 +0200

    Fix #1419: Support `RequestFactory` with empty `data` (#1420)
    support empty body in RequestFactory

--- starlite/testing/request_factory.py
+++ starlite/testing/request_factory.py
@@ -269,6 +269,8 @@ class RequestFactory:
             for chunk in stream:
                 body += chunk
             scope["_body"] = body  # type: ignore[typeddict-unknown-key]
+        else:
+            scope["_body"] = b""  # type: ignore[typeddict-unknown-key]
         self._create_cookie_header(headers, cookies)
         scope["headers"] = self._build_headers(headers)
         return Request(scope=scope)
