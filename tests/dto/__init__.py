e15504c28814dc6e46796e509ffe77c993bbd0ea
Peter Schutt <peter.github@proton.me>
2023-04-06 12:42:17 +1000

    Extractor tests.

---
+++ tests/dto/__init__.py
@@ -0,0 +1,37 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import TYPE_CHECKING
+
+from typing_extensions import Self
+
+from starlite.dto.interface import DTOInterface
+from starlite.types.protocols import DataclassProtocol
+from starlite.types.serialization import StarliteEncodableType
+
+if TYPE_CHECKING:
+    from typing import Any
+
+    from starlite.connection import Request
+
+
+@dataclass
+class Model:
+    a: int
+    b: str
+
+
+class MockDTO(DTOInterface[DataclassProtocol]):
+    def to_data_type(self) -> DataclassProtocol:
+        return Model(a=1, b="2")
+
+    def to_encodable_type(self, request: Request[Any, Any, Any]) -> bytes | StarliteEncodableType:
+        return Model(a=1, b="2")
+
+    @classmethod
+    async def from_connection(cls, connection: Request[Any, Any, Any]) -> Self:
+        return cls()
+
+    @classmethod
+    def from_data(cls, data: DataclassProtocol) -> Self:
+        return cls()
--- tests/openapi/typescript_converter/test_converter.py
+++ tests/openapi/typescript_converter/test_converter.py
@@ -1,5 +1,3 @@
-from sys import version_info
-
 import pytest
 from polyfactory import BaseFactory
 
