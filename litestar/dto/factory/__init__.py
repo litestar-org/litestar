12bffdcb183fac8df7e25f818d133e5a47889cbf
Peter Schutt <peter.github@proton.me>
2023-04-10 07:22:27 +1000

    Add `dto_field` function to `dto.factory` namespace.

--- litestar/dto/factory/__init__.py
+++ litestar/dto/factory/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 from .config import DTOConfig
-from .field import DTOField, Mark, Purpose
+from .field import DTOField, Mark, Purpose, dto_field
 
 __all__ = (
     "DTOConfig",
     "DTOField",
     "Mark",
     "Purpose",
+    "dto_field",
 )
