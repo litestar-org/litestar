e860e57420ed8f63b644c19f77636960bcea2d5d
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-04-11 15:03:21 +0200

    fix Partial modifying dataclasses (#1481)
    v1.51.9
    
    Co-authored-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>

--- starlite/constants.py
+++ starlite/constants.py
@@ -1,6 +1,5 @@
 from inspect import Signature
 
-from msgspec.inspect import UNSET
 from pydantic.fields import Undefined
 
 from starlite.types import Empty
@@ -15,5 +14,5 @@ RESERVED_KWARGS = {"state", "headers", "cookies", "request", "socket", "data", "
 SCOPE_STATE_DEPENDENCY_CACHE = "dependency_cache"
 SCOPE_STATE_NAMESPACE = "__starlite__"
 SCOPE_STATE_RESPONSE_COMPRESSED = "response_compressed"
-UNDEFINED_SENTINELS = {Undefined, Signature.empty, UNSET, Empty, Ellipsis}
+UNDEFINED_SENTINELS = {Undefined, Signature.empty, Empty, Ellipsis}
 SKIP_VALIDATION_NAMES = {"request", "socket", "scope", "receive", "send"}
