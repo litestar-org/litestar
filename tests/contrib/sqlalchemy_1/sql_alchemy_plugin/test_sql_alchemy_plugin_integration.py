e237e5a5963e7ad430d7f680c30099fe6613a82e
Peter Schutt <peter.github@proton.me>
2023-04-16 15:37:37 +1000

    A test to demonstrate an undesired behavior of the current serialization plugin approach.

--- tests/contrib/sqlalchemy_1/sql_alchemy_plugin/test_sql_alchemy_plugin_integration.py
+++ tests/contrib/sqlalchemy_1/sql_alchemy_plugin/test_sql_alchemy_plugin_integration.py
@@ -1,12 +1,15 @@
 from typing import List
 
+import pytest
 from polyfactory.value_generators.primitives import (
     create_random_float,
     create_random_string,
 )
+from sqlalchemy import Column, Integer
 
 from litestar import get, post
 from litestar.contrib.sqlalchemy_1.plugin import SQLAlchemyPlugin
+from litestar.di import Provide
 from litestar.status_codes import HTTP_200_OK, HTTP_201_CREATED
 from litestar.testing import create_test_client
 from tests.contrib.sqlalchemy_1.sql_alchemy_plugin.models import Company
@@ -75,3 +78,29 @@ def test_serializing_a_list_of_sql_alchemy_instances() -> None:
         response = client.post("/companies/bulk", json=serialized_companies)
         assert response.status_code == HTTP_201_CREATED
         assert response.json() == serialized_companies
+
+
+@pytest.mark.xfail(reason="To demonstrate undesired behavior")
+def test_sqlalchemy_plugin_with_sqlalchemy_dependency() -> None:
+    from tests.contrib.sqlalchemy_1.sql_alchemy_plugin.models import SQLAlchemyBase
+
+    class Foo(SQLAlchemyBase):
+        __tablename__ = "foo"
+        id = Column(Integer, primary_key=True)
+
+    foo_instance = Foo(id=1)
+
+    def provide_foo() -> Foo:
+        return foo_instance
+
+    @get()
+    def get_handler(foo: Foo) -> Foo:
+        assert foo is foo_instance
+        return foo
+
+    with create_test_client(
+        [get_handler], plugins=[SQLAlchemyPlugin()], dependencies={"foo": Provide(provide_foo)}, debug=True
+    ) as client:
+        response = client.get("/")
+        assert response.status_code == HTTP_200_OK
+        assert response.json() == {"id": foo_instance.id}
