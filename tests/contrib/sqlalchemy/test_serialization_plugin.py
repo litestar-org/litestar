03fe646137a04029f0791fc83cf8c663d1a976a3
Peter Schutt <peter.github@proton.me>
2023-04-21 16:37:09 +1000

    Includes collection and additional scalar annotated handler in test.

--- tests/contrib/sqlalchemy/test_serialization_plugin.py
+++ tests/contrib/sqlalchemy/test_serialization_plugin.py
@@ -19,9 +19,11 @@ async def test_serialization_plugin(
         """
 from __future__ import annotations
 
+from typing import List
+
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-from litestar import Litestar, post
+from litestar import Litestar, get, post
 from litestar.contrib.sqlalchemy.plugins import SQLAlchemySerializationPlugin
 
 class Base(DeclarativeBase):
@@ -34,11 +36,25 @@ class A(Base):
 @post("/a")
 def post_handler(data: A) -> A:
     return data
+
+@get("/a")
+def get_handler() -> List[A]:
+    return [A(id=1, a="test"), A(id=2, a="test2")]
+
+@get("/a/1")
+def get_a() -> A:
+    return A(id=1, a="test")
 """
     )
     with create_test_client(
-        route_handlers=[module.post_handler], plugins=[SQLAlchemySerializationPlugin()], debug=True
+        route_handlers=[module.post_handler, module.get_handler, module.get_a],
+        plugins=[SQLAlchemySerializationPlugin()],
+        debug=True,
     ) as client:
         response = client.post("/a", json={"id": 1, "a": "test"})
         assert response.status_code == 201
+        assert response.json() == {"id": 1, "a": "test"}
+        response = client.get("/a")
+        assert response.json() == [{"id": 1, "a": "test"}, {"id": 2, "a": "test2"}]
+        response = client.get("/a/1")
         assert response.json() == {"id": 1, "a": "test"}
