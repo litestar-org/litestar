f69d5d35025ad88d3247ee58b1046bcb39c2a14e
Peter Schutt <peter.github@proton.me>
2023-04-30 19:56:44 +1000

    Moves model parsing into `AbstractDTOBackend`.
    This PR moves the bulk of the logic for parsing a model type from `AbstractDTOFactory` to
    `AbstractDTOBackend`. This is purely a refactor - no functionality change - just moves
    functions (that were private anyway) to within the `_backend` module.
    
    The immediate motivation for this is to move the implementation of `renaming_strategy` from the
    frontend to the backend. Our configuration for this closely mirrors that of msgspec, and so by
    making the field parsing part of the backend, this will allow us to defer field renaming to
    msgspec, or use our own implementation of the renaming strategy for other backend types.

--- litestar/dto/factory/_backends/msgspec/backend.py
+++ litestar/dto/factory/_backends/msgspec/backend.py
@@ -28,7 +28,7 @@ class MsgspecDTOBackend(AbstractDTOBackend[Struct]):
     __slots__ = ()
 
     def create_data_container_type(self, context: BackendContext) -> type[Struct]:
-        return _create_struct_for_field_definitions(str(uuid4()), context.field_definitions)
+        return _create_struct_for_field_definitions(str(uuid4()), self.parsed_field_definitions)
 
     def parse_raw(self, raw: bytes, connection_context: ConnectionContext) -> Struct | Collection[Struct]:
         return decode_media_type(  # type:ignore[no-any-return]
@@ -38,19 +38,19 @@ class MsgspecDTOBackend(AbstractDTOBackend[Struct]):
     def populate_data_from_builtins(self, data: Any) -> Any:
         parsed_data = cast("Struct | Collection[Struct]", from_builtins(data, self.annotation))
         return _build_data_from_struct(
-            self.context.model_type, parsed_data, self.context.field_definitions, self.context.reverse_name_map
+            self.context.model_type, parsed_data, self.parsed_field_definitions, self.reverse_name_map
         )
 
     def populate_data_from_raw(self, raw: bytes, connection_context: ConnectionContext) -> T | Collection[T]:
         parsed_data = self.parse_raw(raw, connection_context)
         return _build_data_from_struct(
-            self.context.model_type, parsed_data, self.context.field_definitions, self.context.reverse_name_map
+            self.context.model_type, parsed_data, self.parsed_field_definitions, self.reverse_name_map
         )
 
     def encode_data(self, data: Any, connection_context: ConnectionContext) -> LitestarEncodableType:
         if isinstance(data, CollectionsCollection):
             return self.context.parsed_type.origin(  # type:ignore[no-any-return]
-                _build_struct_from_model(datum, self.data_container_type, self.context.reverse_name_map)
+                _build_struct_from_model(datum, self.data_container_type, self.reverse_name_map)
                 for datum in data  # pyright:ignore
             )
-        return _build_struct_from_model(data, self.data_container_type, self.context.reverse_name_map)
+        return _build_struct_from_model(data, self.data_container_type, self.reverse_name_map)
--- litestar/dto/factory/_backends/utils.py
+++ litestar/dto/factory/_backends/utils.py
@@ -1,13 +1,25 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, TypeVar, cast
 
 from typing_extensions import get_origin
 
-if TYPE_CHECKING:
-    from typing import Any, Iterable
+from litestar.dto.factory import Mark
+from litestar.types.builtin_types import NoneType
+from litestar.utils.signature import ParsedType
 
-__all__ = ("build_annotation_for_backend",)
+if TYPE_CHECKING:
+    from typing import AbstractSet, Any, Iterable
+
+    from litestar.dto.factory.types import FieldDefinition, RenameStrategy
+    from litestar.dto.types import ForType
+
+__all__ = (
+    "RenameStrategies",
+    "build_annotation_for_backend",
+    "get_model_type",
+    "should_exclude_field",
+)
 
 T = TypeVar("T")
 
@@ -29,3 +41,85 @@ def build_annotation_for_backend(annotation: Any, model: type[T]) -> type[T] | t
         return origin[model]  # type:ignore[no-any-return]
     except TypeError:  # pragma: no cover
         return annotation.copy_with((model,))  # type:ignore[no-any-return]
+
+
+def should_exclude_field(field_definition: FieldDefinition, exclude: AbstractSet[str], dto_for: ForType) -> bool:
+    """Returns ``True`` where a field should be excluded from data transfer.
+
+    Args:
+        field_definition: defined DTO field
+        exclude: names of fields to exclude
+        dto_for: indicates whether the DTO is for the request body or response.
+
+    Returns:
+        ``True`` if the field should not be included in any data transfer.
+    """
+    field_name = field_definition.name
+    dto_field = field_definition.dto_field
+    excluded = field_name in exclude
+    private = dto_field and dto_field.mark is Mark.PRIVATE
+    read_only_for_write = dto_for == "data" and dto_field and dto_field.mark is Mark.READ_ONLY
+    return bool(excluded or private or read_only_for_write)
+
+
+def get_model_type(annotation: type) -> Any:
+    """Get model type represented by the DTO.
+
+    If ``annotation`` is a collection, then the inner type is returned.
+
+    Args:
+        annotation: any type.
+
+    Returns:
+        The model type that is represented by the DTO.
+    """
+    parsed_type = ParsedType(annotation)
+    if parsed_type.is_collection:
+        return parsed_type.inner_types[0].annotation
+    if parsed_type.is_optional:
+        return next(t for t in parsed_type.inner_types if t.annotation is not NoneType).annotation
+    return parsed_type.annotation
+
+
+class RenameStrategies:
+    """Useful renaming strategies than be used with :class:`DTOConfig`"""
+
+    def __init__(self, renaming_strategy: RenameStrategy) -> None:
+        self.renaming_strategy = renaming_strategy
+
+    def __call__(self, field_name: str) -> str:
+        if not isinstance(self.renaming_strategy, str):
+            return self.renaming_strategy(field_name)
+
+        return cast(str, getattr(self, self.renaming_strategy)(field_name))
+
+    @staticmethod
+    def upper(field_name: str) -> str:
+        return field_name.upper()
+
+    @staticmethod
+    def lower(field_name: str) -> str:
+        return field_name.lower()
+
+    @staticmethod
+    def camel(field_name: str) -> str:
+        return RenameStrategies._camelize(field_name)
+
+    @staticmethod
+    def pascal(field_name: str) -> str:
+        return RenameStrategies._camelize(field_name, capitalize_first_letter=True)
+
+    @staticmethod
+    def _camelize(string: str, capitalize_first_letter: bool = False) -> str:
+        """Convert a string to camel case.
+
+        Args:
+            string (str): The string to convert
+            capitalize_first_letter (bool): Default is False, a True value will convert to PascalCase
+        Returns:
+            str: The string converted to camel case or Pascal case
+        """
+        return "".join(
+            word if index == 0 and not capitalize_first_letter else word.capitalize()
+            for index, word in enumerate(string.split("_"))
+        )
