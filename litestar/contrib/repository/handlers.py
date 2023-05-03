79cac8a2675e4bbaf9cdbb5f6d985326b917cc06
sonpro1296 <sonpro1296@gmail.com>
2023-05-03 10:10:13 +0100

    fix typo

---
+++ litestar/contrib/repository/handlers.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from litestar.contrib.repository.filters import BeforeAfter, CollectionFilter, LimitOffset, OrderBy, SearchFilter
+
+if TYPE_CHECKING:
+    from litestar.config.app import AppConfig
+
+
+signature_namespace_values = {
+    "BeforeAfter": BeforeAfter,
+    "CollectionFilter": CollectionFilter,
+    "LimitOffset": LimitOffset,
+    "OrderBy": OrderBy,
+    "SearchFilter": SearchFilter,
+}
+
+
+def on_app_init(app_config: "AppConfig") -> "AppConfig":
+    """Update signature namespace for the application."""
+    app_config.signature_namespace.update(signature_namespace_values)
+    return app_config
