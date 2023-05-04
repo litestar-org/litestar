899779ebed3f703de0d2b47acd7fc062ac63c043
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-05-04 19:34:10 +0200

    add some docstrings

--- litestar/channels/plugin.py
+++ litestar/channels/plugin.py
@@ -24,20 +24,30 @@ if TYPE_CHECKING:
     from litestar.types.asgi_types import WebSocketMode
 
 
+class ChannelsException(LitestarException):
+    pass
+
+
 class Subscriber:
     def __init__(self, plugin: ChannelsPlugin) -> None:
+        """A wrapper around a stream of events published to subscribed channels"""
         self._queue: Queue[bytes | None] = Queue()
         self._task: asyncio.Task | None = None
         self._plugin = plugin
         self._backend = plugin._backend
 
     async def put(self, item: bytes | None) -> None:
+        """Put an item in the subscriber's stream"""
         await self._queue.put(item)
 
     def put_nowait(self, item: bytes | None) -> None:
+        """Put an item in the subscriber's stream without waiting"""
         self._queue.put_nowait(item)
 
     async def iter_events(self) -> AsyncGenerator[bytes, None]:
+        """Iterate over the stream of events. If no items are available, block until
+        one becomes available
+        """
         while True:
             item = await self._queue.get()
             if item is None:
@@ -45,6 +55,7 @@ class Subscriber:
             yield item
 
     async def put_history(self, channels: str | Sequence[str], limit: int | None = None) -> None:
+        """Fetch the history of ``channels`` from the backend and put them in the subscriber's stream"""
         if isinstance(channels, str):
             channels = [channels]
 
@@ -63,6 +74,13 @@ class Subscriber:
 
     @asynccontextmanager
     async def run_in_background(self, socket: WebSocket) -> AsyncGenerator[None, None]:
+        """Start a task in the background that sends events from the subscriber's stream
+        to ``socket`` as they become available. The task will be cancelled when the
+        context manager exits.
+
+        Args:
+            socket: WebSocket to send data to
+        """
         self.start_in_background(socket=socket)
         try:
             yield
@@ -78,14 +96,25 @@ class Subscriber:
             await self._plugin.unsubscribe(self)
 
     def start_in_background(self, socket: WebSocket) -> None:
+        """Start a task in the background that sends events from the subscriber's stream
+        to ``socket`` as they become available.
+
+        Args:
+            socket: WebSocket to send data to
+        """
         if self._task is None:
             self._task = asyncio.create_task(self._socket_worker(socket=socket))
 
     @property
     def is_running(self) -> bool:
+        """Return whether a sending task is currently running"""
         return self._task is not None
 
     async def stop(self) -> None:
+        """Stop a task was previously started with
+        :meth:`start_in_background` or :meth:`run_in_background`. If the task is not yet
+        done it will be cancelled and awaited
+        """
         if not self._task:
             return
 
@@ -106,11 +135,31 @@ class ChannelsPlugin(InitPluginProtocol):
         channels: Iterable[str] | None = None,
         arbitrary_channels_allowed: bool = False,
         create_route_handlers: bool = False,
+        handler_send_history: int = 0,
         handler_base_path: str = "/",
         socket_send_mode: WebSocketMode = "text",
         type_encoders: TypeEncodersMap | None = None,
-        handler_send_history: int = 0,
     ) -> None:
+        """Plugin to handle broadcasting to WebSockets with support for channels.
+
+        This plugin is available as an injected dependency using the ``channels`` key.
+
+        Args:
+            backend: Backend to store data in
+            channels: Channels to serve. If ``arbitrary_channels_allowed`` is ``False`` (the default), trying to
+                subscribe to a channel not in ``channels`` will raise an exception
+            arbitrary_channels_allowed: Allow the creation of channels on the fly
+            create_route_handlers: If ``True``, route handlers will be created for all channels defined in ``channels``.
+                If ``arbitrary_channels_allowed`` is ``True``, a single handler will be created instead, handling all
+                channels. The handlers created will accept WebSocket connections and start sending received events for
+                their respective channels.
+            handler_send_history: Amount of history entries to send from the generated route handlers after a client
+                has connected. A value of ``0`` indicates to not send a history
+            handler_base_path: Path prefix used for the generated route handlers
+            socket_send_mode: Send mode to use for sending data through a :class:`WebSocket <.connections.WebSocket>`.
+                This will be used when sending within generated route handlers or :meth:`Subscriber.run_in_background`
+            type_encoders: An additional mapping of type encoders used to encode data before sending
+        """
         self._backend = backend
         self._pub_queue: Queue[tuple[bytes, list[str]]] | None = None
         self._pub_task: Task | None = None
@@ -132,6 +181,7 @@ class ChannelsPlugin(InitPluginProtocol):
         self._channels: dict[str, set[Subscriber]] = {channel: set() for channel in channels or []}
 
     def encode_data(self, data: LitestarEncodableType) -> bytes:
+        """Encode data before storing it in the backend"""
         if isinstance(data, str):
             data = data.encode()
         if isinstance(data, bytes):
@@ -139,6 +189,7 @@ class ChannelsPlugin(InitPluginProtocol):
         return self._encode_json(data)
 
     def on_app_init(self, app_config: AppConfig) -> AppConfig:
+        """Plugin hook. Set up a ``channels`` dependency, add route handlers and register application hooks"""
         app_config.dependencies["channels"] = Provide(lambda: self, use_cache=True)
         app_config.on_startup.append(self._on_startup)
         app_config.on_shutdown.append(self._on_shutdown)
@@ -159,6 +210,7 @@ class ChannelsPlugin(InitPluginProtocol):
         return app_config
 
     def publish(self, data: LitestarEncodableType, channels: str | Iterable[str]) -> None:
+        """Publish ```data`` to ``channels``"""
         if isinstance(channels, str):
             channels = [channels]
         data = self.encode_data(data)
@@ -168,6 +220,22 @@ class ChannelsPlugin(InitPluginProtocol):
             raise RuntimeError("Plugin not yet initialized. Did you forget to call on_startup?") from e
 
     async def subscribe(self, channels: str | Iterable[str]) -> Subscriber:
+        """Create a :class:`Subscriber`, providing a stream of all events in ``channels``.
+
+        The created subscriber will be passive by default and has to be consumed manually,
+        either by calling :meth:`Subscriber.run_in_background`,
+        :meth:`Subscriber.start_in_background` or iterating over events using :meth:`Subscriber.iter_events`.
+
+        Args:
+            channels: Channel(s) to subscribe to
+
+        Returns:
+            A :class:`Subscriber`
+
+        Raises:
+            ChannelsException: If a channel in ``channels`` has not been declared on this backend and
+                ``arbitrary_channels_allowed`` has not been set to ``True``
+        """
         if isinstance(channels, str):
             channels = [channels]
 
@@ -177,7 +245,7 @@ class ChannelsPlugin(InitPluginProtocol):
         for channel in channels:
             if channel not in self._channels:
                 if not self._arbitrary_channels_allowed:
-                    raise LitestarException(
+                    raise ChannelsException(
                         f"Unknown channel: {channel!r}. Either explicitly defined the channel or set "
                         "arbitrary_channels_allowed=True"
                     )
@@ -194,6 +262,13 @@ class ChannelsPlugin(InitPluginProtocol):
         return subscriber
 
     async def unsubscribe(self, subscriber: Subscriber, channels: str | Iterable[str] | None = None) -> None:
+        """Unsubscribe a :class:`Subscriber` from ``channels``. If the subscriber has a running sending task, it will
+        be stopped.
+
+        Args:
+            channels: Channels to unsubscribe from. If ``None``, unsubscribe from all channels
+            subscriber: :class:`Subscriber` to unsubscribe
+        """
         if channels is None:
             channels = list(self._channels.keys())
         elif isinstance(channels, str):
@@ -214,15 +289,24 @@ class ChannelsPlugin(InitPluginProtocol):
                 channels_to_unsubscribe.add(channel)
 
         if not any(subscriber in queues for queues in self._channels.values()):
-            await subscriber.put(None)
+            await subscriber.put(None)  # this will stop any running task or generator by breaking the inner loop
+            if subscriber.is_running:
+                await subscriber.stop()
 
         if channels_to_unsubscribe:
             await self._backend.unsubscribe(channels_to_unsubscribe)
-        if subscriber.is_running and not any(subscriber in queues for queues in self._channels.values()):
-            await subscriber.stop()
 
     @asynccontextmanager
     async def start_subscription(self, channels: str | Iterable[str]) -> AsyncGenerator[Subscriber, None]:
+        """Create a :class:`Subscriber` and tie its subscriptions to a context manager;
+        Upon exiting the context, :meth:`unsubscribe` will be called.
+
+        Args:
+            channels: Channel(s) to subscribe to
+
+        Returns:
+            A :class:`Subscriber`
+        """
         subscriber = await self.subscribe(channels)
 
         try:
@@ -247,6 +331,7 @@ class ChannelsPlugin(InitPluginProtocol):
         return ws_handler_func
 
     async def handle_socket_send(self, socket: WebSocket, data: bytes) -> None:
+        """Send ``data`` through ``socket``"""
         await socket.send_data(data, mode=self._socket_send_mode)
 
     async def _pub_worker(self) -> None:
