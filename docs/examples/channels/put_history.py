202963ba7d1788ed7ae6e0842fa23df65015c2b2
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-05-08 09:54:15 +0200

    Apply suggestions from code review
    Co-authored-by: Na'aman Hirschfeld <nhirschfeld@gmail.com>

--- docs/examples/channels/put_history.py
+++ docs/examples/channels/put_history.py
@@ -6,6 +6,7 @@ from litestar.channels.backends.memory import MemoryChannelsBackend
 @websocket("/ws")
 async def handler(socket: WebSocket, channels: ChannelsPlugin) -> None:
     await socket.accept()
+
     async with channels.subscribe(["some_channel"]) as subscriber:
         await channels.put_subscriber_history(subscriber, ["some_channel"], limit=10)
 
