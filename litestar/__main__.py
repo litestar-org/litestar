e2b44369802783f82474c2b62f97eab043ec2b99
Cody Fincher <cody.fincher@gmail.com>
2023-06-02 22:01:11 -0500

    fix: correct random not callable error:
    Traceback (most recent call last):
      File "/home/oracle/Code/Starlite/litestar/.venv/bin/litestar", line 6, in <module>
        sys.exit(__main__())
                 ^^^^^^^^^^
    TypeError: 'module' object is not callabl

--- litestar/__main__.py
+++ litestar/__main__.py
@@ -1,3 +1,10 @@
 from litestar.cli.main import litestar_group  # pragma: no cover
 
-litestar_group()  # pragma: no cover
+
+def run_cli() -> None:
+    """Application Entrypoint."""
+    litestar_group()  # pragma: no cover
+
+
+if __name__ == "__main__":
+    run_cli()
