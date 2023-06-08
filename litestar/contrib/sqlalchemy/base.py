3804ea028553cb4aeead9081a12e305a8559a203
Cody Fincher <cody.fincher@gmail.com>
2023-06-08 11:27:04 -0500

    fix: linting update

--- litestar/contrib/sqlalchemy/base.py
+++ litestar/contrib/sqlalchemy/base.py
@@ -137,7 +137,7 @@ class CommonTableAttributes:
         Returns:
             dict[str, Any]: A dict representation of the model
         """
-        exclude = {"_sentinel"}.union(self._sa_instance_state.unloaded).union(exclude or []) # type: ignore[attr-defined]
+        exclude = {"_sentinel"}.union(self._sa_instance_state.unloaded).union(exclude or [])  # type: ignore[attr-defined]
         return {field.name: getattr(self, field.name) for field in self.__table__.columns if field.name not in exclude}
 
 
