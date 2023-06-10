f05126262076f23152c175f06b9e1e7655c8e9db
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-06-10 12:39:58 +0200

    apply unasyncd to base repository

--- litestar/contrib/repository/__init__.py
+++ litestar/contrib/repository/__init__.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
-from .abc import AbstractAsyncRepository, AbstractSyncRepository, FilterTypes
+from .async_base import AbstractAsyncRepository
 from .exceptions import ConflictError, NotFoundError, RepositoryError
+from .filters import FilterTypes
+from .sync_base import AbstractSyncRepository
 
 __all__ = (
     "AbstractAsyncRepository",
