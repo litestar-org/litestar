8c28e64104f8f22655d30d1c7074828b60c5986b
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-06-13 09:58:21 +0200

    Add back overrides

--- tests/handlers/websocket/test_listeners.py
+++ tests/handlers/websocket/test_listeners.py
@@ -405,10 +405,10 @@ def test_websocket_listener_class_hook_dependencies() -> None:
     class Listener(WebsocketListener):
         path = "/{name: str}"
 
-        def on_accept(self, name: str, state: State, query: dict, some: str) -> None:
+        def on_accept(self, name: str, state: State, query: dict, some: str) -> None:  # type: ignore[override]
             on_accept_mock(name=name, state=state, query=query, some=some)
 
-        def on_disconnect(self, name: str, state: State, query: dict, some: str) -> None:
+        def on_disconnect(self, name: str, state: State, query: dict, some: str) -> None:  # type: ignore[override]
             on_disconnect_mock(name=name, state=state, query=query, some=some)
 
         def on_receive(self, data: bytes) -> None:
