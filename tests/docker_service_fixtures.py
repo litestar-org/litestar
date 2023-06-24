99406c8815bc25a93c5d938842bbf88391529e64
Cody Fincher <cody.fincher@gmail.com>
2023-06-24 12:35:09 -0500

    chore: linting clean up

--- tests/docker_service_fixtures.py
+++ tests/docker_service_fixtures.py
@@ -102,7 +102,7 @@ def docker_services() -> Generator[DockerServiceRegistry, None, None]:
     if sys.platform != "linux":
         pytest.skip("Docker not available on this platform")
 
-    registry = DockerServiceRegistry()  # type: ignore[unreachable]
+    registry = DockerServiceRegistry()
     try:
         yield registry
     finally:
