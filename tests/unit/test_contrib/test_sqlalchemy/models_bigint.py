6c2bf8cb4f96152ecb26e1766fd84058a9eff32e
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2023-07-14 16:12:17 +0200

    chore(dto): refactor DTOInterface to use ABC instead of Protocol

--- tests/unit/test_contrib/test_sqlalchemy/models_bigint.py
+++ tests/unit/test_contrib/test_sqlalchemy/models_bigint.py
@@ -2,7 +2,6 @@
 from __future__ import annotations
 
 from datetime import date, datetime
-from typing import List
 
 from sqlalchemy import Column, FetchedValue, ForeignKey, String, Table, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
@@ -16,7 +15,7 @@ class BigIntAuthor(BigIntAuditBase):
 
     name: Mapped[str] = mapped_column(String(length=100))  # pyright: ignore
     dob: Mapped[date] = mapped_column(nullable=True)  # pyright: ignore
-    books: Mapped[List[BigIntBook]] = relationship(  # pyright: ignore  # noqa: UP
+    books: Mapped[list[BigIntBook]] = relationship(  # pyright: ignore  # noqa: UP
         lazy="selectin",
         back_populates="author",
         cascade="all, delete",
@@ -62,7 +61,7 @@ bigint_item_tag = Table(
 class BigIntItem(BigIntBase):
     name: Mapped[str] = mapped_column(String(length=50))  # pyright: ignore
     description: Mapped[str] = mapped_column(String(length=100), nullable=True)  # pyright: ignore
-    tags: Mapped[List[BigIntTag]] = relationship(  # pyright: ignore  # noqa: UP
+    tags: Mapped[list[BigIntTag]] = relationship(  # pyright: ignore  # noqa: UP
         secondary=lambda: bigint_item_tag, back_populates="items"
     )
 
@@ -71,7 +70,7 @@ class BigIntTag(BigIntBase):
     """The event log domain object."""
 
     name: Mapped[str] = mapped_column(String(length=50))  # pyright: ignore
-    items: Mapped[List[BigIntItem]] = relationship(  # pyright: ignore  # noqa: UP
+    items: Mapped[list[BigIntItem]] = relationship(  # pyright: ignore  # noqa: UP
         secondary=lambda: bigint_item_tag, back_populates="tags"
     )
 
