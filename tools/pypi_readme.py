2ceac707ebae4ce96a221b720bf5e9734fe02991
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-07-17 17:37:42 +0200

    chore: Fix PyPi readme generation on all contributors bump
    Remove the all contributors badge from the PyPi readme, since it will cause pre-commit fails after merging a PR from the all-contributors bot.

--- tools/pypi_readme.py
+++ tools/pypi_readme.py
@@ -8,6 +8,7 @@ def generate_pypi_readme() -> None:
     source = Path("README.md").read_text()
     output = re.sub(r"<!-- github-banner-start -->[\w\W]*<!-- github-banner-end -->", PYPI_BANNER, source)
     output = re.sub(r"<!-- contributors-start -->[\w\W]*<!-- contributors-end -->", "", output)
+    output = re.sub(r"<!-- ALL-CONTRIBUTORS-BADGE:START[\w\W]*<!-- ALL-CONTRIBUTORS-BADGE:END -->", "", output)
 
     # ensure a newline here so the other pre-commit hooks don't complain
     output = output.strip() + "\n"
