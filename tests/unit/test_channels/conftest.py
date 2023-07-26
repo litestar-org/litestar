63e80614a2100113d0d95f510deed720dc82acc6
Alc-Alc <45509143+Alc-Alc@users.noreply.github.com>
2023-07-26 21:21:57 +0530

    test(channels): Adds a timeout marker to all test items in the tests/unit/test_channels (#2070)
    * test(channels): Adds a timeout marker to all test items in the tests/unit/test_channels
    
    * chore: Removed unnecessary type conversion
    
    ---------
    
    Co-authored-by: Alc-Alc <alc@localhost>

--- tests/unit/test_channels/conftest.py
+++ tests/unit/test_channels/conftest.py
@@ -7,6 +7,23 @@ from litestar.channels.backends.memory import MemoryChannelsBackend
 from litestar.channels.backends.redis import RedisChannelsPubSubBackend, RedisChannelsStreamBackend
 
 
+def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]) -> None:
+    """Adds a timeout marker of 30 seconds to all test items in the 'tests/unit/test_channels'
+    module and its submodules.
+    """
+
+    # This is an interim measure to diagnose stuck tests
+    # Should be removed once the problem has been identified
+    # Below are a few examples that displayed this behavior
+    # https://github.com/litestar-org/litestar/actions/runs/5629765460/job/15255093668
+    # https://github.com/litestar-org/litestar/actions/runs/5647890525/job/15298927200
+
+    test_module_path = config.rootpath / "tests/unit/test_channels"
+    for item in items:
+        if test_module_path in item.path.parents:
+            item.add_marker(pytest.mark.timeout(30))
+
+
 @pytest.fixture()
 def redis_stream_backend(redis_client: AsyncRedis) -> RedisChannelsStreamBackend:
     return RedisChannelsStreamBackend(history=10, redis=redis_client, cap_streams_approximate=False)
