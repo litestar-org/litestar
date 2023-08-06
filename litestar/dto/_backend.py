9edc941c6df638d2f0553dea26d383eec4725907
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-08-06 16:43:49 +0200

    feat(dto): optimise _transfer_instance_data
    Apply some micro-optimizations to _transfer_instance_data

--- litestar/dto/_backend.py
+++ litestar/dto/_backend.py
@@ -585,35 +585,42 @@ def _transfer_instance_data(
     """
     unstructured_data = {}
 
+    should_use_serialization_name = not override_serialization_name and is_data_field
+    nested_as_dict = destination_type is dict
+
+    # we assume that in most cases we will call these more than one so assigning them
+    # here is going to be more efficient in most cases
+    if isinstance(source_instance, Mapping):
+        get = source_instance.__getitem__  # pyright: ignore
+        has = source_instance.__contains__  # pyright: ignore
+    else:
+
+        def get(a: str) -> Any:
+            return getattr(source_instance, a)
+
+        def has(a: Any) -> bool:
+            return hasattr(source_instance, a)
+
     for field_definition in field_definitions:
-        should_use_serialization_name = not override_serialization_name and is_data_field
         source_name = field_definition.serialization_name if should_use_serialization_name else field_definition.name
 
         if not is_data_field:
             if field_definition.is_excluded:
                 continue
-        elif not (
-            source_name in source_instance
-            if isinstance(source_instance, Mapping)
-            else hasattr(source_instance, source_name)
-        ):
+        elif not has(source_name):
             continue
 
-        transfer_type = field_definition.transfer_type
+        source_value = get(source_name)
+
+        if is_data_field and source_value is UNSET and field_definition.is_partial:
+            continue
+
         destination_name = field_definition.name if is_data_field else field_definition.serialization_name
-        source_value = (
-            source_instance[source_name]
-            if isinstance(source_instance, Mapping)
-            else getattr(source_instance, source_name)
-        )
-
-        if field_definition.is_partial and is_data_field and source_value is UNSET:
-            continue
 
         unstructured_data[destination_name] = _transfer_type_data(
             source_value=source_value,
-            transfer_type=transfer_type,
-            nested_as_dict=destination_type is dict,
+            transfer_type=field_definition.transfer_type,
+            nested_as_dict=nested_as_dict,
             is_data_field=is_data_field,
             override_serialization_name=override_serialization_name,
         )
