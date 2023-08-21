7c50fde2f794e64f1de4ec286e32202e032618ec
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-08-21 20:06:54 +0200

    add feature flag for DTO codegen
    Signed-off-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>

--- tests/unit/test_dto/__init__.py
+++ tests/unit/test_dto/__init__.py
@@ -1,78 +1,9 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any, Collection, Generator, get_type_hints
-
-from litestar._openapi.schema_generation import SchemaCreator
-from litestar.dto import AbstractDTO, DTOField, DTOFieldDefinition
-from litestar.openapi.spec import Reference, Schema
-from litestar.types.serialization import LitestarEncodableType
-from litestar.typing import FieldDefinition
 
 
 @dataclass
 class Model:
     a: int
     b: str
-
-
-class ModelDataDTO(AbstractDTO[Model]):
-    def decode_builtins(self, value: Any) -> Model:
-        return Model(a=1, b="2")
-
-    def decode_bytes(self, value: bytes) -> Model:
-        return Model(a=1, b="2")
-
-    def data_to_encodable_type(self, data: Model | Collection[Model]) -> bytes | LitestarEncodableType:
-        return Model(a=1, b="2")
-
-    @classmethod
-    def create_openapi_schema(
-        cls, field_definition: FieldDefinition, handler_id: str, schema_creator: SchemaCreator
-    ) -> Reference | Schema:
-        return Schema()
-
-    @classmethod
-    def generate_field_definitions(cls, model_type: type[Any]) -> Generator[DTOFieldDefinition, None, None]:
-        for k, v in get_type_hints(model_type).items():
-            yield DTOFieldDefinition.from_field_definition(
-                field_definition=FieldDefinition.from_kwarg(annotation=v, name=k),
-                model_name="Model",
-                default_factory=None,
-                dto_field=DTOField(),
-            )
-
-    @classmethod
-    def detect_nested_field(cls, field_definition: FieldDefinition) -> bool:
-        return False
-
-
-class ModelReturnDTO(AbstractDTO[Model]):
-    def decode_builtins(self, value: Any) -> Any:
-        raise RuntimeError("Return DTO should not have this method called")
-
-    def decode_bytes(self, value: Any) -> Any:
-        raise RuntimeError("Return DTO should not have this method called")
-
-    def data_to_encodable_type(self, data: Model | Collection[Model]) -> bytes | LitestarEncodableType:
-        return b'{"a": 1, "b": "2"}'
-
-    @classmethod
-    def create_openapi_schema(
-        cls, field_definition: FieldDefinition, handler_id: str, schema_creator: SchemaCreator
-    ) -> Reference | Schema:
-        return Schema()
-
-    @classmethod
-    def generate_field_definitions(cls, model_type: type[Any]) -> Generator[DTOFieldDefinition, None, None]:
-        for k, v in get_type_hints(model_type).items():
-            yield DTOFieldDefinition.from_field_definition(
-                field_definition=FieldDefinition.from_kwarg(annotation=v, name=k),
-                model_name="Model",
-                default_factory=None,
-                dto_field=DTOField(),
-            )
-
-    @classmethod
-    def detect_nested_field(cls, field_definition: FieldDefinition) -> bool:
-        return False
--- tests/unit/test_openapi/test_parameters.py
+++ tests/unit/test_openapi/test_parameters.py
@@ -1,11 +1,11 @@
 from typing import TYPE_CHECKING, List, Optional, Type, cast
 
 import pytest
+from polyfactory import BaseFactory
 
 from litestar import Controller, Litestar, Router, get
 from litestar._openapi.parameters import create_parameter_for_handler
 from litestar._openapi.schema_generation import SchemaCreator
-from litestar._openapi.schema_generation.examples import ExampleFactory
 from litestar._openapi.typescript_converter.schema_parsing import is_schema_value
 from litestar._signature import SignatureModel
 from litestar.di import Provide
@@ -42,8 +42,7 @@ def _create_parameters(app: Litestar, path: str) -> List["OpenAPIParameter"]:
 
 
 def test_create_parameters(person_controller: Type[Controller]) -> None:
-    ExampleFactory.seed_random(10)
-
+    BaseFactory.seed_random(1)
     parameters = _create_parameters(app=Litestar(route_handlers=[person_controller]), path="/{service_id}/person")
     assert len(parameters) == 9
     page, name, page_size, service_id, from_date, to_date, gender, secret_header, cookie_value = tuple(parameters)
@@ -105,7 +104,7 @@ def test_create_parameters(person_controller: Type[Controller]) -> None:
                 "items": {
                     "type": "string",
                     "enum": ["M", "F", "O", "A"],
-                    "examples": [{"description": "Example  value", "value": "F"}],
+                    "examples": [{"description": "Example  value", "value": "M"}],
                 },
                 "type": "array",
                 "examples": [{"description": "Example  value", "value": ["A"]}],
@@ -113,7 +112,7 @@ def test_create_parameters(person_controller: Type[Controller]) -> None:
             {
                 "type": "string",
                 "enum": ["M", "F", "O", "A"],
-                "examples": [{"description": "Example  value", "value": "M"}],
+                "examples": [{"description": "Example  value", "value": "O"}],
             },
         ],
         "examples": [{"value": "M"}, {"value": ["M", "O"]}],
