f2ef01857ecf253b6d37f8e6b6265eda98e98598
guacs <guacs.guacs@gmail.com>
2023-08-23 21:25:10 +0530

    test: hardcode expected errors

--- tests/unit/test_contrib/test_pydantic/test_integration.py
+++ tests/unit/test_contrib/test_pydantic/test_integration.py
@@ -1,10 +1,9 @@
 from typing import Any
 
-from pydantic import BaseModel, Field, ValidationError
+from pydantic import VERSION, BaseModel, Field
 
 from litestar import post
 from litestar.contrib.pydantic.pydantic_dto_factory import PydanticDTO
-from litestar.serialization.msgspec_hooks import decode_json
 from litestar.testing import create_test_client
 
 
@@ -19,14 +18,37 @@ def test_pydantic_validation_error_raises_400() -> None:
         return data
 
     model_json = {"foo": "too long"}
-    expected_errors: dict[str, Any] = {}
-    try:
-        Model(**model_json)
-    except ValidationError as ex:
-        expected_errors = decode_json(ex.json())
+    expected_errors: list[dict[str, Any]]
+
+    if VERSION.startswith("1"):
+        expected_errors = [
+            {
+                "loc": ["foo"],
+                "msg": "ensure this value has at most 2 characters",
+                "type": "value_error.any_str.max_length",
+                "ctx": {"limit_value": 2},
+            }
+        ]
+    else:
+        expected_errors = [
+            {
+                "type": "string_too_long",
+                "loc": ["foo"],
+                "msg": "String should have at most 2 characters",
+                "input": "too long",
+                "ctx": {"max_length": 2},
+            }
+        ]
 
     with create_test_client(route_handlers=handler) as client:
         response = client.post("/", json=model_json)
 
         assert response.status_code == 400
-        assert response.json()["extra"] == expected_errors
+
+        extra = response.json()["extra"]
+
+        if VERSION.startswith("2"):
+            # the URL keeps on changing as per the installed pydantic version
+            extra[0].pop("url")
+
+        assert extra == expected_errors
