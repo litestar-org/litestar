6e41cf67a2fe2991547ee6a4805ac5badc1cbde3
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-08-24 20:24:02 +0200

    fix test typing
    Signed-off-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>

--- tests/unit/test_dto/test_interface.py
+++ tests/unit/test_dto/test_interface.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from typing import Any
 from unittest.mock import MagicMock
 
+from litestar.dto import AbstractDTO
 from litestar.openapi.spec.schema import Schema
 from litestar.typing import FieldDefinition
 
 
-def test_dto_interface_create_openapi_schema_default_implementation(ModelDataDTO) -> None:
+def test_dto_interface_create_openapi_schema_default_implementation(ModelDataDTO: type[AbstractDTO]) -> None:
     assert (
         ModelDataDTO.create_openapi_schema(FieldDefinition.from_annotation(Any), MagicMock(), MagicMock()) == Schema()
     )
