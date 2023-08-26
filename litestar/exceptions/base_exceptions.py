88de686dd44457ed34573e572f3268f61a361f93
George Sakkis <george.sakkis@gmail.com>
2023-08-26 16:03:38 +0300

    fix: Exception detail attribute (#2231)
    * Update exception tests with custom subclasses of LitestarException and HTTPException
    
    * LitestarException fixes:
    - Fix LitestarException(detail) to be equivalent to LitestarException(detail=detail)
    - Honor the detail class attribute (if any)
    
    * HTTPException fixes:
    - Fix HTTPException(detail) to be equivalent to HTTPException(detail=detail)
    - Honor the detail class attribute (if any)
    
    * Refactor HTTPException.__init__ to call super().__init__
    
    * Fix annotation for python 3.8
    
    * Mypy fixes

--- litestar/exceptions/base_exceptions.py
+++ litestar/exceptions/base_exceptions.py
@@ -17,8 +17,14 @@ class LitestarException(Exception):
             *args: args are converted to :class:`str` before passing to :class:`Exception`
             detail: detail of the exception.
         """
+        str_args = [str(arg) for arg in args if arg]
+        if not detail:
+            if str_args:
+                detail, *str_args = str_args
+            elif hasattr(self, "detail"):
+                detail = self.detail
         self.detail = detail
-        super().__init__(*(str(arg) for arg in args if arg), detail)
+        super().__init__(*str_args)
 
     def __repr__(self) -> str:
         if self.detail:
@@ -26,7 +32,7 @@ class LitestarException(Exception):
         return self.__class__.__name__
 
     def __str__(self) -> str:
-        return " ".join(self.args).strip()
+        return " ".join((*self.args, self.detail)).strip()
 
 
 class MissingDependencyException(LitestarException, ImportError):
