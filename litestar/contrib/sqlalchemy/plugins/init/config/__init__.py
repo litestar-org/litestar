81b998f827a0a5ab3ef42f2a3326aa4cb5ed4e1c
Cody Fincher <cody.fincher@gmail.com>
2023-08-26 21:24:58 -0500

    fix: adds generic class

--- litestar/contrib/sqlalchemy/plugins/init/config/__init__.py
+++ litestar/contrib/sqlalchemy/plugins/init/config/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from .asyncio import AlembicAsyncConfig, AsyncSessionConfig, SQLAlchemyAsyncConfig
-from .common import GenericSessionConfig, GenericSQLAlchemyConfig
+from .common import GenericAlembicConfig, GenericSessionConfig, GenericSQLAlchemyConfig
 from .engine import EngineConfig
 from .sync import AlembicSyncConfig, SQLAlchemySyncConfig, SyncSessionConfig
 
@@ -12,6 +12,7 @@ __all__ = (
     "EngineConfig",
     "GenericSQLAlchemyConfig",
     "GenericSessionConfig",
+    "GenericAlembicConfig",
     "SQLAlchemyAsyncConfig",
     "SQLAlchemySyncConfig",
     "SyncSessionConfig",
