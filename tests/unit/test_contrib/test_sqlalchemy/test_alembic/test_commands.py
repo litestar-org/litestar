f1b2977518ee1d7a245ff379a86b282a59590b5d
Cody Fincher <cody.fincher@gmail.com>
2023-08-28 14:05:41 -0500

    fix: remove unused imports

--- tests/unit/test_contrib/test_sqlalchemy/test_alembic/test_commands.py
+++ tests/unit/test_contrib/test_sqlalchemy/test_alembic/test_commands.py
@@ -1,40 +1,20 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Literal, Type
+from typing import Type
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 from alembic.util.exc import CommandError
 
 from litestar.contrib.sqlalchemy.alembic import commands
-from litestar.contrib.sqlalchemy.repository import SQLAlchemyAsyncRepository
 from tests.unit.test_contrib.test_sqlalchemy import models_uuid
 
-RepositoryPKType = Literal["uuid", "bigint"]
 AuthorModel = Type[models_uuid.UUIDAuthor]
 RuleModel = Type[models_uuid.UUIDRule]
 ModelWithFetchedValue = Type[models_uuid.UUIDModelWithFetchedValue]
 ItemModel = Type[models_uuid.UUIDItem]
 TagModel = Type[models_uuid.UUIDTag]
-
-AnyAuthor = models_uuid.UUIDAuthor
-AuthorRepository = SQLAlchemyAsyncRepository[AnyAuthor]
-
-AnyRule = models_uuid.UUIDRule
-RuleRepository = SQLAlchemyAsyncRepository[AnyRule]
-
-AnyBook = models_uuid.UUIDBook
-BookRepository = SQLAlchemyAsyncRepository[AnyBook]
-
-AnyTag = models_uuid.UUIDTag
-TagRepository = SQLAlchemyAsyncRepository[AnyTag]
-
-AnyItem = models_uuid.UUIDItem
-ItemRepository = SQLAlchemyAsyncRepository[AnyItem]
-
-AnyModelWithFetchedValue = models_uuid.UUIDModelWithFetchedValue
-ModelWithFetchedValueRepository = SQLAlchemyAsyncRepository[AnyModelWithFetchedValue]
 
 
 @pytest.fixture
