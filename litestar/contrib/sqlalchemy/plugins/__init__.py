14b3ed7178c8b103c128bf29ccc60d90c1fc554a
Cody Fincher <codyfincher@google.com>
2023-09-01 18:43:44 +0000

    fix: linting

--- litestar/contrib/sqlalchemy/plugins/__init__.py
+++ litestar/contrib/sqlalchemy/plugins/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+
 from litestar.contrib.sqlalchemy.plugins import _slots_base
 from litestar.plugins import InitPluginProtocol
 
