ea243ed76d9871872f7587568f49e5a3feb2a19c
Abdulhaq Emhemmed <abdulhaq.emhemmed@gmail.com>
2023-09-06 01:38:27 +0100

    remove redundant line

--- litestar/cli/main.py
+++ litestar/cli/main.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import sys
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -47,8 +46,6 @@ __all__ = ("litestar_group",)
 @pass_context
 def litestar_group(ctx: Context, app_path: str | None, app_dir: Path | None = None) -> None:
     """Litestar CLI."""
-    sys.path.append(str(app_dir))
-
     if ctx.obj is None:  # env has not been loaded yet, so we can lazy load it
         ctx.obj = lambda: LitestarEnv.from_env(app_path, app_dir=app_dir)
 
