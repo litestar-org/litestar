827518efd826f137681605321014c6b6022f9ff2
Robert Rosca <32569096+RobertRosca@users.noreply.github.com>
2023-09-21 16:16:34 +0200

    docs: add workaround for running linkcheck
    using linkcheck with pydata_sphinx_theme leads to a build error,
    moved theme setup into `delayed_setup` function

--- docs/conf.py
+++ docs/conf.py
@@ -268,9 +268,20 @@ def update_html_context(
     context["generate_toctree_html"] = partial(context["generate_toctree_html"], startdepth=0)
 
 
-def setup(app: Sphinx) -> dict[str, bool]:
-    app.setup_extension("litestar_sphinx_theme")
+def delayed_setup(app: Sphinx) -> None:
+    # When running linkcheck pydata_sphinx_theme causes a build failure, and checking
+    # the builder in the initial `setup` function call is not possible, so the check
+    # and extension setup has to be delayed until the builder is initialized.
+    if app.builder.name == "linkcheck":
+        return
+
     app.setup_extension("pydata_sphinx_theme")
     app.connect("html-page-context", update_html_context)
+
+
+def setup(app: Sphinx) -> dict[str, bool]:
+    app.connect("builder-inited", delayed_setup, priority=0)
+
+    app.setup_extension("litestar_sphinx_theme")
 
     return {"parallel_read_safe": True, "parallel_write_safe": True}
