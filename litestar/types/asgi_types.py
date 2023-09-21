2aeeb9d503b9dda4b3b4c2dd52b85a6ac6dcb9c4
guacs <guacs.guacs@gmail.com>
2023-09-21 11:11:22 +0530

    fix: use Union for Python 3.8/3.9 compatibility

--- litestar/types/asgi_types.py
+++ litestar/types/asgi_types.py
@@ -103,7 +103,7 @@ if TYPE_CHECKING:
     from .internal_types import LitestarType, RouteHandlerType
     from .serialization import DataContainerType
 
-Method: TypeAlias = Literal["GET", "POST", "DELETE", "PATCH", "PUT", "HEAD", "TRACE", "OPTIONS"] | HttpMethod
+Method: TypeAlias = Union[Literal["GET", "POST", "DELETE", "PATCH", "PUT", "HEAD", "TRACE", "OPTIONS"], HttpMethod]
 ScopeSession: TypeAlias = "EmptyType | Dict[str, Any] | DataContainerType | None"
 
 
