58cd61c2095962f28d0dcb209725c623137c4dad
Peter Schutt <peter.github@proton.me>
2023-10-12 23:10:15 +1000

    refactor: Deprecate `LitestarType`.
    `LitestarType` currently exists as an alias for `Litestar`, though the rationale behind its existence is unclear. This alias might introduce confusion as the name would more appropriately alias `type[Litestar]`.
    
    This PR removes internal use of, and deprecates `LitestarType`.

--- litestar/types/callable_types.py
+++ litestar/types/callable_types.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, Any, AsyncGenerator, Awaitable, Callable, Gene
 if TYPE_CHECKING:
     from typing_extensions import TypeAlias
 
+    from litestar.app import Litestar
     from litestar.config.app import AppConfig
     from litestar.connection.base import ASGIConnection
     from litestar.connection.request import Request
@@ -13,7 +14,7 @@ if TYPE_CHECKING:
     from litestar.response.base import Response
     from litestar.types.asgi_types import ASGIApp, Message, Method, Scope
     from litestar.types.helper_types import SyncOrAsyncUnion
-    from litestar.types.internal_types import LitestarType, PathParameterDefinition
+    from litestar.types.internal_types import PathParameterDefinition
     from litestar.types.protocols import Logger
 
 
@@ -32,7 +33,7 @@ ExceptionHandler: TypeAlias = "Callable[[Request, Exception], Response]"
 ExceptionLoggingHandler: TypeAlias = "Callable[[Logger, Scope, list[str]], None]"
 GetLogger: TypeAlias = "Callable[..., Logger]"
 Guard: TypeAlias = "Callable[[ASGIConnection, BaseRouteHandler], SyncOrAsyncUnion[None]]"
-LifespanHook: TypeAlias = "Callable[[LitestarType], SyncOrAsyncUnion[Any]] | Callable[[], SyncOrAsyncUnion[Any]]"
+LifespanHook: TypeAlias = "Callable[[Litestar], SyncOrAsyncUnion[Any]] | Callable[[], SyncOrAsyncUnion[Any]]"
 OnAppInitHandler: TypeAlias = "Callable[[AppConfig], AppConfig]"
 OperationIDCreator: TypeAlias = "Callable[[HTTPRouteHandler, Method, list[str | PathParameterDefinition]], str]"
 Serializer: TypeAlias = Callable[[Any], Any]
