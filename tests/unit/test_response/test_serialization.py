0ea62a3ae60c8aa0b7d99027733e41b54e07a8de
Peter Schutt <peter.github@proton.me>
2023-11-01 12:30:41 +1000

    fix: windows compatibility tests (#2582)
    * temporary change to get windows compat CI to run on PR.
    
    * Remove escaped backslash separators.
    
    * Revert "temporary change to get windows compat CI to run on PR."
    
    This reverts commit 711626b7aeb9fd4f92068bfebcaa4695824f11bb.

--- tests/unit/test_response/test_serialization.py
+++ tests/unit/test_response/test_serialization.py
@@ -58,7 +58,7 @@ def test_enum(media_type: MediaType, decode_media_type: DecodeMediaType) -> None
 @pytest.mark.parametrize("path", [PurePath("/path/to/file"), Path("/path/to/file")])
 def test_path(media_type: MediaType, decode_media_type: DecodeMediaType, path: Path) -> None:
     encoded = Response(None).render({"value": path}, media_type=media_type)
-    expected = r"\\path\\to\\file" if isinstance(path, PureWindowsPath) else "/path/to/file"
+    expected = r"\path\to\file" if isinstance(path, PureWindowsPath) else "/path/to/file"
     assert decode_media_type(encoded) == {"value": expected}
 
 
