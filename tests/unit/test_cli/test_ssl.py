54e1ba3ebf8392d7dfa642239bd1ee992a003356
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-11-04 11:32:24 +0100

    Fix 2613
    Signed-off-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>

--- tests/unit/test_cli/test_ssl.py
+++ tests/unit/test_cli/test_ssl.py
@@ -321,9 +321,12 @@ def test_arguments_passed(
             "--host=127.0.0.1",
             "--port=8000",
             "--workers=2",
-            f"--ssl-certfile={None if ssl_certfile is None else str(project_path / ssl_certfile)}",
-            f"--ssl-keyfile={None if ssl_keyfile is None else str(project_path / ssl_keyfile)}",
         ]
+        if ssl_certfile is not None:
+            expected_args.append(f"--ssl-certfile={project_path / ssl_certfile}")
+        if ssl_keyfile is not None:
+            expected_args.append(f"--ssl-keyfile={project_path / ssl_keyfile}")
+
         mock_subprocess_run.assert_called_once()
         assert sorted(mock_subprocess_run.call_args_list[0].args[0]) == sorted(expected_args)
 
