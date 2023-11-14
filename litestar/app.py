afd390e68a6f70199ae8ba05ce4b857d0b6d1c2c
Cody Fincher <cody.fincher@gmail.com>
2023-11-14 09:07:40 -0600

    chore: remove linting ignores

--- litestar/app.py
+++ litestar/app.py
@@ -553,7 +553,7 @@ class Litestar(Router):
     async def _call_lifespan_hook(self, hook: LifespanHook) -> None:
         ret = hook(self) if inspect.signature(hook).parameters else hook()  # type: ignore
 
-        if is_async_callable(hook):  # type: ignore[arg-type] # pyright: ignore[reportGeneralTypeIssues]
+        if is_async_callable(hook):  # pyright: ignore[reportGeneralTypeIssues]
             await ret
 
     @asynccontextmanager
