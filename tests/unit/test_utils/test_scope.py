f16cb58d60d54850fdc4c15c90e44a92e52502a7
Peter Schutt <peter.github@proton.me>
2023-11-27 14:48:06 +1000

    Extend tests for utility compat.

--- tests/unit/test_utils/test_scope.py
+++ tests/unit/test_utils/test_scope.py
@@ -1,30 +1,57 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING, Callable
 
 import pytest
 
+from litestar.types.empty import Empty
 from litestar.utils import (
     get_litestar_scope_state,
     set_litestar_scope_state,
 )
+from litestar.utils.scope.state import ScopeState
 
 if TYPE_CHECKING:
     from litestar.types.asgi_types import Scope
 
 
 @pytest.fixture()
-def scope(create_scope: "Callable[..., Scope]") -> "Scope":
+def scope(create_scope: Callable[..., Scope]) -> Scope:
     return create_scope()
 
 
-def test_get_litestar_scope_state_removes_value_from_state(scope: "Scope") -> None:
+@pytest.mark.parametrize(("pop",), [(True,), (False,)])
+def test_get_litestar_scope_state_arbitrary_value(pop: bool, scope: Scope) -> None:
     key = "test"
     value = {"key": "value"}
-    set_litestar_scope_state(scope, key, value)
-    retrieved_value = get_litestar_scope_state(scope, key, pop=True)
+    connection_state = ScopeState.from_scope(scope)
+    connection_state._compat_ns[key] = value
+    retrieved_value = get_litestar_scope_state(scope, key, pop=pop)
     assert retrieved_value == value
-    assert get_litestar_scope_state(scope, key) is None
+    if pop:
+        assert connection_state._compat_ns.get(key) is None
+    else:
+        assert connection_state._compat_ns.get(key) == value
 
 
-def test_set_litestar_scope_state(scope: "Scope") -> None:
+@pytest.mark.parametrize(("pop",), [(True,), (False,)])
+def test_get_litestar_scope_state_defined_value(pop: bool, scope: Scope) -> None:
+    connection_state = ScopeState.from_scope(scope)
+    connection_state.is_cached = True
+    assert get_litestar_scope_state(scope, "is_cached", pop=pop) is True
+    if pop:
+        assert connection_state.is_cached is Empty
+    else:
+        assert connection_state.is_cached is True
+
+
+def test_set_litestar_scope_state_arbitrary_value(scope: Scope) -> None:
+    connection_state = ScopeState.from_scope(scope)
     set_litestar_scope_state(scope, "key", "value")
-    assert get_litestar_scope_state(scope, "key") == "value"
+    assert connection_state._compat_ns["key"] == "value"
+
+
+def test_set_litestar_scope_state_defined_value(scope: Scope) -> None:
+    connection_state = ScopeState.from_scope(scope)
+    set_litestar_scope_state(scope, "is_cached", True)
+    assert connection_state.is_cached is True
