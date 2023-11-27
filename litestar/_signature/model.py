18d5a6bfd15c31ff223a21f26feace27969c7415
Peter Schutt <peter.github@proton.me>
2023-11-27 23:42:47 +1000

    fix: consistent sequence union parameter errors (#2776)

--- litestar/_signature/model.py
+++ litestar/_signature/model.py
@@ -291,9 +291,10 @@ class SignatureModel(Struct):
                     type_decoders=type_decoders,
                     meta_data=meta_data,
                 )
-                for inner_type in (t for t in field_definition.inner_types if t.annotation is not type(None))
+                for inner_type in field_definition.inner_types
+                if not inner_type.is_none_type
             ]
-            return Optional[types[0]] if field_definition.is_optional else Union[tuple(types)]  # pyright: ignore
+            return Optional[Union[tuple(types)]] if field_definition.is_optional else Union[tuple(types)]  # pyright: ignore
 
         if decoder := _get_decoder_for_type(annotation, type_decoders=type_decoders):
             # FIXME: temporary (hopefully) hack, see: https://github.com/jcrist/msgspec/issues/497
