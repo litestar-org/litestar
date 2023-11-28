7d13f1895ef0f320d348953c7aa096b999e150f6
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-11-28 18:20:07 +0100

    Fix tests
    Signed-off-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>

--- tests/examples/test_security/test_jwt/test_using_oauth2_password_bearer.py
+++ tests/examples/test_security/test_jwt/test_using_oauth2_password_bearer.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from docs.examples.security.jwt import app
+from docs.examples.security.jwt.using_oauth2_password_bearer import app
 
 from litestar.status_codes import HTTP_201_CREATED, HTTP_401_UNAUTHORIZED
 from litestar.testing import TestClient
