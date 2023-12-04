fad4e9d83e3771689e1e4786e702cda5c920d777
Cody Fincher <codyfincher@google.com>
2023-12-04 10:22:59 -0600

    fix: fail back to previous import location

--- litestar/contrib/sqlalchemy/base.py
+++ litestar/contrib/sqlalchemy/base.py
@@ -1,7 +1,6 @@
 """Application ORM configuration."""
 from __future__ import annotations
 
-from advanced_alchemy._listeners import touch_updated_timestamp
 from advanced_alchemy.base import (
     AuditColumns,
     BigIntAuditBase,
@@ -15,6 +14,13 @@ from advanced_alchemy.base import (
     create_registry,
     orm_registry,
 )
+
+try :
+    # v0.6.0+
+    from advanced_alchemy._listeners import touch_updated_timestamp
+except ImportError:
+    from advanced_alchemy.base import touch_updated_timestamp
+
 
 __all__ = (
     "AuditColumns",
