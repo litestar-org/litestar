litestar-org/litestar

commit b9814c2564b25569b59d962e816126347ac4d286

feat: rework and simplify DTOs (#2088)
* feat(internal): reworked DTO logic

* feat(internal): cleanup listeners

* chore(compat): python 3.8 fixes

* feat: update plugins access to use registry

* fix(dto): resolve issue 1929

* Update tests/unit/test_contrib/test_sqlalchemy/test_serialization_plugin.py

* Update tests/unit/test_contrib/test_sqlalchemy/test_serialization_plugin.py

* chore(dead code): cleanup

* Update litestar/dto/base_dto.py

Co-authored-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>

* chore: address review comments

* Update litestar/handlers/base.py

Co-authored-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>

* chore: address review comments

* chore: make handler_id unique

---------

Co-authored-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
Co-authored-by: Cody Fincher <204685+cofin@users.noreply.github.com>

litestar/_signature/model.py
@@ -1,4 +1,4 @@
-# ruff: noqa: UP006
+# ruff: noqa: UP006, UP007
 from __future__ import annotations
 
 import re
@@ -14,6 +14,7 @@ from typing import (
     Optional,
     Sequence,
     Set,
+    Type,
     TypedDict,
     Union,
     cast,
@@ -31,6 +32,7 @@ from litestar._signature.utils import (
     _validate_signature_dependencies,
 )
 from litestar.datastructures import ImmutableState
+from litestar.dto import AbstractDTO, DTOData
 from litestar.enums import ParamType, ScopeType
 from litestar.exceptions import InternalServerException, ValidationException
 from litestar.params import KwargDefinition, ParameterKwarg
@@ -74,16 +76,19 @@ MSGSPEC_CONSTRAINT_FIELDS = (
 ERR_RE = re.compile(r"`\$\.(.+)`$")
 
 DEFAULT_TYPE_DECODERS = [
-    (lambda x: is_class_and_subclass(x, (Path, PurePath, ImmutableState, UUID)), lambda t, v: t(v))
+    (lambda x: is_class_and_subclass(x, (Path, PurePath, ImmutableState, UUID)), lambda t, v: t(v)),
 ]
 
 
 def _deserializer(target_type: Any, value: Any, default_deserializer: Callable[[Any, Any], Any]) -> Any:
+    if isinstance(value, DTOData):
+        return value
+
     if isinstance(value, target_type):
         return value
 
-    if hasattr(target_type, "_decoder"):
-        return target_type._decoder(target_type, value)
+    if decoder := getattr(target_type, "_decoder", None):
+        return decoder(target_type, value)
 
     return default_deserializer(target_type, value)
 
@@ -91,8 +96,9 @@ def _deserializer(target_type: Any, value: Any, default_deserializer: Callable[[
 class SignatureModel(Struct):
     """Model that represents a function signature that uses a msgspec specific type or types."""
 
-    # NOTE: we have to use Set and Dict here because python 3.8 goes haywire if we use 'set' and 'dict'
+    _data_dto: ClassVar[Optional[Type[AbstractDTO]]]
     _dependency_name_set: ClassVar[Set[str]]
+    # NOTE: we have to use Set and Dict here because python 3.8 goes haywire if we use 'set' and 'dict'
     _fields: ClassVar[Dict[str, FieldDefinition]]
     _return_annotation: ClassVar[Any]
 
@@ -209,9 +215,9 @@ class SignatureModel(Struct):
         cls,
         dependency_name_set: set[str],
         fn: AnyCallable,
-        has_data_dto: bool,
         parsed_signature: ParsedSignature,
         type_decoders: TypeDecodersSequence,
+        data_dto: type[AbstractDTO] | None = None,
     ) -> type[SignatureModel]:
         fn_name = (
             fn_name if (fn_name := getattr(fn, "__name__", "anonymous")) and fn_name != "<lambda>" else "anonymous"
@@ -245,7 +251,6 @@ class SignatureModel(Struct):
 
             annotation = cls._create_annotation(
                 field_definition=field_definition,
-                has_data_dto=has_data_dto,
                 type_decoders=[*(type_decoders or []), *DEFAULT_TYPE_DECODERS],
                 meta_data=meta_data,
             )
@@ -262,6 +267,7 @@ class SignatureModel(Struct):
                 "_return_annotation": parsed_signature.return_type.annotation,
                 "_dependency_name_set": dependency_names,
                 "_fields": parsed_signature.parameters,
+                "_data_dto": data_dto,
             },
             kw_only=True,
         )
@@ -270,11 +276,10 @@ class SignatureModel(Struct):
     def _create_annotation(
         cls,
         field_definition: FieldDefinition,
-        has_data_dto: bool,
         type_decoders: TypeDecodersSequence,
         meta_data: Meta | None = None,
     ) -> Any:
-        annotation = _normalize_annotation(field_definition=field_definition, has_data_dto=has_data_dto)
+        annotation = _normalize_annotation(field_definition=field_definition)
 
         if annotation is Any:
             return annotation
@@ -283,7 +288,6 @@ class SignatureModel(Struct):
             types = [
                 cls._create_annotation(
                     field_definition=inner_type,
-                    has_data_dto=has_data_dto,
                     type_decoders=type_decoders,
                     meta_data=meta_data,
                 )

tests/unit/test_signature/test_parsing.py
@@ -23,7 +23,7 @@ def test_create_function_signature_model_parameter_parsing() -> None:
     model = SignatureModel.create(
         dependency_name_set=set(),
         fn=my_fn.fn.value,
-        has_data_dto=False,
+        data_dto=None,
         parsed_signature=ParsedSignature.from_fn(my_fn.fn.value, {}),
         type_decoders=[],
     )
@@ -50,7 +50,7 @@ def test_create_function_signature_model_ignore_return_annotation() -> None:
     signature_model_type = SignatureModel.create(
         dependency_name_set=set(),
         fn=health_check.fn.value,
-        has_data_dto=False,
+        data_dto=None,
         parsed_signature=ParsedSignature.from_fn(health_check.fn.value, {}),
         type_decoders=[],
     )
@@ -104,7 +104,7 @@ def test_field_definition_is_non_string_iterable() -> None:
     model = SignatureModel.create(
         dependency_name_set=set(),
         fn=fn,
-        has_data_dto=False,
+        data_dto=None,
         parsed_signature=ParsedSignature.from_fn(fn, {}),
         type_decoders=[],
     )
@@ -120,7 +120,7 @@ def test_field_definition_is_non_string_sequence() -> None:
     model = SignatureModel.create(
         dependency_name_set=set(),
         fn=fn,
-        has_data_dto=False,
+        data_dto=None,
         parsed_signature=ParsedSignature.from_fn(fn, signature_namespace={}),
         type_decoders=[],
     )

commit 27fa9f1cede65ee83d893fe4c0f3d939425249bb

fix: no signature model data validation if DTO
By the time the signature model is parsed, the DTO has already validated and built the object to be injected as `data`, so we don't need the signature model to do it again.

This PR ensures that `data` fields are typed as `Any` on the signature model so that validation doesn't occur twice.

This is a regression introduced in #2088, so I've added an explicit test for it.

Closes #2149

litestar/_signature/model.py
@@ -252,6 +252,7 @@ class SignatureModel(Struct):
                 field_definition=field_definition,
                 type_decoders=[*(type_decoders or []), *DEFAULT_TYPE_DECODERS],
                 meta_data=meta_data,
+                data_dto=data_dto,
             )
 
             default = field_definition.default if field_definition.has_default else NODEFAULT
@@ -277,7 +278,12 @@ class SignatureModel(Struct):
         field_definition: FieldDefinition,
         type_decoders: TypeDecodersSequence,
         meta_data: Meta | None = None,
+        data_dto: type[AbstractDTO] | None = None,
     ) -> Any:
+        # DTOs have already validated their data, so we can just use Any here
+        if field_definition.name == "data" and data_dto:
+            return Any
+
         annotation = _normalize_annotation(field_definition=field_definition)
 
         if annotation is Any:

tests/unit/test_signature/test_parsing.py
@@ -1,12 +1,15 @@
+from dataclasses import dataclass
 from types import ModuleType
 from typing import Any, Callable, Iterable, List, Optional, Sequence, Union
 from unittest.mock import MagicMock
 
+import msgspec
 import pytest
 from typing_extensions import Annotated
 
 from litestar import get
 from litestar._signature import SignatureModel
+from litestar.dto import DataclassDTO
 from litestar.params import Body, Parameter
 from litestar.status_codes import HTTP_200_OK, HTTP_204_NO_CONTENT
 from litestar.testing import TestClient, create_test_client
@@ -181,3 +184,30 @@ def test_collection_union_struct_fields(with_optional: bool) -> None:
         response = client.get("/?param=foo&param=bar&param=123")
         assert response.status_code == 500
         assert "TypeError: Type unions may not contain more than one array-like" in response.text
+
+
+def test_dto_data_typed_as_any() -> None:
+    """DTOs already validate the payload, we don't need the signature model to do it too.
+
+    https://github.com/litestar-org/litestar/issues/2149
+    """
+
+    @dataclass
+    class Test:
+        a: str
+
+    dto = DataclassDTO[Test]
+
+    def fn(data: Test) -> None:
+        pass
+
+    model = SignatureModel.create(
+        dependency_name_set=set(),
+        fn=fn,
+        data_dto=dto,
+        parsed_signature=ParsedSignature.from_fn(fn, signature_namespace={"Test": Test}),
+        type_decoders=[],
+    )
+    (field,) = msgspec.structs.fields(model)
+    assert field.name == "data"
+    assert field.type is Any