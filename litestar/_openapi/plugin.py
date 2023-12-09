26d46261668ee71bf615447951944603089fb0ba
guacs <guacs.guacs@gmail.com>
2023-12-09 11:39:36 +0530

    fix: keep track of routes based on path

--- litestar/_openapi/plugin.py
+++ litestar/_openapi/plugin.py
@@ -27,7 +27,7 @@ class OpenAPIPlugin(InitPluginProtocol, ReceiveRoutePlugin):
 
     def __init__(self, app: Litestar) -> None:
         self.app = app
-        self.included_routes: list[HTTPRoute] = []
+        self.included_routes: dict[str, HTTPRoute] = {}
         self._openapi_config: OpenAPIConfig | None = None
         self._openapi_schema: OpenAPI | None = None
 
@@ -35,7 +35,8 @@ class OpenAPIPlugin(InitPluginProtocol, ReceiveRoutePlugin):
         openapi = self.openapi_config.to_openapi_schema()
         context = OpenAPIContext(openapi_config=self.openapi_config, plugins=self.app.plugins.openapi)
         openapi.paths = {
-            route.path_format or "/": create_path_item_for_route(context, route) for route in self.included_routes
+            route.path_format or "/": create_path_item_for_route(context, route)
+            for route in self.included_routes.values()
         }
         openapi.components.schemas = context.schema_registry.generate_components_schemas()
         return openapi
@@ -64,4 +65,4 @@ class OpenAPIPlugin(InitPluginProtocol, ReceiveRoutePlugin):
         if any(route_handler.resolve_include_in_schema() for route_handler, _ in route.route_handler_map.values()):
             # Force recompute the schema if a new route is added
             self._openapi_schema = None
-            self.included_routes.append(route)
+            self.included_routes[route.path] = route
