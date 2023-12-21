8e850864d5eb2ed440c9b43e3bb44d6b257d76fb
horo <143025439+horo-fox@users.noreply.github.com>
2023-12-21 12:48:25 +0900

    Fix another spotted mismatch

--- litestar/contrib/prometheus/config.py
+++ litestar/contrib/prometheus/config.py
@@ -15,7 +15,7 @@ __all__ = ("PrometheusConfig",)
 try:
     import prometheus_client  # noqa: F401
 except ImportError as e:
-    raise MissingDependencyException("prometheus_client") from e
+    raise MissingDependencyException("prometheus_client", extra="prometheus") from e
 
 
 if TYPE_CHECKING:
