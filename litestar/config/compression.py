6ddc2d1e82a9b4842d644b91238e911162f71f88
guacs <126393040+guacs@users.noreply.github.com>
2024-01-08 18:00:55 +0530

    feat: allow using custom `CompressionFacade` implementations (#2952)
    * refactor: move the different compressions into their own implementations
    
    * feat: check if encoding given by the facade is accepted
    
    * Bump version to 2.6.0
    
    Signed-off-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
    
    * feat: allow use of custom CompressionFacade implementations
    
    * feat: allow gzip to be used as fallback for any backend
    
    * docs: clarify 'backend'
    
    * test: rename test to a clearer name
    
    * docs: add docstring for 'CompressionFacade.encoding'
    
    * fix: explicitly specify facade type
    
    * fix: only import BrotliCompression if backend is brotli
    
    If the backend is not brotli, then the user may not have installed brotli which would result in an incorrect
    MissingDependency exception.
    
    ---------
    
    Signed-off-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
    Co-authored-by: Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
    Co-authored-by: Janek Nouvertné <provinzkraut@posteo.de>
    Co-authored-by: Cody Fincher <204685+cofin@users.noreply.github.com>

--- litestar/config/compression.py
+++ litestar/config/compression.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Literal
+from typing import TYPE_CHECKING, Any, Literal
 
 from litestar.exceptions import ImproperlyConfiguredException
 from litestar.middleware.compression import CompressionMiddleware
+from litestar.middleware.compression.gzip_facade import GzipCompression
+
+if TYPE_CHECKING:
+    from litestar.middleware.compression.facade import CompressionFacade
 
 __all__ = ("CompressionConfig",)
 
@@ -17,8 +21,11 @@ class CompressionConfig:
     using the ``compression_config`` key.
     """
 
-    backend: Literal["gzip", "brotli"]
-    """Literal of "gzip" or "brotli"."""
+    backend: Literal["gzip", "brotli"] | str
+    """The backend to use.
+
+    If the value given is `gzip` or `brotli`, then the builtin gzip and brotli compression is used.
+    """
     minimum_size: int = field(default=500)
     """Minimum response size (bytes) to enable compression, affects all backends."""
     gzip_compress_level: int = field(default=9)
@@ -48,16 +55,29 @@ class CompressionConfig:
     """A pattern or list of patterns to skip in the compression middleware."""
     exclude_opt_key: str | None = None
     """An identifier to use on routes to disable compression for a particular route."""
+    compression_facade: type[CompressionFacade] = GzipCompression
+    """The compression facade to use for the actual compression."""
+    backend_config: Any = None
+    """Configuration specific to the backend."""
+    gzip_fallback: bool = True
+    """Use GZIP as a fallback if the provided backend is not supported by the client."""
 
     def __post_init__(self) -> None:
         if self.minimum_size <= 0:
             raise ImproperlyConfiguredException("minimum_size must be greater than 0")
 
-        if self.gzip_compress_level < 0 or self.gzip_compress_level > 9:
-            raise ImproperlyConfiguredException("gzip_compress_level must be a value between 0 and 9")
+        if self.backend == "gzip":
+            if self.gzip_compress_level < 0 or self.gzip_compress_level > 9:
+                raise ImproperlyConfiguredException("gzip_compress_level must be a value between 0 and 9")
+        elif self.backend == "brotli":
+            # Brotli is not guaranteed to be installed.
+            from litestar.middleware.compression.brotli_facade import BrotliCompression
 
-        if self.brotli_quality < 0 or self.brotli_quality > 11:
-            raise ImproperlyConfiguredException("brotli_quality must be a value between 0 and 11")
+            if self.brotli_quality < 0 or self.brotli_quality > 11:
+                raise ImproperlyConfiguredException("brotli_quality must be a value between 0 and 11")
 
-        if self.brotli_lgwin < 10 or self.brotli_lgwin > 24:
-            raise ImproperlyConfiguredException("brotli_lgwin must be a value between 10 and 24")
+            if self.brotli_lgwin < 10 or self.brotli_lgwin > 24:
+                raise ImproperlyConfiguredException("brotli_lgwin must be a value between 10 and 24")
+
+            self.gzip_fallback = self.brotli_gzip_fallback
+            self.compression_facade = BrotliCompression
