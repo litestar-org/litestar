72fbaa8c75ed3a7cd4b90a99306d930ad93d20fe
euri10 <benoit.barthelet@gmail.com>
2024-01-30 14:48:48 +0100

    trigger mypy error

--- tests/unit/test_response/test_sse.py
+++ tests/unit/test_response/test_sse.py
@@ -1,4 +1,4 @@
-from typing import Any, AsyncIterator, Iterator, List
+from typing import AsyncIterator, Iterator, List, Union
 
 import anyio
 import pytest
@@ -61,7 +61,7 @@ async def test_sse_steaming_response() -> None:
 async def test_various_sse_inputs(input: str, expected_events: List[HTTPXServerSentEvent]) -> None:
     @get("/testme")
     async def handler() -> ServerSentEvent:
-        async def numbers() -> AsyncIterator[Any]:
+        async def numbers() -> AsyncIterator[Union[int, str, bytes, dict, ServerSentEventMessage]]:
             for i in range(1, 6):
                 await anyio.sleep(0.001)
                 if input == "integer":
