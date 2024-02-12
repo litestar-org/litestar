a83a8b52172294d26d0a89134b0810b641dcbc65
avikstroem <anton.vikstroem@1und1.de>
2024-02-12 15:17:25 +0100

    fix: Use regex for json media type matching

--- litestar/response/base.py
+++ litestar/response/base.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import itertools
+import re
 from typing import TYPE_CHECKING, Any, ClassVar, Generic, Iterable, Literal, Mapping, TypeVar, overload
 
 from litestar.datastructures.cookie import Cookie
@@ -34,6 +35,8 @@ if TYPE_CHECKING:
 __all__ = ("ASGIResponse", "Response")
 
 T = TypeVar("T")
+
+MEDIA_TYPE_APPLICATION_JSON_PATTERN = re.compile(r"^application/(?:.+\+)?json")
 
 
 class ASGIResponse:
@@ -385,8 +388,8 @@ class Response(Generic[T]):
             if media_type == MediaType.MESSAGEPACK:
                 return encode_msgpack(content, enc_hook)
 
-            if media_type.startswith("application/json") or (
-                media_type.startswith("application/") and media_type.endswith("+json")
+            if MEDIA_TYPE_APPLICATION_JSON_PATTERN.match(
+                media_type,
             ):
                 return encode_json(content, enc_hook)
 
