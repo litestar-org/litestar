c1a6cf8430602ecb9d28c3a61426230e79074ed3
avikstroem <anton.vikstroem@1und1.de>
2024-02-13 14:02:38 +0100

    fix: Do not use pydantic in json_suffix_responses.py
    pydantic is not a base dependency of litestar

--- docs/examples/responses/json_suffix_responses.py
+++ docs/examples/responses/json_suffix_responses.py
@@ -1,26 +1,14 @@
-from typing import Optional
-
-from pydantic import BaseModel
-
 import litestar.status_codes
 from litestar import Litestar, get
 
 
-class ProblemDetails(BaseModel):
-    title: str
-    type: str
-    status: Optional[int] = None
-    detail: Optional[str] = None
-    instance: Optional[str] = None
-
-
 @get("/resources", status_code=litestar.status_codes.HTTP_418_IM_A_TEAPOT, media_type="application/problem+json")
-async def retrieve_resource() -> ProblemDetails:
-    return ProblemDetails(
-        title="Server thinks it is a teapot",
-        type="Server delusion",
-        status=litestar.status_codes.HTTP_418_IM_A_TEAPOT,
-    )
+async def retrieve_resource() -> dict:
+    return {
+        "title": "Server thinks it is a teapot",
+        "type": "Server delusion",
+        "status": litestar.status_codes.HTTP_418_IM_A_TEAPOT,
+    }
 
 
 app = Litestar(route_handlers=[retrieve_resource])
