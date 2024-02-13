3e1de6d54e7f000078905a06fd0329d138a30162
Tuukka Mustonen <tuukka.mustonen@gmail.com>
2024-02-13 15:24:31 +0200

    feat: Support `ResponseSpec(..., examples=[...])` (#3100)
    feat: Support `ResponseSpec(..., examples=[...])` (#3068)
    
    Allows to define custom examples for the responses via `ResponseSpec`.
    
    The examples set this way are always generated locally, for each response:
    
    ```json
    {
        "paths": {
        "/": {
            "get": {
            "responses": {
                "200": {
                "content": {
                    "application/json": {
                    "schema": {...},
                    "examples": ...
    ```
    
    Examples that go within the schema definition cannot be set by this.

--- litestar/_openapi/responses.py
+++ litestar/_openapi/responses.py
@@ -9,9 +9,10 @@ from operator import attrgetter
 from typing import TYPE_CHECKING, Any, Iterator
 
 from litestar._openapi.schema_generation import SchemaCreator
+from litestar._openapi.schema_generation.utils import get_formatted_examples
 from litestar.enums import MediaType
 from litestar.exceptions import HTTPException, ValidationException
-from litestar.openapi.spec import Example, OpenAPIResponse
+from litestar.openapi.spec import Example, OpenAPIResponse, Reference
 from litestar.openapi.spec.enums import OpenAPIFormat, OpenAPIType
 from litestar.openapi.spec.header import OpenAPIHeader
 from litestar.openapi.spec.media_type import OpenAPIMediaType
@@ -240,14 +241,20 @@ class ResponseFactory:
                 prefer_alias=False,
                 generate_examples=additional_response.generate_examples,
             )
-            schema = schema_creator.for_field_definition(
-                FieldDefinition.from_annotation(additional_response.data_container)
-            )
+            field_def = FieldDefinition.from_annotation(additional_response.data_container)
+            schema = schema_creator.for_field_definition(field_def)
+
+            examples: dict[str, Example | Reference] | None
+            if additional_response.examples:
+                examples = dict(get_formatted_examples(field_def, additional_response.examples))
+            else:
+                examples = None
+
             yield (
                 str(status_code),
                 OpenAPIResponse(
                     description=additional_response.description,
-                    content={additional_response.media_type: OpenAPIMediaType(schema=schema)},
+                    content={additional_response.media_type: OpenAPIMediaType(schema=schema, examples=examples)},
                 ),
             )
 
