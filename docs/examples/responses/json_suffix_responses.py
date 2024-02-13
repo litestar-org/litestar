e0f34e43678ef6422b6d036d1cdd3a83769caf59
avikstroem <anton.vikstroem@1und1.de>
2024-02-13 14:29:20 +0100

    fix: broken type hint in python 3.8
    Using built-in types as generics only
    became possible in 3.9

--- docs/examples/responses/json_suffix_responses.py
+++ docs/examples/responses/json_suffix_responses.py
@@ -1,11 +1,11 @@
-from typing import Any
+from typing import Any, Dict
 
 import litestar.status_codes
 from litestar import Litestar, get
 
 
 @get("/resources", status_code=litestar.status_codes.HTTP_418_IM_A_TEAPOT, media_type="application/problem+json")
-async def retrieve_resource() -> dict[str, Any]:
+async def retrieve_resource() -> Dict[str, Any]:
     return {
         "title": "Server thinks it is a teapot",
         "type": "Server delusion",
