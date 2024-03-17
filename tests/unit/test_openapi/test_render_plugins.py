de1fefbb46f1d049539209a5df9ed5d6f6cc2ad5
Peter Schutt <peter.github@proton.me>
2024-03-17 12:36:04 +1000

    fix: simplify test.

--- tests/unit/test_openapi/test_render_plugins.py
+++ tests/unit/test_openapi/test_render_plugins.py
@@ -5,5 +5,5 @@ from litestar.testing import RequestFactory
 
 
 def test_render_plugin_get_openapi_json_route() -> None:
-    request = RequestFactory(root_path="/root").get()
+    request = RequestFactory().get()
     assert OpenAPIRenderPlugin.get_openapi_json_route(request) == "/schema/openapi.json"
