ff0be36c7f2706439eda5e74ac558d71a1666ff6
Peter Schutt <peter.github@proton.me>
2024-03-17 12:39:46 +1000

    refactor: openapi router (#2893)
    * refactor: openapi router
    
    This PR refactors the way that we support multiple UIs for OpenAPI.
    
    We add `litestar.openapi.plugins` where `OpenAPIRenderPlugin` is defined, and implementations of that plugin for the frameworks we currently support.
    
    We add `OpenAPIConfig.render_plugins` config option, where a user can explicitly declare a set of plugins for UIs they wish to support.
    
    If a user declares a sub-class of `OpenAPIController` at `OpenAPIConfig.openapi_controller`, then existing behavior is preserved exactly.
    
    However, if no controller is explicitly declared, we invoke the new router-based approach, which should behave identically to the controller based approach (i.e., respect `enabled_endpoints` and `root_schema_site`).
    
    Closes #2541
    
    * docs: start of documentation re-write.
    
    - creates an indexed directory for openapi
    - removes the controller docs
    - start of docs for plugins
    
    * refactor: move JsonRenderPlugin into private ns
    
    We add the json plugin, and have hardcoded refs to the path that it serves, so best not to make this public API just yet.
    
    * docs: reference docs for plugins
    
    * Revert "refactor: move JsonRenderPlugin into private ns"
    
    This reverts commit 60719aabd31255ff315eb6dda768478dee371b8f.
    
    * docs: JsonRenderPlugin undocumented.
    
    * docs: continue plugin docs
    
    * test: run tests for both plugin and controller
    
    Modifies tests where appropriate to run on both the plugin-based approach and the controller based approach.
    
    * Implement default endpoint selection logic.
    
    * Deprecation of OpenAPIController configs
    
    * docs: swagger oauth examples
    
    * Update docs/usage/openapi/ui_plugins.rst
    
    * Update docs/usage/openapi/ui_plugins.rst
    
    * fix: linting
    
    * refactor: don't rely on DI for openapi schema in plugin handler.
    
    * fix(test): there's an extra schema route to serve 404s.
    
    * fix(docs): docstring indent
    
    * fix(lint): remove redundant return
    
    * refactor: plugins receive style tag instead of tag content.
    
    * feat: allow openapi router to be handed to openapi config.
    
    Allows for customization, such as adding guards, middleware, other routes, etc.
    
    * feat: add `scalar` schema ui (#2906)
    
    * Update litestar/openapi/plugins.py
    
    Co-authored-by: Jacob Coffee <jacob@z7x.org>
    
    * Update litestar/openapi/plugins.py
    
    Co-authored-by: Jacob Coffee <jacob@z7x.org>
    
    * Update litestar/openapi/plugins.py
    
    Co-authored-by: Jacob Coffee <jacob@z7x.org>
    
    * Update litestar/openapi/config.py
    
    Co-authored-by: Jacob Coffee <jacob@z7x.org>
    
    * Update litestar/openapi/config.py
    
    Co-authored-by: Jacob Coffee <jacob@z7x.org>
    
    * fix: update deprecation version
    
    * fix: use GH repo for scalar links
    
    * fix: update default scalar version
    
    * fix: scalar plugin style attribute render.
    
    Plugins expect that the style value is already wrapped in `<style>` tags.
    
    * fix: serve default static files via jsdeliver
    
    * fix: docstring syntax
    
    * fix: removes custom repr
    
    Can always add if there's a need for it, but we aren't using it.
    
    * docs: another pass
    
    * fix: style
    
    * fix: test for updated build openapi plugin example
    
    * fix: absolute paths for openapi.json
    
    Resolves #3047 for the openapi router case.
    
    * refactor: simplify scalar plugin.
    
    * fix: linting
    
    * Update litestar/_openapi/plugin.py
    
    * refactor: test app to use scalar ui plugin
    
    * fix: plugin customization example
    
    Version arg is ignored if `js_url` is provided.
    
    * fix: remove unnecessary kwargs
    
    Removes passing default values to plugin kwargs in examples.
    
    * fix: grammar error
    
    * feat: make OpenAPIRenderPlugin an ABC
    
    Abstract on `render()` method.
    
    * fix: correct referenced lines
    
    Referenced LoC in example had drifted.
    
    * fix: more small docs corrections
    
    * chore: remove dup spec of enabled endpoints.
    
    * fix: simplify test.
    
    ---------
    
    Co-authored-by: Jacob Coffee <jacob@z7x.org>

--- tests/unit/test_openapi/conftest.py
+++ tests/unit/test_openapi/conftest.py
@@ -6,6 +6,7 @@ import pytest
 from litestar import Controller, MediaType, delete, get, patch, post, put
 from litestar.datastructures import ResponseHeader, State
 from litestar.dto import DataclassDTO, DTOConfig, DTOData
+from litestar.openapi.controller import OpenAPIController
 from litestar.openapi.spec.example import Example
 from litestar.params import Parameter
 from tests.models import DataclassPerson, DataclassPersonFactory, DataclassPet
@@ -20,7 +21,7 @@ def create_person_controller() -> Type[Controller]:
     class PersonController(Controller):
         path = "/{service_id:int}/person"
 
-        @get()
+        @get(sync_to_thread=False)
         def get_persons(
             self,
             # expected to be ignored
@@ -53,41 +54,46 @@ def create_person_controller() -> Type[Controller]:
         ) -> List[DataclassPerson]:
             return []
 
-        @post(media_type=MediaType.TEXT)
+        @post(media_type=MediaType.TEXT, sync_to_thread=False)
         def create_person(
             self, data: DataclassPerson, secret_header: str = Parameter(header="secret")
         ) -> DataclassPerson:
             return data
 
-        @post(path="/bulk", dto=PartialDataclassPersonDTO)
+        @post(path="/bulk", dto=PartialDataclassPersonDTO, sync_to_thread=False)
         def bulk_create_person(
             self, data: DTOData[List[DataclassPerson]], secret_header: str = Parameter(header="secret")
         ) -> List[DataclassPerson]:
             return []
 
-        @put(path="/bulk")
+        @put(path="/bulk", sync_to_thread=False)
         def bulk_update_person(
             self, data: List[DataclassPerson], secret_header: str = Parameter(header="secret")
         ) -> List[DataclassPerson]:
             return []
 
-        @patch(path="/bulk", dto=PartialDataclassPersonDTO)
+        @patch(path="/bulk", dto=PartialDataclassPersonDTO, sync_to_thread=False)
         def bulk_partial_update_person(
             self, data: DTOData[List[DataclassPerson]], secret_header: str = Parameter(header="secret")
         ) -> List[DataclassPerson]:
             return []
 
-        @get(path="/{person_id:str}")
+        @get(path="/{person_id:str}", sync_to_thread=False)
         def get_person_by_id(self, person_id: str) -> DataclassPerson:
             """Description in docstring."""
             return DataclassPersonFactory.build(id=person_id)
 
-        @patch(path="/{person_id:str}", description="Description in decorator", dto=PartialDataclassPersonDTO)
+        @patch(
+            path="/{person_id:str}",
+            description="Description in decorator",
+            dto=PartialDataclassPersonDTO,
+            sync_to_thread=False,
+        )
         def partial_update_person(self, person_id: str, data: DTOData[DataclassPerson]) -> DataclassPerson:
             """Description in docstring."""
             return DataclassPersonFactory.build(id=person_id)
 
-        @put(path="/{person_id:str}")
+        @put(path="/{person_id:str}", sync_to_thread=False)
         def update_person(self, person_id: str, data: DataclassPerson) -> DataclassPerson:
             """Multiline docstring example.
 
@@ -95,11 +101,11 @@ def create_person_controller() -> Type[Controller]:
             """
             return data
 
-        @delete(path="/{person_id:str}")
+        @delete(path="/{person_id:str}", sync_to_thread=False)
         def delete_person(self, person_id: str) -> None:
             return None
 
-        @get(path="/dataclass")
+        @get(path="/dataclass", sync_to_thread=False)
         def get_person_dataclass(self) -> DataclassPerson:
             return DataclassPerson(
                 first_name="Moishe", last_name="zuchmir", id="1", optional=None, complex={}, pets=None
@@ -112,12 +118,15 @@ def create_pet_controller() -> Type[Controller]:
     class PetController(Controller):
         path = "/pet"
 
-        @get()
+        @get(sync_to_thread=False)
         def pets(self) -> List[DataclassPet]:
             return []
 
         @get(
-            path="/owner-or-pet", response_headers=[ResponseHeader(name="x-my-tag", value="123")], raises=[PetException]
+            path="/owner-or-pet",
+            response_headers=[ResponseHeader(name="x-my-tag", value="123")],
+            raises=[PetException],
+            sync_to_thread=False,
         )
         def get_pets_or_owners(self) -> List[Union[DataclassPerson, DataclassPet]]:
             return []
@@ -135,3 +144,8 @@ def person_controller() -> Type[Controller]:
 @pytest.mark.usefixtures("disable_warn_implicit_sync_to_thread")
 def pet_controller() -> Type[Controller]:
     return create_pet_controller()
+
+
+@pytest.fixture(params=[OpenAPIController, None])
+def openapi_controller(request: pytest.FixtureRequest) -> Optional[Type[OpenAPIController]]:
+    return request.param  # type: ignore[no-any-return]
