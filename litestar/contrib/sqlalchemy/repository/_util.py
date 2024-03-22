5546eb83f74e50a64bd2665c0aefd49b66614e76
James Bennett <james@b-list.org>
2024-03-22 00:10:44 -0700

    fix: Handle move of `wrap_sqlalchemy_exception()` (#3238)
    In advanced-alchemy prior to 0.8.0, this function is found in
    `advanced_alchemy.repository._util`, but as of 0.8.0 it moves to
    `advanced_alchemy.exceptions`.

--- litestar/contrib/sqlalchemy/repository/_util.py
+++ litestar/contrib/sqlalchemy/repository/_util.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
-from advanced_alchemy.repository._util import get_instrumented_attr, wrap_sqlalchemy_exception
+from advanced_alchemy.repository._util import get_instrumented_attr
+
+try:
+    from advanced_alchemy.exceptions import wrap_sqlalchemy_exception
+except ImportError:
+    from advanced_alchemy.repository._util import wrap_sqlalchemy_exception
+
 
 __all__ = (
     "wrap_sqlalchemy_exception",
