45d25232bd26949300a4bc2fefeeeb6898099d22
Peter Schutt <peter.github@proton.me>
2024-03-31 17:54:53 +1000

    feat: Pydantic dto non instantiable types (#3296)
    feat: pydantic DTO with non-instantiable types.
    
    This PR simplifies the type that we apply to transfer models for pydantic field types in specific circumstances.
    
    - `JsonValue`: this field type is an instance of `TypeAliasType` at runtime, and contains recursive type definitions. Pydantic allow `list`, `dict`, `str`, `bool`, `int`, `float` and `None`, and the value types of `list` and `dict` are allowed to be the same. We type this as `Any` on the transfer models as this is pretty much the same thing for msgspec ([ref][1]).
    - `EmailStr`. These are typed as `EmailStr` which is a class at runtime which is not a `str`, however they are a string and `if TYPE_CHECKING` is used to get type checkers to play along. If we return a `str` from a msgspec decode hook for the type, it msgspec won't validate the input. So we must tell msgspec its a string. This also works well with encoding because it is one.
    - IP/Network/Interface types. These are represented by types such as `IPvAnyAddress`, but they are actually parsed into instances of stdlib types such as `IPv4Address` and others from the `ipaddress` module. Given that an instance of `IPvAnyAddress` cannot be meaningfully returned from a decoding hook, we have to tell msgspec that these are strings on the transfer models. Encoding the stdlib ip types to str is natively handled by msgspec.
    
    Closes #2334
    
    1: https://jcristharif.com/msgspec/supported-types.html#any

--- tests/unit/test_contrib/test_pydantic/__init__.py
+++ tests/unit/test_contrib/test_pydantic/__init__.py
@@ -1,3 +1,11 @@
-from typing import Literal
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal
+
+if TYPE_CHECKING:
+    import pydantic as pydantic_v2
+    from pydantic import v1 as pydantic_v1
+    from typing_extensions import TypeAlias
 
 PydanticVersion = Literal["v1", "v2"]
+BaseModelType: TypeAlias = "type[pydantic_v1.BaseModel| pydantic_v2.BaseModel]"
