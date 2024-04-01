eb3e8805426b5ebc14ab24e7dc20c0100bfe9b9a
guacs <guacs.guacs@gmail.com>
2024-04-01 14:23:18 +0530

    refactor: respect HTTPException arguments, and extract more information from HTTPException

--- litestar/plugins/problem_details.py
+++ litestar/plugins/problem_details.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Any, Callable, Mapping, TypeVar
 
 from litestar.connection.request import Request
 from litestar.exceptions.http_exceptions import HTTPException
 from litestar.plugins.base import InitPluginProtocol
 from litestar.response.base import Response
-from litestar.serialization.msgspec_hooks import encode_json
 from litestar.types.callable_types import ExceptionHandler, ExceptionT
 
 if TYPE_CHECKING:
@@ -33,58 +32,67 @@ def _create_exception_handler(
     return _exception_handler
 
 
+def _http_exception_to_problem_detail_exception(exc: HTTPException) -> ProblemDetailsException:
+    return ProblemDetailsException(status_code=exc.status_code, detail=exc.detail, extra=exc.extra, headers=exc.headers)
+
+
 class ProblemDetailsException(HTTPException):
-    _PROBLEM_DETAIL_HEADER = "application/problem+json"
+    """A problem details exception as per RFC 9457."""
+
+    _PROBLEM_DETAIL_MEDIA_TYPE = "application/problem+json"
 
     def __init__(
         self,
-        type_: str = "about:blank",
+        *args: Any,
+        detail: str = "",
         status_code: int | None = None,
+        headers: dict[str, str] | None = None,
+        extra: dict[str, Any] | list[Any] | None = None,
+        type_: str = "about: blank",
         title: str | None = None,
         instance: str | None = None,
-        detail: str | dict[str, Any] | list[Any] | None = None,
-        extra: dict[str, Any] | None = None,
-        headers: dict[str, Any] | None = None,
     ) -> None:
+        """Initialize ``ProblemDetailsException``.
+
+        Args:
+            *args: if ``detail`` kwarg not provided, first arg should be error detail.
+            detail: Exception details or message. Will default to args[0] if not provided.
+            status_code: Exception HTTP status code.
+            headers: Headers to set on the response.
+            extra: An extra mapping to attach to the exception.
+            type_: The type field in the problem details.
+            title: The title field in the problem details.
+            instance: The instance field in the problem details.
+        """
+
+        super().__init__(*args, detail=detail, status_code=status_code, headers=headers, extra=extra)
+
         self.type_ = type_
-        self.status_code = status_code or self.status_code
         self.title = title
         self.instance = instance
-        self.detail = detail  # type: ignore
-        self.extra = extra
-        self.headers = headers
 
     def to_response(self, request: Request) -> Response:
+        """Convert the problem details exception into a ``Response.``"""
+
         problem_details = {"type": self.type_, "status": self.status_code}
-        if self.title is not None:
-            problem_details["title"] = self.title
-        if self.instance is not None:
-            problem_details["instance"] = self.instance
-        if self.detail is not None:
-            problem_details["detail"] = self.detail
-        if self.extra is not None:
-            problem_details.update(self.extra)
+        for attr in ("title", "instance", "detail", "extra"):
+            if (value := getattr(self, attr, None)) is not None:
+                problem_details[attr] = value
 
-        content = encode_json(problem_details, request.route_handler.default_serializer)
-        headers = self.headers or {}
-        headers["content-type"] = self._PROBLEM_DETAIL_HEADER
-
-        return Response(content, headers=headers)
-
-    @classmethod
-    def from_http_exception(cls, exc: HTTPException) -> ProblemDetailsException:
-        return ProblemDetailsException(status_code=exc.status_code)
+        return Response(problem_details, headers=self.headers, media_type=self._PROBLEM_DETAIL_MEDIA_TYPE)
 
 
 @dataclass
 class ProblemDetailsConfig:
+    """The configuration object for ``ProblemDetailsPlugin.``"""
+
     exception_handler: ProblemDetailsExceptionHandlerType = _problem_details_exception_handler
     """The exception handler used for ``ProblemdetailsException.``"""
 
     enable_for_all_http_exceptions: bool = False
     """Flag indicating whether to convert all exceptions into ``ProblemDetailsException.``"""
 
-    exception_to_problem_detail_map: ExceptionToProblemDetailMapType | None = None
+    exception_to_problem_detail_map: ExceptionToProblemDetailMapType = field(default_factory=dict)
     """A mapping to convert exceptions into ``ProblemDetailsException.``
 
     All exceptions provided in this will get a custom exception handler where these exceptions
@@ -93,6 +101,8 @@ class ProblemDetailsConfig:
 
 
 class ProblemDetailsPlugin(InitPluginProtocol):
+    """A plugin to convert exceptions into problem details as per RFC 9457."""
+
     def __init__(self, config: ProblemDetailsConfig | None = None):
         self.config = config or ProblemDetailsConfig()
 
@@ -101,11 +111,10 @@ class ProblemDetailsPlugin(InitPluginProtocol):
 
         if self.config.enable_for_all_http_exceptions:
             app_config.exception_handlers[HTTPException] = _create_exception_handler(
-                ProblemDetailsException.from_http_exception, HTTPException
+                _http_exception_to_problem_detail_exception, HTTPException
             )
 
-        if conversion_map := self.config.exception_to_problem_detail_map:
-            for exc_type, conversion_fn in conversion_map.items():
-                app_config.exception_handlers[exc_type] = _create_exception_handler(conversion_fn, exc_type)
+        for exc_type, conversion_fn in self.config.exception_to_problem_detail_map.items():
+            app_config.exception_handlers[exc_type] = _create_exception_handler(conversion_fn, exc_type)
 
         return app_config
