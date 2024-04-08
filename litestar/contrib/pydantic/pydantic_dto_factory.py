litestar-org/litestar

commit 45d25232bd26949300a4bc2fefeeeb6898099d22

feat: Pydantic dto non instantiable types (#3296)
feat: pydantic DTO with non-instantiable types.

This PR simplifies the type that we apply to transfer models for pydantic field types in specific circumstances.

- `JsonValue`: this field type is an instance of `TypeAliasType` at runtime, and contains recursive type definitions. Pydantic allow `list`, `dict`, `str`, `bool`, `int`, `float` and `None`, and the value types of `list` and `dict` are allowed to be the same. We type this as `Any` on the transfer models as this is pretty much the same thing for msgspec ([ref][1]).
- `EmailStr`. These are typed as `EmailStr` which is a class at runtime which is not a `str`, however they are a string and `if TYPE_CHECKING` is used to get type checkers to play along. If we return a `str` from a msgspec decode hook for the type, it msgspec won't validate the input. So we must tell msgspec its a string. This also works well with encoding because it is one.
- IP/Network/Interface types. These are represented by types such as `IPvAnyAddress`, but they are actually parsed into instances of stdlib types such as `IPv4Address` and others from the `ipaddress` module. Given that an instance of `IPvAnyAddress` cannot be meaningfully returned from a decoding hook, we have to tell msgspec that these are strings on the transfer models. Encoding the stdlib ip types to str is natively handled by msgspec.

Closes #2334

1: https://jcristharif.com/msgspec/supported-types.html#any

litestar/contrib/pydantic/pydantic_dto_factory.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
 from dataclasses import replace
-from typing import TYPE_CHECKING, Collection, Generic, TypeVar
+from typing import TYPE_CHECKING, Any, Collection, Generic, TypeVar
 from warnings import warn
 
-from typing_extensions import TypeAlias, override
+from typing_extensions import Annotated, TypeAlias, override
 
 from litestar.contrib.pydantic.utils import is_pydantic_undefined
 from litestar.dto.base_dto import AbstractDTO
@@ -12,11 +12,10 @@ from litestar.dto.data_structures import DTOFieldDefinition
 from litestar.dto.field import DTO_FIELD_META_KEY, extract_dto_field
 from litestar.exceptions import MissingDependencyException, ValidationException
 from litestar.types.empty import Empty
+from litestar.typing import FieldDefinition
 
 if TYPE_CHECKING:
-    from typing import Any, Generator
-
-    from litestar.typing import FieldDefinition
+    from typing import Generator
 
 try:
     import pydantic as _  # noqa: F401
@@ -47,6 +46,18 @@ T = TypeVar("T", bound="ModelType | Collection[ModelType]")
 
 __all__ = ("PydanticDTO",)
 
+_down_types = {
+    pydantic_v2.JsonValue: Any,
+    pydantic_v1.EmailStr: str,
+    pydantic_v2.EmailStr: str,
+    pydantic_v1.IPvAnyAddress: str,
+    pydantic_v2.IPvAnyAddress: str,
+    pydantic_v1.IPvAnyInterface: str,
+    pydantic_v2.IPvAnyInterface: str,
+    pydantic_v1.IPvAnyNetwork: str,
+    pydantic_v2.IPvAnyNetwork: str,
+}
+
 
 def convert_validation_error(validation_error: ValidationErrorV1 | ValidationErrorV2) -> list[dict[str, Any]]:
     error_list = validation_error.errors()
@@ -54,6 +65,14 @@ def convert_validation_error(validation_error: ValidationErrorV1 | ValidationErr
         if isinstance(exception := error.get("ctx", {}).get("error"), Exception):
             error["ctx"]["error"] = type(exception).__name__
     return error_list  # type: ignore[return-value]
+
+
+def downtype_for_data_transfer(field_definition: FieldDefinition) -> FieldDefinition:
+    if sub := _down_types.get(field_definition.annotation):
+        return FieldDefinition.from_kwarg(
+            annotation=Annotated[sub, field_definition.metadata], name=field_definition.name
+        )
+    return field_definition
 
 
 class PydanticDTO(AbstractDTO[T], Generic[T]):
@@ -89,7 +108,7 @@ class PydanticDTO(AbstractDTO[T], Generic[T]):
             }
 
         for field_name, field_info in model_fields.items():
-            field_definition = model_field_definitions[field_name]
+            field_definition = downtype_for_data_transfer(model_field_definitions[field_name])
             dto_field = extract_dto_field(field_definition, field_definition.extra)
 
             try:

tests/unit/test_contrib/test_pydantic/__init__.py
@@ -1,3 +1,11 @@
-from typing import Literal
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal
+
+if TYPE_CHECKING:
+    import pydantic as pydantic_v2
+    from pydantic import v1 as pydantic_v1
+    from typing_extensions import TypeAlias
 
 PydanticVersion = Literal["v1", "v2"]
+BaseModelType: TypeAlias = "type[pydantic_v1.BaseModel| pydantic_v2.BaseModel]"

tests/unit/test_contrib/test_pydantic/test_integration.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Type, Union
+from typing import Any, Dict, List
 
 import pydantic as pydantic_v2
 import pytest
@@ -13,7 +13,7 @@ from litestar.status_codes import HTTP_400_BAD_REQUEST
 from litestar.testing import create_test_client
 from tests.unit.test_contrib.test_pydantic.models import PydanticPerson, PydanticV1Person
 
-from . import PydanticVersion
+from . import BaseModelType, PydanticVersion
 
 
 @pytest.mark.parametrize(("meta",), [(None,), (Body(media_type=RequestEncodingType.URL_ENCODED),)])
@@ -140,9 +140,7 @@ def test_serialize_raw_errors_v2() -> None:
         ]
 
 
-def test_signature_model_invalid_input(
-    base_model: Type[Union[pydantic_v2.BaseModel, pydantic_v1.BaseModel]], pydantic_version: PydanticVersion
-) -> None:
+def test_signature_model_invalid_input(base_model: BaseModelType, pydantic_version: PydanticVersion) -> None:
     class OtherChild(base_model):  # type: ignore[misc, valid-type]
         val: List[int]
 
@@ -221,7 +219,7 @@ class V2ModelWithPrivateFields(pydantic_v2.BaseModel):
 
 
 @pytest.mark.parametrize("model_type", [V1ModelWithPrivateFields, V2ModelWithPrivateFields])
-def test_private_fields(model_type: Type[Union[pydantic_v1.BaseModel, pydantic_v2.BaseModel]]) -> None:
+def test_private_fields(model_type: BaseModelType) -> None:
     @post("/")
     async def handler(data: V2ModelWithPrivateFields) -> V2ModelWithPrivateFields:
         return data
@@ -230,3 +228,79 @@ def test_private_fields(model_type: Type[Union[pydantic_v1.BaseModel, pydantic_v
         res = client.post("/", json={"bar": "value"})
         assert res.status_code == 201
         assert res.json() == {"bar": "value"}
+
+
+@pytest.mark.parametrize(
+    ("base_model", "type_", "in_"),
+    [
+        pytest.param(pydantic_v2.BaseModel, pydantic_v2.JsonValue, {"foo": "bar"}, id="pydantic_v2.JsonValue"),
+        pytest.param(
+            pydantic_v1.BaseModel, pydantic_v1.IPvAnyAddress, "127.0.0.1", id="pydantic_v1.IPvAnyAddress (v4)"
+        ),
+        pytest.param(
+            pydantic_v2.BaseModel, pydantic_v2.IPvAnyAddress, "127.0.0.1", id="pydantic_v2.IPvAnyAddress (v4)"
+        ),
+        pytest.param(
+            pydantic_v1.BaseModel,
+            pydantic_v1.IPvAnyAddress,
+            "2001:db8::ff00:42:8329",
+            id="pydantic_v1.IPvAnyAddress (v6)",
+        ),
+        pytest.param(
+            pydantic_v2.BaseModel,
+            pydantic_v2.IPvAnyAddress,
+            "2001:db8::ff00:42:8329",
+            id="pydantic_v2.IPvAnyAddress (v6)",
+        ),
+        pytest.param(
+            pydantic_v1.BaseModel, pydantic_v1.IPvAnyInterface, "127.0.0.1/24", id="pydantic_v1.IPvAnyInterface (v4)"
+        ),
+        pytest.param(
+            pydantic_v2.BaseModel, pydantic_v2.IPvAnyInterface, "127.0.0.1/24", id="pydantic_v2.IPvAnyInterface (v4)"
+        ),
+        pytest.param(
+            pydantic_v1.BaseModel,
+            pydantic_v1.IPvAnyInterface,
+            "2001:db8::ff00:42:8329/128",
+            id="pydantic_v1.IPvAnyInterface (v6)",
+        ),
+        pytest.param(
+            pydantic_v2.BaseModel,
+            pydantic_v2.IPvAnyInterface,
+            "2001:db8::ff00:42:8329/128",
+            id="pydantic_v2.IPvAnyInterface (v6)",
+        ),
+        pytest.param(
+            pydantic_v1.BaseModel, pydantic_v1.IPvAnyNetwork, "127.0.0.1/32", id="pydantic_v1.IPvAnyNetwork (v4)"
+        ),
+        pytest.param(
+            pydantic_v2.BaseModel, pydantic_v2.IPvAnyNetwork, "127.0.0.1/32", id="pydantic_v2.IPvAnyNetwork (v4)"
+        ),
+        pytest.param(
+            pydantic_v1.BaseModel,
+            pydantic_v1.IPvAnyNetwork,
+            "2001:db8::ff00:42:8329/128",
+            id="pydantic_v1.IPvAnyNetwork (v6)",
+        ),
+        pytest.param(
+            pydantic_v2.BaseModel,
+            pydantic_v2.IPvAnyNetwork,
+            "2001:db8::ff00:42:8329/128",
+            id="pydantic_v2.IPvAnyNetwork (v6)",
+        ),
+        pytest.param(pydantic_v1.BaseModel, pydantic_v1.EmailStr, "test@example.com", id="pydantic_v1.EmailStr"),
+        pytest.param(pydantic_v2.BaseModel, pydantic_v2.EmailStr, "test@example.com", id="pydantic_v2.EmailStr"),
+    ],
+)
+def test_dto_with_non_instantiable_types(base_model: BaseModelType, type_: Any, in_: Any) -> None:
+    class Model(base_model):  # type: ignore[misc, valid-type]
+        foo: type_
+
+    @post("/", dto=PydanticDTO[Model])
+    async def handler(data: Model) -> Model:
+        return data
+
+    with create_test_client(handler) as client:
+        res = client.post("/", json={"foo": in_})
+        assert res.status_code == 201
+        assert res.json() == {"foo": in_}

commit 9a4acef9233efb68df931883e7319d7f3e834f48

fix: bug when pydantic==1.10 is installed
This PR fixes a bug introduced in #3296 where it failed to take into account that the `pydantic_v2` variable could be `Empty`.

Closes #3334

litestar/contrib/pydantic/pydantic_dto_factory.py
@@ -46,17 +46,23 @@ T = TypeVar("T", bound="ModelType | Collection[ModelType]")
 
 __all__ = ("PydanticDTO",)
 
-_down_types = {
-    pydantic_v2.JsonValue: Any,
+_down_types: dict[Any, Any] = {
     pydantic_v1.EmailStr: str,
-    pydantic_v2.EmailStr: str,
     pydantic_v1.IPvAnyAddress: str,
-    pydantic_v2.IPvAnyAddress: str,
     pydantic_v1.IPvAnyInterface: str,
-    pydantic_v2.IPvAnyInterface: str,
     pydantic_v1.IPvAnyNetwork: str,
-    pydantic_v2.IPvAnyNetwork: str,
 }
+
+if pydantic_v2 is not Empty:  # type: ignore[comparison-overlap]  # pragma: no cover
+    _down_types.update(
+        {
+            pydantic_v2.JsonValue: Any,
+            pydantic_v2.EmailStr: str,
+            pydantic_v2.IPvAnyAddress: str,
+            pydantic_v2.IPvAnyInterface: str,
+            pydantic_v2.IPvAnyNetwork: str,
+        }
+    )
 
 
 def convert_validation_error(validation_error: ValidationErrorV1 | ValidationErrorV2) -> list[dict[str, Any]]: