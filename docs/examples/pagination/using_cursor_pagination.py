20d892e1a6b81030c5fccfdce3632648d444773f
Peter Schutt <peter.github@proton.me>
2024-04-11 06:35:27 +1000

    feat!: no implicit default for optional params (#3361)
    This PR changes behavior of parameters that are typed in a union with `None`. Prior behavior was to implicitly default their value to `None` if a value wasn't provided making an apparently non-optional parameter (i.e., no declared default) actually optional. Surprising behavior at best, dangerous at worst.
    
    New behavior is to throw a client error when a parameter without a default is not provided.

--- docs/examples/pagination/using_cursor_pagination.py
+++ docs/examples/pagination/using_cursor_pagination.py
@@ -34,7 +34,7 @@ paginator = PersonCursorPaginator()
 # we now create a regular handler. The handler will receive a single query parameter - 'cursor', which
 # we will pass to the paginator.
 @get("/people", sync_to_thread=False)
-def people_handler(cursor: Optional[str], results_per_page: int) -> CursorPagination[str, Person]:
+def people_handler(*, cursor: Optional[str] = None, results_per_page: int) -> CursorPagination[str, Person]:
     return paginator(cursor=cursor, results_per_page=results_per_page)
 
 
