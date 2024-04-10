05023f25c17bc56a084381c1b7b42fa4febe78bd
Janek Nouvertné <provinzkraut@posteo.de>
2024-04-10 19:41:30 +0200

    refactor!: Remove deprecated `StaticFileConfig` (#3357)
    * Remove deprecated StaticFiles and StaticFilesConfig
    * remove special casing of static files app from handlers/router
    * remove outdated docs sections
    * Add what's new section
    * Rename tests for consistency

--- litestar/app.py
+++ litestar/app.py
@@ -44,6 +44,7 @@ from litestar.plugins import (
 )
 from litestar.plugins.base import CLIPlugin
 from litestar.router import Router
+from litestar.routes import ASGIRoute, HTTPRoute, WebSocketRoute
 from litestar.stores.registry import StoreRegistry
 from litestar.types import Empty, TypeDecodersSequence
 from litestar.types.internal_types import PathParameterDefinition, TemplateConfigType
@@ -66,7 +67,6 @@ if TYPE_CHECKING:
     from litestar.openapi.spec import SecurityRequirement
     from litestar.openapi.spec.open_api import OpenAPI
     from litestar.response import Response
-    from litestar.routes import ASGIRoute, HTTPRoute, WebSocketRoute
     from litestar.stores.base import Store
     from litestar.types import (
         AfterExceptionHookHandler,
