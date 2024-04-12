7ce60c5ddc14c8e42b496007c61b1f6fd5daeb0f
kedod <kedod>
2024-04-12 12:02:41 +0200

    Debug CI

--- tests/examples/test_data_transfer_objects/test_defining_dtos_on_layers.py
+++ tests/examples/test_data_transfer_objects/test_defining_dtos_on_layers.py
@@ -2,6 +2,7 @@ from unittest.mock import ANY
 
 from docs.examples.data_transfer_objects.defining_dtos_on_layers import app
 
+from litestar.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
 from litestar.testing.client.sync_client import TestClient
 
 app.debug = True
@@ -11,6 +12,7 @@ def test_create_user(user_data: dict) -> None:
     with TestClient(app=app) as client:
         response = client.post("/", json=user_data)
 
+    assert response.status_code == HTTP_201_CREATED
     assert response.json() == {"id": ANY, "name": "Mr Sunglass", "email": "mr.sunglass@example.com", "age": 30}
 
 
@@ -18,6 +20,7 @@ def test_get_users() -> None:
     with TestClient(app=app) as client:
         response = client.get("/")
 
+    assert response.status_code == HTTP_200_OK
     assert response.json() == [{"id": ANY, "name": "Mr Sunglass", "email": "mr.sunglass@example.com", "age": 30}]
 
 
@@ -25,6 +28,7 @@ def test_get_user() -> None:
     with TestClient(app=app) as client:
         response = client.get("a3cad591-5b01-4341-ae8f-94f78f790674")
 
+    assert response.status_code == HTTP_200_OK
     assert response.json() == {
         "id": "a3cad591-5b01-4341-ae8f-94f78f790674",
         "name": "Mr Sunglass",
@@ -37,6 +41,7 @@ def test_update_user(user_data: dict) -> None:
     with TestClient(app=app) as client:
         response = client.put("/a3cad591-5b01-4341-ae8f-94f78f790674", json=user_data)
 
+    assert response.status_code == HTTP_200_OK
     assert response.json() == {"id": ANY, "name": "Mr Sunglass", "email": "mr.sunglass@example.com", "age": 30}
 
 
@@ -44,4 +49,5 @@ def test_delete_user() -> None:
     with TestClient(app=app) as client:
         response = client.delete("/a3cad591-5b01-4341-ae8f-94f78f790674")
 
+    assert response.status_code == HTTP_204_NO_CONTENT
     assert response.content == b""
