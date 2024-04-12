8801f69c3c92ba87a0db68b4c8778ed613e699f1
kedod <kedod>
2024-04-12 12:11:25 +0200

    Debug CI

--- docs/examples/data_transfer_objects/defining_dtos_on_layers.py
+++ docs/examples/data_transfer_objects/defining_dtos_on_layers.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from dataclasses import dataclass, field
 from typing import List
 from uuid import UUID, uuid4
--- tests/examples/test_data_transfer_objects/test_defining_dtos_on_layers.py
+++ tests/examples/test_data_transfer_objects/test_defining_dtos_on_layers.py
@@ -1,15 +1,13 @@
 from unittest.mock import ANY
 
-from docs.examples.data_transfer_objects.defining_dtos_on_layers import app
+from docs.examples.data_transfer_objects.defining_dtos_on_layers import UserController
 
 from litestar.status_codes import HTTP_200_OK, HTTP_201_CREATED, HTTP_204_NO_CONTENT
-from litestar.testing.client.sync_client import TestClient
-
-app.debug = True
+from litestar.testing import create_test_client
 
 
 def test_create_user(user_data: dict) -> None:
-    with TestClient(app=app) as client:
+    with create_test_client(UserController) as client:
         response = client.post("/", json=user_data)
 
     assert response.status_code == HTTP_201_CREATED
@@ -17,7 +15,7 @@ def test_create_user(user_data: dict) -> None:
 
 
 def test_get_users() -> None:
-    with TestClient(app=app) as client:
+    with create_test_client(UserController) as client:
         response = client.get("/")
 
     assert response.status_code == HTTP_200_OK
@@ -25,8 +23,8 @@ def test_get_users() -> None:
 
 
 def test_get_user() -> None:
-    with TestClient(app=app) as client:
-        response = client.get("a3cad591-5b01-4341-ae8f-94f78f790674")
+    with create_test_client(UserController) as client:
+        response = client.get("/a3cad591-5b01-4341-ae8f-94f78f790674")
 
     assert response.status_code == HTTP_200_OK
     assert response.json() == {
@@ -38,7 +36,7 @@ def test_get_user() -> None:
 
 
 def test_update_user(user_data: dict) -> None:
-    with TestClient(app=app) as client:
+    with create_test_client(UserController) as client:
         response = client.put("/a3cad591-5b01-4341-ae8f-94f78f790674", json=user_data)
 
     assert response.status_code == HTTP_200_OK
@@ -46,7 +44,7 @@ def test_update_user(user_data: dict) -> None:
 
 
 def test_delete_user() -> None:
-    with TestClient(app=app) as client:
+    with create_test_client(UserController) as client:
         response = client.delete("/a3cad591-5b01-4341-ae8f-94f78f790674")
 
     assert response.status_code == HTTP_204_NO_CONTENT
