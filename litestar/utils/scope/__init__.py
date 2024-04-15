c4aaa6f9ca2eb9e97bd0ebc47946cf556a5bae75
Peter Schutt <peter.github@proton.me>
2024-04-15 19:57:06 +1000

    refactor!: Remove deprecated utils (#3394)
    Remove the deprecated utility functions, `get_litestar_scope_state``, ``set_litestar_scope_state``, ``delete_litestar_scope_state``, and ``is_sync_or_async_generator``.

--- litestar/utils/scope/__init__.py
+++ litestar/utils/scope/__init__.py
@@ -1,12 +1,8 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any
+from typing import TYPE_CHECKING
 
 from litestar.serialization import get_serializer
-from litestar.utils.deprecation import warn_deprecation
-from litestar.utils.scope.state import delete_litestar_scope_state as _delete_litestar_scope_state
-from litestar.utils.scope.state import get_litestar_scope_state as _get_litestar_scope_state
-from litestar.utils.scope.state import set_litestar_scope_state as _set_litestar_scope_state
 
 if TYPE_CHECKING:
     from litestar.types import Scope, Serializer
@@ -39,24 +35,3 @@ def get_serializer_from_scope(scope: Scope) -> Serializer:
         type_encoders = {**type_encoders, **(response_class.type_encoders or {})}
 
     return get_serializer(type_encoders)
-
-
-_deprecated_names = {
-    "get_litestar_scope_state": _get_litestar_scope_state,
-    "set_litestar_scope_state": _set_litestar_scope_state,
-    "delete_litestar_scope_state": _delete_litestar_scope_state,
-}
-
-
-def __getattr__(name: str) -> Any:
-    if name in _deprecated_names:
-        warn_deprecation(
-            deprecated_name=f"litestar.utils.scope.{name}",
-            version="2.4",
-            kind="import",
-            removal_in="3.0",
-            info=f"'litestar.utils.scope.{name}' is deprecated. The Litestar scope state is private and should not be "
-            f"used. Plugin authors should maintain their own scope state namespace.",
-        )
-        return globals()["_deprecated_names"][name]
-    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")  # pragma: no cover
