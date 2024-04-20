baabd7012a675f74e438ffcac47129377097b1ab
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-04-20 12:09:13 +0200

    Fix optional sequence in multipart data validation

--- tests/unit/test_kwargs/test_multipart_data.py
+++ tests/unit/test_kwargs/test_multipart_data.py
@@ -415,10 +415,17 @@ class Files:
     file_list: List[UploadFile]
 
 
+# https://github.com/litestar-org/litestar/issues/3407
+@dataclass
+class OptionalFiles:
+    file_list: Optional[List[UploadFile]]
+
+
+@pytest.mark.parametrize("file_model", (Files, OptionalFiles))
 @pytest.mark.parametrize("file_count", (1, 2))
-def test_upload_multiple_files_in_model(file_count: int) -> None:
-    @post("/")
-    async def handler(data: Files = Body(media_type=RequestEncodingType.MULTI_PART)) -> None:
+def test_upload_multiple_files_in_model(file_count: int, file_model: type) -> None:
+    @post("/", signature_namespace={"file_model": file_model})
+    async def handler(data: file_model = Body(media_type=RequestEncodingType.MULTI_PART)) -> None:
         assert len(data.file_list) == file_count
 
         for file in data.file_list:
