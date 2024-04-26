443682fa8f60bd8b1a7097bf00341715a106bd73
Peter Schutt <peter.github@proton.me>
2024-04-26 19:49:23 +1000

    fix: regular handler under mounted app
    Fix an issue where a regular handler under a mounted asgi app would prevent a request routing through the mounted application if the request path contained the path of the regular handler as a substring.

---
+++ tests/e2e/test_regular_handler_under_asgi_mount_path.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from litestar import Litestar, asgi, get
+from litestar.testing import TestClient
+
+if TYPE_CHECKING:
+    from litestar.types.asgi_types import Receive, Scope, Send
+
+
+async def asgi_app(scope: Scope, receive: Receive, send: Send) -> None:
+    assert scope["type"] == "http"
+    await send(
+        {
+            "type": "http.response.start",
+            "status": 200,
+            "headers": [
+                (b"content-type", b"text/plain"),
+                (b"content-length", b"%d" % len(scope["raw_path"])),
+            ],
+        }
+    )
+    await send(
+        {
+            "type": "http.response.body",
+            "body": scope["raw_path"],
+        }
+    )
+
+
+asgi_handler = asgi("/", is_mount=True)(asgi_app)
+
+
+@get("/path")
+async def get_handler() -> str:
+    return "Hello, world!"
+
+
+def test_regular_handler_under_mounted_asgi_app() -> None:
+    app = Litestar(
+        route_handlers=[asgi("/", is_mount=True)(asgi_app), get_handler],
+        openapi_config=None,
+        debug=True,
+    )
+
+    with TestClient(app) as client:
+        resp = client.get("/some/path")
+        assert resp.content == b"/some/path"
