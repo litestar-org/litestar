d0aa1a789377142085e49fd975970345c85752be
kedod <kedod>
2024-04-30 10:35:36 +0200

    Revert "Try to fix tests"
    This reverts commit 8515e2aeb9b7302fa92c7aa509bdb3b58666199d.

--- tests/examples/test_data_transfer_objects/test_overriding_implicit_return_dto.py
+++ tests/examples/test_data_transfer_objects/test_overriding_implicit_return_dto.py
@@ -1,10 +1,7 @@
-import pytest
-
 from litestar.status_codes import HTTP_201_CREATED
 from litestar.testing.client import TestClient
 
 
-@pytest.mark.xdist_group(name="doc-examples")
 def test_create_user(user_data: dict) -> None:
     from docs.examples.data_transfer_objects.overriding_implicit_return_dto import app
 
