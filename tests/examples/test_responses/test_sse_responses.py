cacf38f16a9719ae72fa3215f07edc0ecd22f6c3
euri10 <benoit.barthelet@gmail.com>
2024-04-30 18:13:16 +0200

    docs: improve sse (#3454)
    * docs: improve sse ?
    
    * removed some assert on the test example following its modification, we test sse anyway elsewhere
    
    * Update docs/examples/responses/sse_responses.py
    
    Co-authored-by: Peter Schutt <peter.github@proton.me>
    
    * added import
    
    * added correct type
    
    ---------
    
    Co-authored-by: Peter Schutt <peter.github@proton.me>

--- tests/examples/test_responses/test_sse_responses.py
+++ tests/examples/test_responses/test_sse_responses.py
@@ -8,6 +8,4 @@ async def test_sse_responses_example() -> None:
     async with AsyncTestClient(app=app) as client:
         async with aconnect_sse(client, "GET", f"{client.base_url}/count") as event_source:
             events = [sse async for sse in event_source.aiter_sse()]
-            assert len(events) == 10
-        assert all(e.event == "message" for e in events)
-        assert all(e.data == str(i) for i, e in enumerate(events, 1))
+            assert len(events) == 50
