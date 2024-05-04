a118aef54b678dd2d47455efe082239e5a4690d1
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-05-04 12:25:13 +0200

    Ignore pyright issue :)

--- tests/third_party/test_beanie_integration.py
+++ tests/third_party/test_beanie_integration.py
@@ -8,7 +8,7 @@ from starlite.status_codes import HTTP_201_CREATED
 
 async def initialize_beanie() -> None:
     client = AsyncMongoMockClient()
-    await init_beanie(document_models=[Widget], database=client.get_database(name="db"))
+    await init_beanie(document_models=[Widget], database=client.get_database(name="db"))  # pyright: ignore
 
 
 class Widget(Document):
