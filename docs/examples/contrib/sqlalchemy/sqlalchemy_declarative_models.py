5cfca23d139f9425b5480175117a13bb0fe1adac
Joren Six <joren.six@ugent.be>
2024-05-13 12:03:50 +0200

    Update docs/examples/contrib/sqlalchemy/sqlalchemy_declarative_models.py
    create_all flag
    
    Co-authored-by: Cody Fincher <204685+cofin@users.noreply.github.com>

--- docs/examples/contrib/sqlalchemy/sqlalchemy_declarative_models.py
+++ docs/examples/contrib/sqlalchemy/sqlalchemy_declarative_models.py
@@ -33,7 +33,7 @@ class Book(UUIDAuditBase):
 
 session_config = AsyncSessionConfig(expire_on_commit=False)
 sqlalchemy_config = SQLAlchemyAsyncConfig(
-    connection_string="sqlite+aiosqlite:///test.sqlite", session_config=session_config
+    connection_string="sqlite+aiosqlite:///test.sqlite", session_config=session_config, create_all=True
 )  # Create 'async_session' dependency.
 sqlalchemy_plugin = SQLAlchemyPlugin(config=sqlalchemy_config)
 
