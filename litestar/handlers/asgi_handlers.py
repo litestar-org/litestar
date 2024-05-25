779f8fd2456bfadcfd83364844231fa7331d9ef8
Janek Nouvertné <provinzkraut@posteo.de>
2024-05-25 12:05:39 +0200

    feat!: Make route handlers functional decorators (#3436)
    * make route handlers functional decorators

--- litestar/handlers/asgi_handlers.py
+++ litestar/handlers/asgi_handlers.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Mapping, Sequence
+from typing import TYPE_CHECKING, Any, Callable, Mapping, Sequence
 
 from litestar.exceptions import ImproperlyConfiguredException
 from litestar.handlers.base import BaseRouteHandler
@@ -13,24 +13,20 @@ __all__ = ("ASGIRouteHandler", "asgi")
 if TYPE_CHECKING:
     from litestar.connection import ASGIConnection
     from litestar.types import (
+        AsyncAnyCallable,
         ExceptionHandlersMap,
         Guard,
-        MaybePartial,  # noqa: F401
     )
 
 
 class ASGIRouteHandler(BaseRouteHandler):
-    """ASGI Route Handler decorator.
-
-    Use this decorator to decorate ASGI applications.
-    """
-
     __slots__ = ("is_mount",)
 
     def __init__(
         self,
         path: str | Sequence[str] | None = None,
         *,
+        fn: AsyncAnyCallable,
         exception_handlers: ExceptionHandlersMap | None = None,
         guards: Sequence[Guard] | None = None,
         name: str | None = None,
@@ -39,17 +35,20 @@ class ASGIRouteHandler(BaseRouteHandler):
         signature_namespace: Mapping[str, Any] | None = None,
         **kwargs: Any,
     ) -> None:
-        """Initialize ``ASGIRouteHandler``.
+        """Route handler for ASGI routes.
 
         Args:
+            path: A path fragment for the route handler function or a list of path fragments. If not given defaults to
+                ``/``.
+            fn: The handler function.
+
+                .. versionadded:: 3.0
             exception_handlers: A mapping of status codes and/or exception types to handler functions.
             guards: A sequence of :class:`Guard <.types.Guard>` callables.
             name: A string identifying the route handler.
             opt: A string key mapping of arbitrary values that can be accessed in :class:`Guards <.types.Guard>` or
                 wherever you have access to :class:`Request <.connection.Request>` or
                 :class:`ASGI Scope <.types.Scope>`.
-            path: A path fragment for the route handler function or a list of path fragments. If not given defaults to
-                ``/``
             is_mount: A boolean dictating whether the handler's paths should be regarded as mount paths. Mount path
                 accept any arbitrary paths that begin with the defined prefixed path. For example, a mount with the path
                 ``/some-path/`` will accept requests for ``/some-path/`` and any sub path under this, e.g.
@@ -61,6 +60,7 @@ class ASGIRouteHandler(BaseRouteHandler):
         self.is_mount = is_mount
         super().__init__(
             path,
+            fn=fn,
             exception_handlers=exception_handlers,
             guards=guards,
             name=name,
@@ -101,4 +101,50 @@ class ASGIRouteHandler(BaseRouteHandler):
         await self.fn(scope=connection.scope, receive=connection.receive, send=connection.send)
 
 
-asgi = ASGIRouteHandler
+def asgi(
+    path: str | Sequence[str] | None = None,
+    *,
+    exception_handlers: ExceptionHandlersMap | None = None,
+    guards: Sequence[Guard] | None = None,
+    name: str | None = None,
+    opt: Mapping[str, Any] | None = None,
+    is_mount: bool = False,
+    signature_namespace: Mapping[str, Any] | None = None,
+    handler_class: type[ASGIRouteHandler] = ASGIRouteHandler,
+    **kwargs: Any,
+) -> Callable[[AsyncAnyCallable], ASGIRouteHandler]:
+    """Create an :class:`ASGIRouteHandler`.
+
+    Args:
+        path: A path fragment for the route handler function or a sequence of path fragments. If not given defaults
+            to ``/``
+        exception_handlers: A mapping of status codes and/or exception types to handler functions.
+        guards: A sequence of :class:`Guard <.types.Guard>` callables.
+        name: A string identifying the route handler.
+        opt: A string keyed mapping of arbitrary values that can be accessed in :class:`Guards <.types.Guard>` or
+            wherever you have access to :class:`Request <.connection.Request>` or
+            :class:`ASGI Scope <.types.Scope>`.
+        signature_namespace: A mapping of names to types for use in forward reference resolution during signature
+            modelling.
+        is_mount: A boolean dictating whether the handler's paths should be regarded as mount paths. Mount path
+            accept any arbitrary paths that begin with the defined prefixed path. For example, a mount with the path
+            ``/some-path/`` will accept requests for ``/some-path/`` and any sub path under this, e.g.
+            ``/some-path/sub-path/`` etc.
+        handler_class: Route handler class instantiated by the decorator
+        **kwargs: Any additional kwarg - will be set in the opt dictionary.
+    """
+
+    def decorator(fn: AsyncAnyCallable) -> ASGIRouteHandler:
+        return handler_class(
+            fn=fn,
+            path=path,
+            exception_handlers=exception_handlers,
+            guards=guards,
+            name=name,
+            opt=opt,
+            is_mount=is_mount,
+            signature_namespace=signature_namespace,
+            **kwargs,
+        )
+
+    return decorator
