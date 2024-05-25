edde06d146594f5c4bf2d52d01ef02ba42a8b85e
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-05-25 12:07:17 +0200

    fix typing

--- tests/unit/test_handlers/test_http_handlers/test_deprecation.py
+++ tests/unit/test_handlers/test_http_handlers/test_deprecation.py
@@ -9,5 +9,5 @@ from litestar.handlers import delete, get, head, patch, post, put
 def test_subclass_warns_deprecation(handler_cls: get | post | put | patch | delete | head) -> None:
     with pytest.warns(DeprecationWarning):
 
-        class SubClass(handler_cls):
+        class SubClass(handler_cls):  # type: ignore[valid-type, misc]
             pass
