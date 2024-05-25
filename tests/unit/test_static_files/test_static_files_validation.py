747f863f3c5dbf2ead7e217ce8485c7334888bc4
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-05-25 10:39:04 +0200

    Resolve merge conflicts

--- tests/unit/test_static_files/test_static_files_validation.py
+++ tests/unit/test_static_files/test_static_files_validation.py
@@ -1,6 +1,5 @@
-import asyncio
 from pathlib import Path
-from typing import List, cast
+from typing import List
 
 import pytest
 
@@ -72,37 +71,3 @@ def test_runtime_validation_of_request_method_create_handler(tmpdir: "Path", met
     with create_test_client(create_static_files_router(path="/static", directories=[tmpdir])) as client:
         response = client.request(method, "/static/test.txt")
         assert response.status_code == expected
-
-
-def test_config_validation_of_path_prevents_directory_traversal(tmpdir: "Path") -> None:
-    # Setup: Create a 'secret.txt' outside the static directory to simulate sensitive file
-    secret_path = Path(tmpdir) / "../secret.txt"
-    secret_path.write_text("This is a secret file.", encoding="utf-8")
-
-    # Setup: Create 'test.txt' inside the static directory
-    test_file_path = Path(tmpdir) / "test.txt"
-    test_file_path.write_text("This is a test file.", encoding="utf-8")
-
-    # Get StaticFiles handler
-    config = StaticFilesConfig(path="/static", directories=[tmpdir])
-    asgi_router = config.to_static_files_app()
-    static_files_handler = cast("StaticFiles", asgi_router.fn)
-
-    # Resolve file path with the StaticFiles handler
-    string_path = Path("../secret.txt").as_posix()
-
-    coroutine = static_files_handler.get_fs_info(directories=static_files_handler.directories, file_path=string_path)
-    resolved_path, fs_info = asyncio.run(coroutine)
-
-    assert resolved_path is None  # Because the resolved path is outside the static directory
-    assert fs_info is None  # Because the file doesn't exist, so there is no info
-
-    # Resolve file path with the StaticFiles handler
-    string_path = Path("test.txt").as_posix()
-
-    coroutine = static_files_handler.get_fs_info(directories=static_files_handler.directories, file_path=string_path)
-    resolved_path, fs_info = asyncio.run(coroutine)
-
-    expected_resolved_path = tmpdir / "test.txt"
-    assert resolved_path == expected_resolved_path  # Because the resolved path is inside the static directory
-    assert fs_info is not None  # Because the file exists, so there is info
