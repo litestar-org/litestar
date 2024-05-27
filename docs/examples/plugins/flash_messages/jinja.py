0e704b9cd67138fe1ff48da18d67db8b123277b8
Cody Fincher <204685+cofin@users.noreply.github.com>
2024-05-27 10:20:27 -0500

    docs: updated example for flash messages (#3530)
    * chore(docs): updated example for flash messages
    
    * chore: remove redundant store config

--- docs/examples/plugins/flash_messages/jinja.py
+++ docs/examples/plugins/flash_messages/jinja.py
@@ -1,9 +1,13 @@
 from litestar import Litestar
 from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.middleware.session.server_side import ServerSideSessionConfig
 from litestar.plugins.flash import FlashConfig, FlashPlugin
 from litestar.template.config import TemplateConfig
 
 template_config = TemplateConfig(engine=JinjaTemplateEngine, directory="templates")
 flash_plugin = FlashPlugin(config=FlashConfig(template_config=template_config))
 
-app = Litestar(plugins=[flash_plugin])
+app = Litestar(
+    plugins=[flash_plugin],
+    middleware=[ServerSideSessionConfig().middleware],
+)
