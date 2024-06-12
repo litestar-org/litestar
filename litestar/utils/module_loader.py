efd19089f796b40f4b91f5272d22153f6b3bdac0
supercoder-dev <mukunda@superagi.com>
2024-06-12 16:35:07 +0530

    Update module_loader.py

--- litestar/utils/module_loader.py
+++ litestar/utils/module_loader.py
@@ -39,7 +39,8 @@ def module_to_os_path(dotted_path: str = "app") -> Path:
     except ModuleNotFoundError as e:
         raise TypeError(f"Couldn't find the path for {dotted_path}") from e
 
-    return Path(str(src.origin).rsplit(os.path.sep + "__init__.py", maxsplit=1)[0])
+    path = Path(src.origin)
+    return path.parent if path.is_file() else path
 
 
 def import_string(dotted_path: str) -> Any:
