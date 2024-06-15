407332f593feb9600d1086dd92858bc73657352a
Alc-Alc <alc@localhost>
2024-06-15 14:51:12 +0530

    test: add test to check (lack of) warnings

--- tests/unit/test_handlers/test_http_handlers/test_deprecation.py
+++ tests/unit/test_handlers/test_http_handlers/test_deprecation.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from importlib import reload
+from warnings import catch_warnings, simplefilter
+
 import pytest
 
 from litestar.handlers import delete, get, head, patch, post, put
@@ -11,3 +14,15 @@ def test_subclass_warns_deprecation(handler_cls: get | post | put | patch | dele
 
         class SubClass(handler_cls):  # type: ignore[valid-type, misc]
             pass
+
+
+def test_default_no_warns() -> None:
+    with catch_warnings(record=True) as warnings:
+        simplefilter("always")
+        import litestar.handlers.http_handlers.decorators
+
+        reload(litestar.handlers.http_handlers.decorators)
+        assert len(warnings) == 0
+
+        # revert to previous filter
+        simplefilter("default")
