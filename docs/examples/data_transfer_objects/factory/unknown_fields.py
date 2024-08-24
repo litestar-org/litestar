0c6f60b39c5e1842f69e8b0c6812d732875b85b3
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-08-24 12:51:08 +0200

    feat(DTO): introduce forbid_unknown_fields config

---
+++ docs/examples/data_transfer_objects/factory/unknown_fields.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from typing_extensions import Annotated
+
+from litestar import Litestar, post
+from litestar.dto import DataclassDTO, DTOConfig
+
+
+@dataclass
+class User:
+    id: str
+
+
+UserDTO = DataclassDTO[Annotated[User, DTOConfig(forbid_unknown_fields=True)]]
+
+
+@post("/users", dto=UserDTO)
+async def create_user(data: User) -> User:
+    return data
+
+
+app = Litestar([create_user])
+
+
+# run: /users -H "Content-Type: application/json" -d '{"id": "1", "name": "Peter"}'
