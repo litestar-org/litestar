8388650d00ca158d0edd993ee6570d4fdd0f1808
euri10 <benoit.barthelet@gmail.com>
2024-08-29 17:50:11 +0200

    add test

--- tests/unit/test_middleware/test_base_middleware.py
+++ tests/unit/test_middleware/test_base_middleware.py
@@ -123,7 +123,7 @@ def test_exclude_by_pattern_list() -> None:
         assert "test" in response.headers
 
 
-@pytest.mark.parametrize("excludes", ["/", ["/", "/foo"]])
+@pytest.mark.parametrize("excludes", ["/", ["/", "/foo"], "^/?"])
 def test_exclude_by_pattern_warns_if_exclude_all(excludes: Union[str, List[str]]) -> None:
     class SubclassMiddleware(AbstractMiddleware):
         exclude = excludes
@@ -134,6 +134,17 @@ def test_exclude_by_pattern_warns_if_exclude_all(excludes: Union[str, List[str]]
     with pytest.warns(LitestarWarning, match="Middleware 'SubclassMiddleware' exclude pattern"):
         create_test_client(middleware=[SubclassMiddleware])
 
+def test_exclude_doesnt_warn_on_non_greedy_pattern() -> None:
+    class SubclassMiddleware(AbstractMiddleware):
+        exclude = "^/$"
+
+        async def __call__(self, scope: "Scope", receive: "Receive", send: "Send") -> None:
+            await self.app(scope, receive, send)
+
+    with pytest.warns(None) as record:
+        create_test_client(middleware=[SubclassMiddleware])
+
+    assert not record.list
 
 def test_exclude_by_opt_key() -> None:
     class SubclassMiddleware(AbstractMiddleware):
