53841d0390abd0d1731f7387314df51c771f73a4
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-09-15 15:56:50 +0200

    Fix tests

--- tests/unit/test_testing/test_sub_client/demo.py
+++ tests/unit/test_testing/test_sub_client/demo.py
@@ -2,36 +2,26 @@
 Assemble components into an app that shall be tested
 """
 
+import asyncio
 from typing import AsyncIterator
 
-from redis.asyncio import Redis
-
 from litestar import Litestar, get
-from litestar.channels import ChannelsPlugin
-from litestar.channels.backends.redis import RedisChannelsPubSubBackend
 from litestar.response import ServerSentEvent
 
 
 @get("/notify/{topic:str}")
-async def get_notified(topic: str, channels: ChannelsPlugin) -> ServerSentEvent:
+async def get_notified(topic: str) -> ServerSentEvent:
     async def generator() -> AsyncIterator[bytes]:
-        async with channels.start_subscription([topic]) as subscriber:
-            async for event in subscriber.iter_events():
-                yield event
+        yield topic
+        while True:
+            await asyncio.sleep(0.1)
 
     return ServerSentEvent(generator(), event_type="Notifier")
 
 
 def create_test_app() -> Litestar:
-    redis_instance = Redis()
-    channels_backend = RedisChannelsPubSubBackend(redis=redis_instance)
-    channels_instance = ChannelsPlugin(backend=channels_backend, arbitrary_channels_allowed=True)
-
     return Litestar(
-        route_handlers=[
-            get_notified,
-        ],
-        plugins=[channels_instance],
+        route_handlers=[get_notified],
     )
 
 
