c7a305230ea4ceeadf58bf1788428d6030cabed2
Cody Fincher <cody@litestar.dev>
2024-10-20 21:48:38 +0000

    feat: implement `htmx` plugin

---
+++ litestar/plugins/htmx.py
@@ -0,0 +1,51 @@
+# ruff: noqa: TCH004, F401
+# pyright: reportUnusedImport=false
+from __future__ import annotations
+
+from litestar_htmx import (
+    ClientRedirect,
+    ClientRefresh,
+    EventAfterType,
+    HTMXDetails,
+    HTMXHeaders,
+    HtmxHeaderType,
+    HTMXPlugin,
+    HTMXRequest,
+    HTMXTemplate,
+    HXLocation,
+    HXStopPolling,
+    LocationType,
+    PushUrl,
+    PushUrlType,
+    ReplaceUrl,
+    Reswap,
+    ReSwapMethod,
+    Retarget,
+    TriggerEvent,
+    TriggerEventType,
+    _utils,
+)
+
+__all__ = (
+    "HTMXPlugin",
+    "HTMXDetails",
+    "HTMXHeaders",
+    "HTMXRequest",
+    "HXStopPolling",
+    "HXLocation",
+    "ClientRedirect",
+    "ClientRefresh",
+    "PushUrl",
+    "ReplaceUrl",
+    "Reswap",
+    "Retarget",
+    "TriggerEvent",
+    "HTMXTemplate",
+    "HtmxHeaderType",
+    "LocationType",
+    "TriggerEventType",
+    "EventAfterType",
+    "PushUrlType",
+    "ReSwapMethod",
+    "_utils",
+)
