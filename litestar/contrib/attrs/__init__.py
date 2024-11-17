88eb9328274182521ccc321770155f301eccd6fc
Cody Fincher <cody@litestar.dev>
2024-11-17 16:20:04 +0000

    fix: correct deprecated version

--- litestar/contrib/attrs/__init__.py
+++ litestar/contrib/attrs/__init__.py
@@ -14,7 +14,7 @@ def __getattr__(attr_name: str) -> object:
 
         warn_deprecation(
             deprecated_name=f"litestar.contrib.attrs.{attr_name}",
-            version="2.12",
+            version="2.12.1",
             kind="import",
             removal_in="3.0",
             info=f"importing {attr_name} from 'litestar.contrib.attrs' is deprecated, please "
