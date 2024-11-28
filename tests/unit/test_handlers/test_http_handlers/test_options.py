b56525fcb5c509d3c796878b31ffdf54fe89e6b8
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2024-11-28 18:46:04 +0100

    add a comment

--- tests/unit/test_handlers/test_http_handlers/test_options.py
+++ tests/unit/test_handlers/test_http_handlers/test_options.py
@@ -1,8 +1,20 @@
+import pytest
+
 from litestar import get, Router
 from litestar.testing import create_test_client
 
 
+@pytest.mark.xfail(reason="broken behaviour that never really worked")
 def test_option_handler_inherits_layer_config() -> None:
+    # Currently broken. The reason this cannot work reliably is that when auto-creating
+    # OPTIONS handlers, we *assume* that it should inherit the configuration of
+    # path-adjacent route handlers, e.g. if a route handler is defined on '/', and
+    # registered on a router with the path '/one', it would receive the configuration
+    # from that router. However, since it is possible to have multiple handlers per
+    # path, which can also be registered with different routers, we would still only
+    # create one 'OPTIONS' handler for each path, even if multiple routers exist with
+    # handlers for that path. In this case, it is unclear which configuration the
+    # 'OPTIONS' handler should receive
     @get("/")
     def handler() -> None:
         return None
