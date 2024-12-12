f31ef97d6cb725bf9898f55abbb5150b36823f27
Janek Nouvertné <provinzkraut@posteo.de>
2024-12-12 15:35:39 +0100

    feat: WebSocket send stream (#3894)

---
+++ docs/examples/websockets/stream_di_hog.py
@@ -0,0 +1,23 @@
+import asyncio
+from typing import AsyncGenerator
+
+from app.lib import ping_external_resource
+from litestar import Litestar, websocket_stream
+
+RESOURCE_LOCK = asyncio.Lock()
+
+
+async def acquire_lock() -> AsyncGenerator[None, None]:
+    async with RESOURCE_LOCK:
+        yield
+
+
+@websocket_stream("/")
+async def ping(lock: asyncio.Lock) -> AsyncGenerator[float, None]:
+    while True:
+        alive = await ping_external_resource()
+        yield alive
+        await asyncio.sleep(1)
+
+
+app = Litestar([ping], dependencies={"lock": acquire_lock})
