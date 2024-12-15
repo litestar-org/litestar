9fd31414ef4036c09eb359bd1fa1f1efbd091380
Cody Fincher <cody@litestar.dev>
2024-12-15 23:39:21 +0000

    fix: updated

--- tests/examples/test_contrib/test_sqlalchemy/test_sqlalchemy_examples.py
+++ tests/examples/test_contrib/test_sqlalchemy/test_sqlalchemy_examples.py
@@ -5,9 +5,10 @@ from litestar.testing import TestClient
 pytestmark = pytest.mark.xdist_group("sqlalchemy_examples")
 
 
-def test_sqlalchemy_declarative_models() -> None:
-    from docs.examples.contrib.sqlalchemy.sqlalchemy_declarative_models import app
+async def test_sqlalchemy_declarative_models() -> None:
+    from docs.examples.contrib.sqlalchemy.sqlalchemy_declarative_models import app, sqlalchemy_config
 
+    await sqlalchemy_config.create_all_metadata(app)
     with TestClient(app) as client:
         response = client.get("/authors")
         assert response.status_code == 200
