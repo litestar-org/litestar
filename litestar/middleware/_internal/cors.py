f393a17024e2d6288b2a7473431498d3c1ed8206
Saska Karsi <44523813+saska@users.noreply.github.com>
2025-02-27 21:22:19 +0200

    fix: check for default headers before appending to cors header failures (#4015)

--- litestar/middleware/_internal/cors.py
+++ litestar/middleware/_internal/cors.py
@@ -113,8 +113,12 @@ class CORSMiddleware(AbstractMiddleware):
                 response_headers["Access-Control-Allow-Headers"] = ", ".join(
                     sorted(set(pre_flight_requested_headers) | DEFAULT_ALLOWED_CORS_HEADERS)  # pyright: ignore
                 )
-            elif any(header.lower() not in self.config.allow_headers for header in pre_flight_requested_headers):
-                failures.append("headers")
+            else:
+                all_allowed_headers = set(self.config.allow_headers).union(
+                    default_header.lower() for default_header in DEFAULT_ALLOWED_CORS_HEADERS
+                )
+                if any(header.lower() not in all_allowed_headers for header in pre_flight_requested_headers):
+                    failures.append("headers")
 
         return (
             Response(
