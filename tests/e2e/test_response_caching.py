f35c88100eeae71b890ee06809054b6e8cdc47b0
euri10 <benoit.barthelet@gmail.com>
2025-03-04 22:28:50 +0100

    mypy sh*t

--- tests/e2e/test_response_caching.py
+++ tests/e2e/test_response_caching.py
@@ -225,9 +225,9 @@ def test_middleware_not_applied_to_non_cached_routes(
     unpacked_middleware = []
     cur = client.app.asgi_router.root_route_map_node.children["/"].asgi_handlers["GET"][0]
     while hasattr(cur, "app"):
-        unpacked_middleware.append(cur.func.__self__ if isinstance(cur, partial) else cur)
+        unpacked_middleware.append(cur.func.__self__ if isinstance(cur, partial) else cur)  # type: ignore[attr-defined]
         cur = cur.app
-    unpacked_middleware.append(cur.func.__self__ if isinstance(cur, partial) else cur)
+    unpacked_middleware.append(cur.func.__self__ if isinstance(cur, partial) else cur)  # type: ignore[attr-defined]
 
     assert len([m for m in unpacked_middleware if isinstance(m, ResponseCacheMiddleware)]) == int(expect_applied)
 
