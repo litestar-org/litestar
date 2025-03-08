3f1a145dc62e6ba8b64bacdad41716d1a13d7f41
euri10 <benoit.barthelet@gmail.com>
2025-03-08 10:08:28 +0100

    rate middleware switch too, not handling keys atm

--- docs/examples/stores/registry_default_factory_namespacing.py
+++ docs/examples/stores/registry_default_factory_namespacing.py
@@ -1,5 +1,5 @@
 from litestar import Litestar, get
-from litestar.middleware.rate_limit import RateLimitConfig
+from litestar.middleware.rate_limit import RateLimitConfig, RateLimitMiddleware
 from litestar.middleware.session.server_side import ServerSideSessionConfig
 from litestar.stores.redis import RedisStore
 from litestar.stores.registry import StoreRegistry
@@ -17,7 +17,7 @@ app = Litestar(
     [cached_handler],
     stores=StoreRegistry(default_factory=root_store.with_namespace),
     middleware=[
-        RateLimitConfig(("second", 1)).middleware,
+        RateLimitMiddleware(RateLimitConfig(("second", 1))),
         ServerSideSessionConfig().middleware,
     ],
 )
