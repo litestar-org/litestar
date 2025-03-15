f5d96c6544cfcc6ef862f343efe6c117102c5145
euri10 <benoit.barthelet@gmail.com>
2025-03-15 16:04:46 +0100

    fix tests

--- docs/examples/testing/test_get_session_data.py
+++ docs/examples/testing/test_get_session_data.py
@@ -1,5 +1,6 @@
 from litestar import Litestar, Request, post
-from litestar.middleware.session.server_side import ServerSideSessionConfig
+from litestar.middleware.session import SessionMiddleware
+from litestar.middleware.session.server_side import ServerSideSessionBackend, ServerSideSessionConfig
 from litestar.testing import TestClient
 
 session_config = ServerSideSessionConfig()
@@ -10,7 +11,11 @@ def set_session_data(request: Request) -> None:
     request.session["foo"] = "bar"
 
 
-app = Litestar(route_handlers=[set_session_data], middleware=[session_config.middleware], debug=True)
+app = Litestar(
+    route_handlers=[set_session_data],
+    middleware=[SessionMiddleware(ServerSideSessionBackend(session_config))],
+    debug=True,
+)
 
 with TestClient(app=app, session_config=session_config) as client:
     client.post("/test").json()
