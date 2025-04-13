61f1dedf1c2fe212c1a126d1996f5c9e91c4e470
Cody Fincher <cody@litestar.dev>
2025-04-13 19:34:27 +0000

    chore: updated test

--- tests/unit/test_plugins/test_sqlalchemy.py
+++ tests/unit/test_plugins/test_sqlalchemy.py
@@ -1,21 +1,16 @@
-from advanced_alchemy.extensions import litestar as sa_litestar
-from advanced_alchemy.extensions.litestar import base as sa_base
-from advanced_alchemy.extensions.litestar import exceptions as sa_exceptions
-from advanced_alchemy.extensions.litestar import filters as sa_filters
-from advanced_alchemy.extensions.litestar import mixins as sa_mixins
-from advanced_alchemy.extensions.litestar import repository as sa_repository
-from advanced_alchemy.extensions.litestar import service as sa_service
-from advanced_alchemy.extensions.litestar import types as sa_types
-from advanced_alchemy.extensions.litestar import utils as sa_utils
-
-from litestar.plugins import sqlalchemy
-
-
 def test_re_exports() -> None:
-    assert sqlalchemy.base is sa_base
+    from advanced_alchemy.extensions import litestar as sa_litestar
+    from advanced_alchemy.extensions.litestar import exceptions as sa_exceptions
+    from advanced_alchemy.extensions.litestar import filters as sa_filters
+    from advanced_alchemy.extensions.litestar import repository as sa_repository
+    from advanced_alchemy.extensions.litestar import service as sa_service
+    from advanced_alchemy.extensions.litestar import types as sa_types
+    from advanced_alchemy.extensions.litestar import utils as sa_utils
+
+    from litestar.plugins import sqlalchemy
+
     assert sqlalchemy.filters is sa_filters
     assert sqlalchemy.types is sa_types
-    assert sqlalchemy.mixins is sa_mixins
     assert sqlalchemy.utils is sa_utils
     assert sqlalchemy.repository is sa_repository
     assert sqlalchemy.service is sa_service
