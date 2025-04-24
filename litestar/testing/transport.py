57018574aaa2661ae8cc4df13d6b109f63391583
euri10 <benoit.barthelet@gmail.com>
2025-04-24 10:02:56 +0200

    small refactor

--- litestar/testing/transport.py
+++ litestar/testing/transport.py
@@ -43,7 +43,7 @@ class SendReceiveContext(TypedDict):
     context: Any | None
 
 
-class AsyncTestClientTransport(Generic[T]):
+class BaseTestClientTransport(Generic[T]):
     def __init__(
         self,
         client: T,
@@ -138,13 +138,15 @@ class AsyncTestClientTransport(Generic[T]):
             "server": (host, port),
         }
 
+
+class AsyncTestClientTransport(BaseTestClientTransport):
     async def handle_async_request(self, request: Request) -> Response:
         scope = self.parse_request(request=request)
         if scope["type"] == "websocket":
             scope.update(
                 subprotocols=[value.strip() for value in request.headers.get("sec-websocket-protocol", "").split(",")]
             )
-            session = WebSocketTestSession(client=self.client, scope=cast("WebSocketScope", scope))  # type: ignore[arg-type]
+            session = WebSocketTestSession(client=self.client, scope=cast("WebSocketScope", scope))
             raise ConnectionUpgradeExceptionError(session)
 
         scope.update(method=request.method, http_version="1.1", extensions={"http.response.template": {}})
@@ -190,108 +192,14 @@ class AsyncTestClientTransport(Generic[T]):
         """Close the transport."""
 
 
-class TestClientTransport(Generic[T]):
-    def __init__(
-        self,
-        client: T,
-        raise_server_exceptions: bool = True,
-        root_path: str = "",
-    ) -> None:
-        self.client = client
-        self.raise_server_exceptions = raise_server_exceptions
-        self.root_path = root_path
-
-    @staticmethod
-    def create_receive(request: Request, context: SendReceiveContext) -> Receive:
-        async def receive() -> ReceiveMessage:
-            if context["request_complete"]:
-                if not context["response_complete"].is_set():
-                    await context["response_complete"].wait()
-                disconnect_event: HTTPDisconnectEvent = {"type": "http.disconnect"}
-                return disconnect_event
-
-            body = cast("Union[bytes, str, GeneratorType]", (request.read() or b""))
-            request_event: HTTPRequestEvent = {"type": "http.request", "body": b"", "more_body": False}
-            if isinstance(body, GeneratorType):  # pragma: no cover
-                try:
-                    chunk = body.send(None)
-                    request_event["body"] = chunk if isinstance(chunk, bytes) else chunk.encode("utf-8")
-                    request_event["more_body"] = True
-                except StopIteration:
-                    context["request_complete"] = True
-            else:
-                context["request_complete"] = True
-                request_event["body"] = body if isinstance(body, bytes) else body.encode("utf-8")
-            return request_event
-
-        return receive
-
-    @staticmethod
-    def create_send(request: Request, context: SendReceiveContext) -> Send:
-        async def send(message: Message) -> None:
-            if message["type"] == "http.response.start":
-                assert not context["response_started"], 'Received multiple "http.response.start" messages.'  # noqa: S101
-                context["raw_kwargs"]["status_code"] = message["status"]
-                context["raw_kwargs"]["headers"] = [
-                    (k.decode("utf-8"), v.decode("utf-8")) for k, v in message.get("headers", [])
-                ]
-                context["response_started"] = True
-            elif message["type"] == "http.response.body":
-                assert context["response_started"], 'Received "http.response.body" without "http.response.start".'  # noqa: S101
-                assert not context[  # noqa: S101
-                    "response_complete"
-                ].is_set(), 'Received "http.response.body" after response completed.'
-                body = message.get("body", b"")
-                more_body = message.get("more_body", False)
-                if request.method != "HEAD":
-                    context["raw_kwargs"]["stream"].write(body)
-                if not more_body:
-                    context["raw_kwargs"]["stream"].seek(0)
-                    context["response_complete"].set()
-            elif message["type"] == "http.response.template":  # type: ignore[comparison-overlap] # pragma: no cover
-                context["template"] = message["template"]  # type: ignore[unreachable]
-                context["context"] = message["context"]
-
-        return send
-
-    def parse_request(self, request: Request) -> dict[str, Any]:
-        scheme = request.url.scheme
-        netloc = unquote(request.url.netloc.decode(encoding="ascii"))
-        path = request.url.path
-        raw_path = request.url.raw_path
-        query = request.url.query.decode(encoding="ascii")
-        default_port = 433 if scheme in {"https", "wss"} else 80
-
-        if ":" in netloc:
-            host, port_string = netloc.split(":", 1)
-            port = int(port_string)
-        else:
-            host = netloc
-            port = default_port
-
-        host_header = request.headers.pop("host", host if port == default_port else f"{host}:{port}")
-
-        headers = [(k.lower().encode(), v.encode()) for k, v in (("host", host_header), *request.headers.items())]
-
-        return {
-            "type": "websocket" if scheme in {"ws", "wss"} else "http",
-            "path": unquote(path),
-            "raw_path": raw_path,
-            "root_path": self.root_path,
-            "scheme": scheme,
-            "query_string": query.encode(),
-            "headers": headers,
-            "client": ("testclient", 50000),
-            "server": (host, port),
-        }
-
+class TestClientTransport(BaseTestClientTransport):
     def handle_request(self, request: Request) -> Response:
         scope = self.parse_request(request=request)
         if scope["type"] == "websocket":
             scope.update(
                 subprotocols=[value.strip() for value in request.headers.get("sec-websocket-protocol", "").split(",")]
             )
-            session = WebSocketTestSession(client=self.client, scope=cast("WebSocketScope", scope))  # type: ignore[arg-type]
+            session = WebSocketTestSession(client=self.client, scope=cast("WebSocketScope", scope))
             raise ConnectionUpgradeExceptionError(session)
 
         scope.update(method=request.method, http_version="1.1", extensions={"http.response.template": {}})
