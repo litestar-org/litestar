8f58dfe54265947413c385e48a9f9e2cc589c404
euri10 <benoit.barthelet@gmail.com>
2025-04-24 08:55:29 +0200

    added failing test

--- tests/unit/test_testing/test_lifespan_handler.py
+++ tests/unit/test_testing/test_lifespan_handler.py
@@ -1,6 +1,9 @@
+import asyncio
+
 import pytest
 
-from litestar.testing import TestClient
+from litestar import Litestar, get
+from litestar.testing import AsyncTestClient, TestClient
 from litestar.testing.life_span_handler import LifeSpanHandler
 from litestar.types import Receive, Scope, Send
 
@@ -35,3 +38,17 @@ async def test_implicit_startup() -> None:
         handler = LifeSpanHandler(TestClient(app))
         await handler.wait_shutdown()
         handler.close()
+
+
+async def test_multiple_clients_event_loop() -> None:
+    @get("/")
+    def return_loop_id() -> dict:
+        return {"loop_id": id(asyncio.get_event_loop())}
+
+    app = Litestar(route_handlers=[return_loop_id])
+
+    async with AsyncTestClient(app) as client_1, AsyncTestClient(app) as client_2:
+        response_1 = await client_1.get("/")
+        response_2 = await client_2.get("/")
+
+    assert response_1.json() == response_2.json()  # FAILS
