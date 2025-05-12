372e8917f669f37503710aa6bb4cce656d68af88
RenameMe1 <v1mpire@icloud.com>
2025-05-12 20:54:26 +0300

    pre-commit update

--- docs/examples/responses/sse_responses_with_ping.py
+++ docs/examples/responses/sse_responses_with_ping.py
@@ -5,12 +5,14 @@ from litestar import Litestar, get
 from litestar.response import ServerSentEvent, ServerSentEventMessage
 from litestar.types import SSEData
 
+
 async def my_slow_generator() -> AsyncGenerator[SSEData, None]:
     count = 0
     while count < 1:
         await sleep(1)
         count += 1
         yield ServerSentEventMessage(data="content", event="message")
+
 
 @get(path="/with_ping", sync_to_thread=False)
 def sse_handler_with_ping_events() -> ServerSentEvent:
