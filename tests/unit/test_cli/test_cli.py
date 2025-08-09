281d48969996b154044eb4db6277f6adfdc7344c
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2025-08-09 16:23:01 +0200

    remove weird test

--- tests/unit/test_cli/test_cli.py
+++ tests/unit/test_cli/test_cli.py
@@ -65,34 +65,6 @@ def test_info_command_with_app_dir(
 
 
 @pytest.mark.xdist_group("cli_autodiscovery")
-def test_register_commands_from_entrypoint(mocker: "MockerFixture", runner: "CliRunner", app_file: "Path") -> None:
-    mock_command_callback = MagicMock()
-
-    @group()
-    def custom_group() -> None:
-        pass
-
-    @custom_group.command()
-    def custom_command(app: Litestar) -> None:
-        mock_command_callback()
-
-    mock_entry_point = MagicMock()
-    mock_entry_point.load = lambda: custom_group
-    if sys.version_info < (3, 10):
-        mocker.patch("importlib_metadata.entry_points", return_value=[mock_entry_point])
-    else:
-        mocker.patch("importlib.metadata.entry_points", return_value=[mock_entry_point])
-
-    importlib.reload(litestar.cli._utils)
-    cli_command = importlib.reload(litestar.cli.main).litestar_group
-
-    result = runner.invoke(cli_command, f"--app={app_file.stem}:app custom-group custom-command")
-
-    assert result.exit_code == 0
-    mock_command_callback.assert_called_once()
-
-
-@pytest.mark.xdist_group("cli_autodiscovery")
 @pytest.mark.parametrize("invalid_app", ["invalid", "info_with_app_dir"])
 def test_incorrect_app_argument(
     invalid_app: str, mocker: "MockerFixture", runner: "CliRunner", create_app_file: CreateAppFileFixture
