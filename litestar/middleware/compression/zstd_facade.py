fb89f8c86ffbffa0b4fa2fbdb1a58536126ffb74
ceballots <j.esteve.sorribas@gmail.com>
2025-09-15 23:05:14 +0200

    fixup! make dependency optional and add python version specifier

--- litestar/middleware/compression/zstd_facade.py
+++ litestar/middleware/compression/zstd_facade.py
@@ -2,10 +2,14 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Literal
 
-import zstandard as zstd
-
 from litestar.enums import CompressionEncoding
+from litestar.exceptions import MissingDependencyException
 from litestar.middleware.compression.facade import CompressionFacade
+
+try:
+    import zstandard as zstd
+except ImportError as e:
+    raise MissingDependencyException("zstandard") from e
 
 if TYPE_CHECKING:
     from io import BytesIO
