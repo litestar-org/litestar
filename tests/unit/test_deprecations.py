04e1d28ac7d06bc33062097b8e9569b61fc9d8c0
Мельников Максим <m.melnikov@idaproject.com>
2025-09-30 12:50:47 +0300

    feat(contrib): remove test

--- tests/unit/test_deprecations.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-def test_minijinja_from_state_deprecation() -> None:
-    with pytest.warns(DeprecationWarning):
-        from litestar.contrib.minijinja import minijinja_from_state  # noqa: F401
