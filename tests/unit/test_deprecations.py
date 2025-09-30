3ede7d077b74ec12c975b674d547020c76a8fe27
Максим Мельников <nikposd@mail.ru>
2025-09-30 13:01:52 +0300

    feat(contrib)!: remove deprecated minijnja module (#4361)

--- tests/unit/test_deprecations.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-
-def test_contrib_minijnja_deprecation() -> None:
-    with pytest.warns(DeprecationWarning):
-        from litestar.contrib.minijnja import MiniJinjaTemplateEngine  # noqa: F401
