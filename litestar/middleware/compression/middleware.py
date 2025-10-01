0cf27fdc455716c6dc88d4715c84d98be74a9a80
euri10 <benoit.barthelet@gmail.com>
2025-10-01 11:36:18 +0200

    local mypy and ci mypy dont report same errors ?

--- litestar/middleware/compression/middleware.py
+++ litestar/middleware/compression/middleware.py
@@ -104,7 +104,7 @@ class CompressionMiddleware(AbstractMiddleware):
         # We can't use `self.config.compression_facade` directly if the compression is `gzip` since
         # it may be being used as a fallback.
         if compression_encoding == CompressionEncoding.GZIP:
-            facade = GzipCompression(buffer=bytes_buffer, compression_encoding=compression_encoding, config=self.config)  # type: ignore[assignment]
+            facade = GzipCompression(buffer=bytes_buffer, compression_encoding=compression_encoding, config=self.config)
         elif compression_encoding == CompressionEncoding.ZSTD:
             facade = ZstdCompression(buffer=bytes_buffer, compression_encoding=compression_encoding, config=self.config)  # type: ignore[assignment]
         else:
