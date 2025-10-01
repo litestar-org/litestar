9dc13944d22d34910be97535317c7a3080f0a37c
euri10 <benoit.barthelet@gmail.com>
2025-10-01 11:39:29 +0200

    wrong line

--- litestar/middleware/compression/middleware.py
+++ litestar/middleware/compression/middleware.py
@@ -108,7 +108,7 @@ class CompressionMiddleware(AbstractMiddleware):
         elif compression_encoding == CompressionEncoding.ZSTD:
             facade = ZstdCompression(buffer=bytes_buffer, compression_encoding=compression_encoding, config=self.config)  # type: ignore[assignment]
         else:
-            facade = self.config.compression_facade(
+            facade = self.config.compression_facade(  # type: ignore[assignment]
                 buffer=bytes_buffer, compression_encoding=compression_encoding, config=self.config
             )
 
