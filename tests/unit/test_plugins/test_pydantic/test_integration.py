11f54647d83660097ceb4a295ddc6c8f9d862141
Janek Nouvertné <provinzkraut@posteo.de>
2025-10-05 16:45:41 +0200

    more mypy fixes

--- tests/unit/test_plugins/test_pydantic/test_integration.py
+++ tests/unit/test_plugins/test_pydantic/test_integration.py
@@ -442,7 +442,7 @@ def test_v2_computed_fields(with_dto: bool) -> None:
 def test_pydantic_v2_round_trip() -> None:
     class Submodel(pydantic_v2.BaseModel):
         bar: str
-        baz: list[int]
+        baz: List[int]
 
     class Model(pydantic_v2.BaseModel):
         foo: pydantic_v2.Json[Submodel]
@@ -451,7 +451,7 @@ def test_pydantic_v2_round_trip() -> None:
 
     @get("/")
     async def handler() -> Model:
-        return Model(foo=resp)  # type: ignore[arg-type] # pyright: ignore[reportArgumentType]
+        return Model(foo=resp)  # pyright: ignore[reportArgumentType]
 
     with create_test_client([handler], plugins=[PydanticPlugin(round_trip=True)]) as client:
         assert client.get("/").json() == {"foo": resp}
