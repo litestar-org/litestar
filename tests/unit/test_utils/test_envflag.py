afdfabedc23dc71a501a68acb143a8252a12fc96
Klu1d <asabaev.islam@icloud.com>
2025-10-19 19:30:00 +0300

    test: fix type annotations

--- tests/unit/test_utils/test_envflag.py
+++ tests/unit/test_utils/test_envflag.py
@@ -3,19 +3,19 @@ import os
 from litestar.utils import envflag
 
 
-def test_envflag_truthy_values():
+def test_envflag_truthy_values() -> None:
     for value in ("1", "true", "TRUE", "t", "T", "yes", "YES", "on", "ON", "y", "Y"):
         os.environ["TEST_FLAG"] = value
         assert envflag("TEST_FLAG") is True
         del os.environ["TEST_FLAG"]
 
 
-def test_envflag_falsy_values():
+def test_envflag_falsy_values() -> None:
     for value in ("0", "false", "no", "off", ""):
         os.environ["TEST_FLAG"] = value
         assert envflag("TEST_FLAG") is False
         del os.environ["TEST_FLAG"]
 
 
-def test_envflag_missing():
+def test_envflag_missing() -> None:
     assert envflag("NONEXISTENT_VAR") is False
