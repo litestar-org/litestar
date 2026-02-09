819abb0d6b7efcbd853cb4713efcd4a905f059ad
Janek Nouvertné <provinzkraut@posteo.de>
2026-02-09 20:23:33 +0100

    even more refactoring

--- litestar/logging/structlog.py
+++ litestar/logging/structlog.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import dataclasses
+import functools
 import logging
 import sys
 from typing import TYPE_CHECKING, Any, Callable
@@ -15,7 +16,7 @@ if TYPE_CHECKING:
     from collections.abc import Iterable
 
     from litestar import Litestar
-    from litestar.types import LifespanHook, Scope, GetLogger
+    from litestar.types import LifespanHook, Scope, GetLogger, Logger
 
 
 def structlog_exception_logging_handler(logger: structlog.BoundLogger, scope: Scope, tb: list[str]) -> None:
@@ -61,42 +62,44 @@ LITESTAR_PROCESSORS = (stdlib_extra_processor,)
 
 @dataclasses.dataclass(frozen=True)
 class StructLoggingConfig(LoggingConfig):
-    get_logger: GetLogger = dataclasses.field(default=structlog.get_logger)
     exception_logging_handler: ExceptionLoggingHandler = dataclasses.field(default=structlog_exception_logging_handler)  # type: ignore[assignment]
-    enabled_structured_logging: bool = dataclasses.field(default=True)
     formatter: logging.Formatter = dataclasses.field(default=None)
-    supports_json_like_data: bool = dataclasses.field(default=True)
-
-    configure_structlog: bool = True
     """
-    Whether to call :func:`structlog.configure`
+    This should always be 'None' to disable the Litestar formatter for structlog
     """
+    can_log_structured_data: bool = dataclasses.field(default=True)
 
     pretty_print_tty: bool = True
     """Pretty print log output when run from an interactive terminal."""
 
     processors: Iterable[structlog.typing.Processor] | None = None
     """
-    Passed to :func:`structlog.configure`
+    Passed to :class:`structlog.typing.BindableLogger`
     """
+
     wrapper_class: type[structlog.typing.BindableLogger] | None = None
     """
-    Passed to :func:`structlog.configure`
+    Passed to :class:`structlog.typing.BindableLogger`
     """
+
     context_class: type[structlog.typing.Context] | None = None
     """
-    Passed to :func:`structlog.configure`
+    Passed to :class:`structlog.typing.BindableLogger`
     """
-    logger_factory: Callable[..., structlog.typing.WrappedLogger] | None = structlog.stdlib.LoggerFactory
+
+    logger_factory: Callable[..., structlog.typing.WrappedLogger] | None = None
     """
     Passed to :func:`structlog.configure` It is highly recommended to use 
     :class:`structlog.stdlib.LoggerFactory`, as it will use the 
     ``litestar_queue_handler`` to ensure non-blocking logging
     """
-    cache_logger_on_first_use: bool | None = True
+
+    cache_logger_on_first_use: bool = True
     """
-    Passed to :func:`structlog.configure`
+    Passed to :class:`structlog.typing.BindableLogger`
     """
+
+    _structlog_config: dict[str, Any] | None = None
 
     @property
     def should_log_as_json(self) -> bool:
@@ -104,27 +107,31 @@ class StructLoggingConfig(LoggingConfig):
             return not self.pretty_print_tty
         return True
 
-    def configure_logger(self, app: Litestar) -> LifespanHook | None:
-        if self.configure_structlog:
-            processors = self.processors
-            if processors is None:
-                if self.should_log_as_json:
-                    processors = json_processors()
-                else:
-                    processors = structlog.get_config()["processors"]
+    def __post_init__(self) -> None:
+        processors = self.processors
+        if processors is None:
+            if self.should_log_as_json:
+                processors = json_processors()
+            else:
+                processors = structlog.get_config()["processors"]
 
-            wrapper_class = structlog.make_filtering_bound_logger(self.level)
-            if self.wrapper_class is not None:
-                wrapper_class = type(
-                    f"{wrapper_class.__name__}_{self.wrapper_class.__name__}", (wrapper_class, self.wrapper_class), {}
-                )
-
-            structlog.configure(
-                processors=[*LITESTAR_PROCESSORS, *processors],
-                wrapper_class=wrapper_class,
-                context_class=self.context_class,
-                logger_factory=self.logger_factory() if self.logger_factory else None,
-                cache_logger_on_first_use=self.cache_logger_on_first_use,
+        wrapper_class = structlog.make_filtering_bound_logger(self.level)
+        if self.wrapper_class is not None:
+            wrapper_class = type(
+                f"{wrapper_class.__name__}_{self.wrapper_class.__name__}", (wrapper_class, self.wrapper_class), {}
             )
 
-        return super().configure_logger(app=app)
+        config = {
+            "processors": [*LITESTAR_PROCESSORS, *processors],
+            "wrapper_class": wrapper_class,
+            "context_class": self.context_class,
+            "cache_logger_on_first_use": self.cache_logger_on_first_use,
+        }
+        # get around frozen dataclasses
+        object.__setattr__(self, "_structlog_config", config)
+
+    def get_litestar_logger(self, name: str | None = None) -> Logger:
+        return structlog.wrap_logger(
+            super().get_litestar_logger(name) if self.logger_factory is None else self.logger_factory(name),
+            **self._structlog_config,
+        )
